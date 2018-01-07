616862e5facd89aa1eb590d4ef2a88a949bc489a
Likit Preeyanon <likit.pre@mahidol.edu>
2018-01-07 22:33:13 +0700

    updated students check in view

--- app/studs/views.py
+++ app/studs/views.py
@@ -1,6 +1,7 @@
 import datetime
 from flask import render_template
-from ..models import Student
+from ..models import Student, ClassCheckIn, StudentCheckInRecord, Class
+from ..main import db
 from . import studbp as stud
 
 
@@ -16,9 +17,14 @@ def checkin(class_id, stud_id):
     if not class_id or not stud_id:
         return render_template(student={})
     else:
-        deadline = datetime.datetime.strptime('2018/01/07 10:00:00', '%Y/%m/%d %H:%M:%S')
+        fmt = '%Y-%m-%d %H:%M:%S'
+        class_checkin = ClassCheckIn.query.filter_by(class_id=class_id).first()
+        today = datetime.datetime.today().date()
+        deadline_str = '{0}-{1:02}-{2:02}'.format(today.year, today.month, today.day)
+        deadline_str += ' ' + class_checkin.deadline
+        deadline = datetime.datetime.strptime(deadline_str, fmt)
         student = Student.query.get(stud_id)
-        klass = {'title': 'Transformative'}
+        klass = Class.query.get(class_id)
         checkin = datetime.datetime.now()
         delta = checkin - deadline
         elapsed_mins = delta.total_seconds() / 60.0
@@ -28,6 +34,15 @@ def checkin(class_id, stud_id):
             status = 'is-warning'
         else:
             status = 'is-danger'
+        chk_record = StudentCheckInRecord(
+                            stud_id=stud_id,
+                            classchk_id=class_checkin.id,
+                            check_in_time=checkin,
+                            check_in_status=status,
+                            elapsed_mins=elapsed_mins
+                        )
+        db.session.add(chk_record)
+        db.session.commit()
         return render_template('/studs/checkin.html',
                 student=student,
                 klass=klass,
