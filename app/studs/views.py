f1b02847eb27270c62874a440d0cffa72671cc80
Likit Preeyanon <likit.pre@mahidol.edu>
2018-01-07 21:36:02 +0700

    updated studs.views to display students data from the db

--- app/studs/views.py
+++ app/studs/views.py
@@ -1,10 +1,13 @@
 import datetime
 from flask import render_template
+from ..models import Student
 from . import studbp as stud
 
 
 @stud.route('/')
 def index():
+    stud = Student.query.first()
+    print(stud.th_first_name)
     return '<h2>Welcome to Students Index Page.</h2>'
 
 
@@ -13,7 +16,20 @@ def checkin(class_id, stud_id):
     if not class_id or not stud_id:
         return render_template(student={})
     else:
-        chk_in_time = datetime.datetime.now()
+        deadline = datetime.datetime.strptime('2018/01/07 10:00:00', '%Y/%m/%d %H:%M:%S')
+        student = Student.query.get(stud_id)
         klass = {'title': 'Transformative'}
-        student = {'name': 'Suchunya Preeyanon', 'checkin': chk_in_time}
-        return render_template('/studs/checkin.html', student=student, klass=klass)
+        checkin = datetime.datetime.now()
+        delta = checkin - deadline
+        elapsed_mins = delta.total_seconds() / 60.0
+        if elapsed_mins < 0:
+            status = 'is-success'
+        elif elapsed_mins < 15.0:
+            status = 'is-warning'
+        else:
+            status = 'is-danger'
+        return render_template('/studs/checkin.html',
+                student=student,
+                klass=klass,
+                checkin=checkin,
+                status=status)
