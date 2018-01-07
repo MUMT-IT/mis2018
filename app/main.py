56b5df9041aeb53be846554eab1153c50e849b21
Likit Preeyanon <likit.pre@mahidol.edu>
2018-01-07 16:14:50 +0700

    added  blueprint and db managment commands

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
-from app import create_app
-from app.database import create_db, load_orgs
+from flask import Flask
+from sqlalchemy import create_engine, MetaData
+engine = create_engine('postgres+psycopg2://postgres:genius01@localhost:5444/test')
+meta = MetaData(bind=engine, reflect=True)
+connect = engine.connect()
 
-app = create_app()
+app = Flask(__name__)
+
+from kpi import kpibp as kpi_blueprint
+app.register_blueprint(kpi_blueprint, url_prefix='/kpi')
+
+
+from database import *
 
 @app.cli.command()
 def initdb():
@@ -12,7 +21,11 @@ def initdb():
 @app.cli.command()
 def populatedb():
     load_orgs()
+    load_strategy()
+    load_tactics()
+    load_themes()
+    load_activities()
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5555)
+    app.run(debug=True, port=5000, host="0.0.0.0")
