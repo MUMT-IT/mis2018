cfca89b1606e2b9cf4f3264a88af5ac49caea6f3
Likit Preeyanon <likit.pre@mahidol.edu>
2018-01-07 17:50:46 +0700

    added migrations folder from alembic

---
+++ migrations/versions/02f1ed0bb4ff_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 02f1ed0bb4ff
+Revises: 42cde3f8c6b9
+Create Date: 2018-01-07 17:46:14.900373
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '02f1ed0bb4ff'
+down_revision = '42cde3f8c6b9'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ migrations/versions/42cde3f8c6b9_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 42cde3f8c6b9
+Revises: 
+Create Date: 2018-01-07 17:44:29.784189
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '42cde3f8c6b9'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
