bf5a6e3fae97869591e1c0bb8605436d944f6a25
Likit Preeyanon <likit.pre@mahidol.edu>
2018-01-09 16:36:51 +0700

    deleted unused code
    modified code that loads orgs data to the db

--- app/database.py
+++ app/database.py
@@ -1,107 +1,22 @@
-# from datetime import datetime
-# from sqlalchemy import Table, Column, Integer, String, ForeignKey, DateTime, Boolean
-# from sqlalchemy.sql import func
-# from main import meta, connect, engine
-
-def create_db():
-    users = Table('users', meta,
-            Column('email', String, nullable=False, primary_key=True),
-            extend_existing=True
-            )
-
-    kpis = Table('kpis', meta,
-            Column('id', Integer, primary_key=True, autoincrement=True),
-            Column('strategy_id', Integer, ForeignKey('strategies.id')),
-            Column('tactic_id', Integer, ForeignKey('strategy_tactics.id')),
-            Column('theme_id', Integer, ForeignKey('strategy_themes.id')),
-            Column('activity_id', Integer, ForeignKey('strategy_activities.id')),
-            Column('created_by', String),
-            Column('created_at', DateTime, server_default=func.now()),
-            Column('name', String, nullable=False),
-            Column('refno', String),
-            Column('intent', String),
-            Column('frequency', Integer),
-            Column('unit', String),
-            Column('source', String),
-            Column('available', Boolean),
-            Column('availability', String),
-            Column('formula', String),
-            Column('keeper', String, ForeignKey('users.email')),
-            Column('note', String),
-            Column('target', String),
-            Column('target_source', String),
-            Column('target_setter', String),
-            Column('target_reporter', String),
-            Column('target_account', String),
-            Column('reporter', String),
-            Column('consult', String),
-            Column('account', String),
-            Column('informed', String),
-            Column('pfm_account', String),
-            Column('pfm_resposible', String),
-            Column('pfm_consult', String),
-            Column('pfm_informed', String),
-            extend_existing=True
-            )
-
-    orgs = Table('orgs', meta,
-            Column('id', Integer, primary_key=True, autoincrement=True),
-            Column('name', String, nullable=False),
-            Column('head', String),
-            Column('parent', Integer, ForeignKey('orgs.id')),
-            extend_existing=True
-            )
-
-    strategies = Table('strategies', meta,
-            Column('id', Integer, primary_key=True, autoincrement=True),
-            Column('refno', String, nullable=False),
-            Column('created_at', DateTime, server_default=func.now()),
-            Column('content', String, nullable=False),
-            Column('owner', Integer, ForeignKey('orgs.id'), nullable=False),
-            extend_existing=True
-            )
-
-    strategy_tactics = Table('strategy_tactics', meta,
-            Column('id', Integer, primary_key=True, autoincrement=True),
-            Column('refno', String, nullable=False),
-            Column('created_at', DateTime, server_default=func.now()),
-            Column('content', String, nullable=False),
-            Column('parent', Integer, ForeignKey('strategies.id'), nullable=False),
-            extend_existing=True
-            )
-
-    strategy_themes = Table('strategy_themes', meta,
-            Column('id', Integer, primary_key=True, autoincrement=True),
-            Column('refno', String, nullable=False),
-            Column('created_at', DateTime, server_default=func.now()),
-            Column('content', String, nullable=False),
-            Column('parent', Integer, ForeignKey('strategy_tactics.id'), nullable=False),
-            extend_existing=True
-            )
-
-    strategy_activities = Table('strategy_activities', meta,
-            Column('id', Integer, primary_key=True, autoincrement=True),
-            Column('refno', String, nullable=False),
-            Column('created_at', DateTime, server_default=func.now()),
-            Column('content', String, nullable=False),
-            Column('parent', Integer, ForeignKey('strategy_themes.id'), nullable=False),
-            extend_existing=True
-            )
-
-    meta.create_all(engine)
-
+from datetime import datetime
+from sqlalchemy.sql import func
+from main import db
+from models import Org
 
 def load_orgs():
     import pandas as pd
     data  = pd.read_excel('staff.xlsx', names=['id', 'name', 'parent', 'head'],  header=None)
-    orgs = meta.tables['orgs']
     for row in data.iterrows():
         idx, d = row
         parent = None if pd.isna(d['parent']) else int(d['parent'])
         head = None if pd.isna(d['head']) else d['head']
-        ins = orgs.insert().values(name=d['name'], head=head, parent=parent)
-        result = connect.execute(ins)
-        print(result.inserted_primary_key)
+        if not parent:
+            org = Org(name=d['name'], head=head, parent=parent)
+        else:
+            parent = Org.query.get(parent)
+            org = Org(name=d['name'], head=head, parent=parent)
+        db.session.add(org)
+        db.session.commit()
 
 
 def load_strategy():
