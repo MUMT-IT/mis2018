9bae5496c91458a5cb2662dde9cd2e4ba442e858
Likit Preeyanon <likit.pre@mahidol.edu>
2018-02-03 22:41:10 +0700

    reformatted the code
    added produce breeds model with a one-to-one relationship to the produce model

--- app/food/models.py
+++ app/food/models.py
@@ -2,11 +2,10 @@ from ..main import db
 from sqlalchemy.sql import func
 from datetime import datetime
 
-
 person_and_farm = db.Table('food_person_and_farm',
-        db.Column('person_id', db.Integer(), db.ForeignKey('food_persons.id')),
-        db.Column('farm_id', db.Integer(), db.ForeignKey('food_farms.id'))
-    )
+                           db.Column('person_id', db.Integer(), db.ForeignKey('food_persons.id')),
+                           db.Column('farm_id', db.Integer(), db.ForeignKey('food_farms.id'))
+                           )
 
 
 class Person(db.Model):
@@ -16,9 +15,9 @@ class Person(db.Model):
     lastname = db.Column(db.String(200), nullable=False)
     pid = db.Column(db.String(13), nullable=True)
     farms = db.relationship('Farm',
-                    secondary=person_and_farm,
-                    backref=db.backref('owners', lazy='dynamic'),
-                    lazy='dynamic')
+                            secondary=person_and_farm,
+                            backref=db.backref('owners', lazy='dynamic'),
+                            lazy='dynamic')
     created_at = db.Column(db.DateTime(), default=datetime.utcnow)
 
     def __str__(self):
@@ -29,27 +28,27 @@ class Farm(db.Model):
     __tablename__ = 'food_farms'
     id = db.Column(db.Integer(), primary_key=True)
     estimated_total_size = db.Column(
-            db.Float(asdecimal=True), nullable=True)
+        db.Float(asdecimal=True), nullable=True)
     estimated_leased_size = db.Column(
-            db.Float(asdecimal=True), nullable=True)
+        db.Float(asdecimal=True), nullable=True)
     estimated_owned_size = db.Column(
-            db.Float(asdecimal=True), nullable=True)
+        db.Float(asdecimal=True), nullable=True)
     village = db.Column(db.String(), nullable=True)
     street = db.Column(db.String(), nullable=True)
     agritype_id = db.Column('agritype', db.Integer(),
-            db.ForeignKey('food_agritype.id'))
+                            db.ForeignKey('food_agritype.id'))
     province_id = db.Column('province_id', db.Integer(),
-            db.ForeignKey('provinces.id'))
+                            db.ForeignKey('provinces.id'))
     district_id = db.Column('district_id', db.Integer(),
-            db.ForeignKey('districts.id'))
+                            db.ForeignKey('districts.id'))
     subdistrict_id = db.Column('subdistrict_id', db.Integer(),
-            db.ForeignKey('subdistricts.id'))
+                               db.ForeignKey('subdistricts.id'))
     created_at = db.Column(db.DateTime(), default=datetime.utcnow)
     sample_lots = db.relationship('SampleLot', backref=db.backref('farm'))
 
     def ref_id(self):
-        return u'{:04}-{:02}-{:02}-{:02}'.format(self.id,self.province_id,
-                self.district_id, self.subdistrict_id)
+        return u'{:04}-{:02}-{:02}-{:02}'.format(self.id, self.province_id,
+                                                 self.district_id, self.subdistrict_id)
 
 
 class AgriType(db.Model):
@@ -64,9 +63,9 @@ class SampleLot(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     collected_at = db.Column('collected_at', db.DateTime())
     registered_at = db.Column('registered_at',
-                        db.DateTime(), default=datetime.utcnow)
+                              db.DateTime(), default=datetime.utcnow)
     farm_id = db.Column('farm_id',
-                db.Integer(), db.ForeignKey('food_farms.id'))
+                        db.Integer(), db.ForeignKey('food_farms.id'))
     samples = db.relationship('Sample', backref=db.backref('lot'))
 
 
@@ -74,12 +73,24 @@ class Sample(db.Model):
     __tablename__ = 'food_samples'
     id = db.Column(db.Integer(), primary_key=True)
     lot_id = db.Column('lot_id',
-                db.Integer(), db.ForeignKey('food_sample_lots.id'))
+                       db.Integer(), db.ForeignKey('food_sample_lots.id'))
     produce_id = db.Column('produce_id',
-                db.Integer(), db.ForeignKey('food_produces.id'))
+                           db.Integer(), db.ForeignKey('food_produces.id'))
 
 
 class Produce(db.Model):
     __tablename__ = 'food_produces'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    produce_breed_id = db.Column('produce_breed_id',
+                                 db.Integer(),
+                                 db.ForeignKey('food_produce_breeds.id'))
+    breed = db.relationship('ProduceBreed',
+                            backref=db.backref('produce'),
+                            uselist=False)
+
+
+class ProduceBreed(db.Model):
+    __tablename__ = 'food_produce_breeds'
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(), nullable=False)
