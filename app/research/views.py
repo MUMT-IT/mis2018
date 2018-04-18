0014c60d8a20acee780cfc5c22439709c0d6863f
Likit Preeyanon <likit.pre@mahidol.edu>
2018-04-18 07:01:44 +0700

    scopus data now can be downloaded for a single or multiple authors
    refactored code a bit for effeciency

--- app/research/views.py
+++ app/research/views.py
@@ -1,5 +1,6 @@
 import os
 import datetime
+from collections import namedtuple
 
 import requests
 from flask import request, render_template, jsonify
@@ -7,6 +8,8 @@ from . import researchbp as research
 from models import APIKey, ResearchPub
 from ..staff.models import StaffAccount
 from ..main import db
+
+Author = namedtuple('Author', ['email', 'firstname', 'lastname'])
 
 usr = os.environ.get('PROXY_USER')
 pwd = os.environ.get('PROXY_PASSWORD')
@@ -18,26 +21,12 @@ def get_key(service):
     api_key = db.session.query(APIKey).filter(APIKey.service==service).first().key
     return api_key
 
-
-@research.route('/scopus/retrieve')
-def retrieve_scopus_data():
-    author_email = request.args.get('author_email', 'likit.pre')
-    author = db.session.query(StaffAccount).filter(StaffAccount.email==author_email).first()
-    if not author:
-        return 'No author with the given email found in the database.'
-    year = request.args.get('year', 2015)
-    if year is None:
-        year = datetime.datetime.today().year
-
-    firstname = author.personal_info.en_firstname.lower()
-    lastname = author.personal_info.en_lastname.lower()
-
-    api_key = get_key('SCOPUS')
+def download_scopus_pub_by_author(author, api_key, year):
     # TODO: need to figure out the proxy IP and port
     # proxy_dict = {'http': 'http://{}:{}@proxy.mahidol/'.format(usr,pwd) }
 
     query = 'AUTHLASTNAME("%s") AUTHFIRST("%s") DOCTYPE(ar) PUBYEAR AFT %d' \
-            % (lastname, firstname[0], year)
+            % (author.lastname, author.firstname[0], year)
     params = {'apiKey': api_key, 'query': query, 'httpAccept': 'application/json',
             'view': 'COMPLETE', 'field': 'dc:title,dc:identifier,author'}
     url = 'http://api.elsevier.com/content/search/scopus'
@@ -46,9 +35,9 @@ def retrieve_scopus_data():
     r = requests.get(url, params=params).json()
     for article in r['search-results']['entry']:
         if 'author' in article:
-            for author in article['author']:
+            for au in article['author']:
                 try:
-                    if firstname.lower() == author['given-name'].lower():
+                    if author.firstname.lower() == au['given-name'].lower():
                         authors_articles_sid.append(
                             article['dc:identifier'].replace('SCOPUS_ID:', ''))
                 except AttributeError:
@@ -65,8 +54,38 @@ def retrieve_scopus_data():
                         'view': 'FULL'}
                 url = 'http://api.elsevier.com/content/abstract/scopus_id/' + scopus_id
                 r = requests.get(url, params=params).json()
-                rp = ResearchPub(author_id=author_email, uid=scopus_id, data=r, indexed_db='scopus')
+                rp = ResearchPub(author_id=author.email, uid=scopus_id, data=r, indexed_db='scopus')
                 db.session.add(rp)
                 db.session.commit()
                 new_articles.append(r)
-    return jsonify({'total_articles': total_results, 'new_articles': new_articles })
+    return {'author': author, 'total': total_results, 'new_articles': new_articles}
+
+
+@research.route('/scopus/retrieve')
+def retrieve_scopus_data():
+    authors = []
+
+    year = request.args.get('year', 2015)
+    if year is None:
+        year = datetime.datetime.today().year
+
+    author_email = request.args.get('author_email', None)
+    if author_email is not None:
+        author = db.session.query(StaffAccount).filter(StaffAccount.email==author_email).first()
+        if not author:
+            return 'No author with the given email found in the database.'
+        firstname = author.personal_info.en_firstname.lower()
+        lastname = author.personal_info.en_lastname.lower()
+        authors.append(Author(author.email, firstname, lastname))
+    else:
+        for author in db.session.query(StaffAccount):
+            firstname = author.personal_info.en_firstname.lower()
+            lastname = author.personal_info.en_lastname.lower()
+            authors.append(Author(author.email, firstname, lastname))
+    api_key = get_key('SCOPUS')
+
+    results = []
+    for author in authors:
+        res = download_scopus_pub_by_author(author, api_key, year)
+        results.append(res)
+    return jsonify(results)
