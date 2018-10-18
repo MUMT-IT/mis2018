dd35aee1ea7631dafc462c74cfa4242d542b5f10
Likit Preeyanon <likit.pre@mahidol.edu>
2018-10-18 15:31:51 +0700

    added room reservation model

--- app/room_scheduler/models.py
+++ app/room_scheduler/models.py
@@ -1,4 +1,5 @@
 from app.main import db, ma
+from sqlalchemy.sql import func
 
 
 class RoomType(db.Model):
@@ -30,3 +31,23 @@ class RoomResource(db.Model):
     availability_id = db.Column('availability_id',
                                 db.ForeignKey('scheduler_room_avails.id'))
     type_id = db.Column('type_id', db.ForeignKey('scheduler_room_types.id'))
+    reservations = db.relationship('RoomEvent', backref='room')
+
+
+class RoomEvent(db.Model):
+    __tablename__ = 'scheduler_room_reservations'
+    id = db.Column('id', db.Integer(), primary_key=True,
+                   autoincrement=True)
+    room_id = db.Column('room_id', db.ForeignKey('scheduler_room_resources.id'),
+                        nullable=False)
+    title = db.Column('title', db.String(255), nullable=False)
+    start = db.Column('start', db.DateTime(), nullable=False)
+    end = db.Column('end', db.DateTime(), nullable=False)
+    occupancy = db.Column('occupancy', db.Integer())
+    refreshment = db.Column('refreshment', db.Integer(), default=0)
+    request = db.Column('request', db.Text())
+    approved = db.Column('approved', db.Boolean(), default=True)
+    required_permission = db.Column('required_permission', db.Boolean(), default=False)
+    created_at = db.Column('created_at', db.DateTime(), server_default=func.now())
+    created_by = db.Column('created_by', db.ForeignKey('staff_account.id'))
+    approved_by = db.Column('approved_by', db.ForeignKey('staff_account.id'))
