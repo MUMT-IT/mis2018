aa86c36b37f01b9c3049f9564bdb455bdd2f79ba
Likit Preeyanon <likit.pre@mahidol.edu>
2019-01-04 15:37:38 +0700

    updated the models by adding Th and En name and the views accordingly

--- app/asset/models.py
+++ app/asset/models.py
@@ -3,7 +3,8 @@ from ..main import db, ma
 class AssetItem(db.Model):
     __tablename__ = 'asset_items'
     id = db.Column('id', db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column('desc', db.String(255), nullable=False)
+    en_name = db.Column('en_desc', db.String(255), nullable=False)
+    th_name = db.Column('th_desc', db.String(255), nullable=False)
     location = db.Column('location', db.String(255))
     value = db.Column('cost', db.Numeric())
     purchased_at = db.Column('purchased_at', db.Date())
@@ -13,3 +14,4 @@ class AssetItem(db.Model):
     type_id = db.Column('type_id', db.String(3), nullable=False)
     desc_id = db.Column('desc_id', db.String(4), nullable=False)
     room_id = db.Column('room_id', db.ForeignKey('scheduler_room_resources.id'))
+    reservable = db.Column('reservable', db.Boolean, default=False)
--- app/asset/views.py
+++ app/asset/views.py
@@ -8,11 +8,13 @@ def get_items():
     data = []
     duplicates = set()
     for item in AssetItem.query.all():
-        if item.name not in duplicates and \
-                not item.room_id:
+        if item.th_name not in duplicates and \
+                not item.room_id \
+                and item.reservable:
             data.append({
-                'name': item.name,
+                'th_name': item.th_name,
+                'en_name': item.en_name
             })
-            duplicates.add(item.name)
+            duplicates.add(item.th_name)
 
     return jsonify(data)
