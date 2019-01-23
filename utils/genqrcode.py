bb287e9b1423a0d7686b905f87b0c021efde90c9
Likit Preeyanon <likit.pre@mahidol.edu>
2019-01-23 13:39:40 +0700

    refactored the script to allow user-defined folder name and input file

--- utils/genqrcode.py
+++ utils/genqrcode.py
@@ -1,19 +1,32 @@
 import sys
+import os
 import pyqrcode
 from jinja2 import Environment, FileSystemLoader
 from pandas import read_excel
 
-data = read_excel(sys.argv[1], header=None,
-            names=['refno', 'uid', 'title', 'firstname', 'lastname'])
 
-students = []
-for _, student in data.iterrows():
-    qrcode = pyqrcode.create(student['uid'])
-    qrcode.png('qrimages/{}.png'.format(student['uid']), scale=5)
-    students.append(student)
+def main(filepath, folder):
+    if not os.path.exists(folder):
+        os.mkdir(folder)
 
-env = Environment(loader=FileSystemLoader('.'))
-template = env.get_template('qrcode-template.html')
-output = open('qrcode.html', 'w')
-output.write(template.render(students=students).encode('utf-8'))
-output.close()
+    data = read_excel(sys.argv[1], header=None,
+                names=['refno', 'uid', 'title', 'firstname', 'lastname'])
+    students = []
+    for _, student in data.iterrows():
+        qrcode = pyqrcode.create(student['uid'])
+        qrcode.png('{}/{}.png'.format(folder, student['uid']), scale=6)
+        students.append(student)
+
+    env = Environment(loader=FileSystemLoader('.'))
+    template = env.get_template('qrcode-template.html')
+    output = open('{}.html'.format(folder), 'w')
+    output.write(template.render(students=students, folder=folder).encode('utf-8'))
+    output.close()
+
+if __name__ == '__main__':
+    if len(sys.argv) == 3:
+        filepath = sys.argv[1]
+        folder = sys.argv[2]
+        main(filepath, folder)
+    else:
+        raise SystemError
