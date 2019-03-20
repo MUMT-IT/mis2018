f2920a21d6d09c9e3c270d860538aa7576b47c00
Likit Preeyanon <likit.pre@mahidol.edu>
2019-03-20 12:04:08 +0700

    edited the edit record view and page

--- app/comhealth/views.py
+++ app/comhealth/views.py
@@ -27,28 +27,9 @@ def display_service_customers(service_id):
 @comhealth.route('/checkin/<int:record_id>')
 def edit_record(record_id):
     record = ComHealthRecord.query.get(record_id)
-    test_profiles = {}
-    tests = ComHealthTest.query.all()
-
-    for p in ComHealthTestProfile.query.all():
-        test_profiles[p.name] = p.tests
-
-    ordered_tests = []
-    ordered_profiled_tests = []
-
-    for p in record.test_profile_items:
-        ordered_profiled_tests += p.profile.tests
-
-    for item in record.test_items:
-        if item not in ordered_profiled_tests:
-            ordered_tests.append(item.test)
 
     return render_template('comhealth/edit_record.html',
-                           record=record,
-                           test_profiles=test_profiles,
-                           ordered_tests=set(ordered_tests),
-                           ordered_profiled_tests=ordered_profiled_tests,
-                           tests=tests)
+                           record=record)
 
 
 @comhealth.route('/tests')
