b7928d59ab96090a4017cb0c2fed0d34ff7397ef
Likit Preeyanon <likit.pre@mahidol.edu>
2019-03-29 12:28:28 +0700

    replaced random key with a key from the environmental variable

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = os.urandom(16)
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
