40622ee94a8350eecbb7541b307fdd3171d5b11c
Likit Preeyanon <likit.pre@mahidol.edu>
2019-07-03 10:58:31 +0700

    added basedir
    updated code according to change in config.py
    
    fixed some pep8 violations

--- app/main.py
+++ app/main.py
@@ -12,7 +12,11 @@ from flask_login import LoginManager
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from flask_wtf.csrf import CSRFProtect
-from app.config import config
+from config import SETTINGS
+
+from app.staff.models import StaffAccount
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -25,9 +29,13 @@ admin = Admin()
 
 dbutils = AppGroup('dbutils')
 
-def create_app(config_name='default'):
+
+def create_app(config_setting='default'):
+    '''Create app based on the config setting
+    '''
+
     app = Flask(__name__)
-    app.config.from_object(config[config_name])
+    app.config.from_object(SETTINGS[config_setting])
     db.init_app(app)
     ma.init_app(app)
     login.init_app(app)
@@ -36,10 +44,8 @@ def create_app(config_name='default'):
     admin.init_app(app)
     return app
 
-config_name = os.environ.get('FLASK_CONFIG', 'default')
-app = create_app(config_name)
-
-from staff.models import StaffAccount
+config_setting = os.environ.get('FLASK_CONFIG', 'default')
+app = create_app(config_setting)
 
 @login.user_loader
 def load_user(user_id):
@@ -58,37 +64,37 @@ def index():
 with app.open_resource(os.environ.get('JSON_KEYFILE')) as jk:
     json_keyfile = json.load(jk)
 
-from kpi import kpibp as kpi_blueprint
+from app.kpi import kpibp as kpi_blueprint
 app.register_blueprint(kpi_blueprint, url_prefix='/kpi')
 
-from studs import studbp as stud_blueprint
+from app.studs import studbp as stud_blueprint
 app.register_blueprint(stud_blueprint, url_prefix='/stud')
 
-from food import foodbp as food_blueprint
+from app.food import foodbp as food_blueprint
 app.register_blueprint(food_blueprint, url_prefix='/food')
-from food.models import Person, Farm, Produce
+from app.food.models import Person, Farm, Produce
 admin.add_views(ModelView(Person, db.session, category='Food'))
 admin.add_views(ModelView(Farm, db.session, category='Food'))
 admin.add_views(ModelView(Produce, db.session, category='Food'))
 
-from research import researchbp as research_blueprint
+from app.research import researchbp as research_blueprint
 app.register_blueprint(research_blueprint, url_prefix='/research')
 from app.research.models import ResearchPub
 
-from staff import staffbp as staff_blueprint
+from app.staff import staffbp as staff_blueprint
 app.register_blueprint(staff_blueprint, url_prefix='/staff')
 
-from staff.models import StaffAccount, StaffPersonalInfo
+from app.staff.models import StaffAccount, StaffPersonalInfo
 admin.add_views(ModelView(StaffAccount, db.session, category='Staff'))
 admin.add_views(ModelView(StaffPersonalInfo, db.session, category='Staff'))
 
-from room_scheduler import roombp as room_blueprint
+from app.room_scheduler import roombp as room_blueprint
 app.register_blueprint(room_blueprint, url_prefix='/room')
-from room_scheduler.models import *
+from app.room_scheduler.models import *
 
-from vehicle_scheduler import vehiclebp as vehicle_blueprint
+from app.vehicle_scheduler import vehiclebp as vehicle_blueprint
 app.register_blueprint(vehicle_blueprint, url_prefix='/vehicle')
-from vehicle_scheduler.models import *
+from app.vehicle_scheduler.models import *
 
 admin.add_views(ModelView(RoomResource, db.session, category='Physicals'))
 admin.add_views(ModelView(RoomEvent, db.session, category='Physicals'))
@@ -100,19 +106,19 @@ admin.add_view(ModelView(VehicleResource, db.session, category='Physicals'))
 admin.add_view(ModelView(VehicleAvailability, db.session, category='Physicals'))
 admin.add_view(ModelView(VehicleType, db.session, category='Physicals'))
 
-from auth import authbp as auth_blueprint
+from app.auth import authbp as auth_blueprint
 app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
-from models import (Student, Class, ClassCheckIn,
+from app.models import (Student, Class, ClassCheckIn,
                         Org, Mission, IOCode, CostCenter,
                         StudentCheckInRecord)
-import database
+import app.database
 
 
 class StudentCheckInAdminModel(ModelView):
     can_create = True
-    form_columns = ('id','classchk', 'check_in_time', 'check_in_status', 'elapsed_mins')
-    column_list = ('id','classchk', 'check_in_time', 'check_in_status', 'elapsed_mins')
+    form_columns = ('id', 'classchk', 'check_in_time', 'check_in_status', 'elapsed_mins')
+    column_list = ('id', 'classchk', 'check_in_time', 'check_in_status', 'elapsed_mins')
 
 
 admin.add_view(ModelView(Student, db.session, category='Student Affairs'))
@@ -145,17 +151,17 @@ class CostCenterAdminModel(ModelView):
 
 admin.add_view(CostCenterAdminModel(CostCenter, db.session, category='Finance'))
 
-from lisedu import lisedu as lis_blueprint
+from app.lisedu import lisedu as lis_blueprint
 app.register_blueprint(lis_blueprint, url_prefix='/lis')
-from lisedu.models import *
+from app.lisedu.models import *
 
 
-from chemdb import chemdbbp as chemdb_blueprint
-import chemdb.models
+from app.chemdb import chemdbbp as chemdb_blueprint
+import app.chemdb.models
 app.register_blueprint(chemdb_blueprint, url_prefix='/chemdb')
 
-from comhealth import comhealth as comhealth_blueprint
-from comhealth.models import *
+from app.comhealth import comhealth as comhealth_blueprint
+from app.comhealth.models import *
 app.register_blueprint(comhealth_blueprint, url_prefix='/comhealth')
 admin.add_view(ModelView(ComHealthTest, db.session, category='Com Health'))
 admin.add_view(ModelView(ComHealthTestProfile, db.session, category='Com Health'))
@@ -167,7 +173,6 @@ admin.add_view(ModelView(ComHealthTestProfileItem, db.session, category='Com Hea
 admin.add_view(ModelView(ComHealthService, db.session, category='Com Health'))
 admin.add_view(ModelView(ComHealthTestGroup, db.session, category='Com Health'))
 admin.add_view(ModelView(ComHealthContainer, db.session, category='Com Health'))
-
 
 
 @app.cli.command()
