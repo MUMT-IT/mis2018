a4b73c58167fcb2724eecfeee68dc3a89e0423ac
Likit Preeyanon <likit.pre@mahidol.edu>
2019-10-16 16:11:49 +0700

    updated the form for vehicle bookings, views and models

--- app/vehicle_scheduler/models.py
+++ app/vehicle_scheduler/models.py
@@ -40,3 +40,38 @@ class VehicleResource(db.Model):
                                 db.ForeignKey('scheduler_vehicle_avails.id'))
     type_id = db.Column('type_id', db.ForeignKey('scheduler_vehicle_types.id'))
     # reservations = db.relationship('RoomEvent', backref='room')
+
+
+class VehicleBooking(db.Model):
+    __tablename__ = 'scheduler_vehicle_bookings'
+    id = db.Column('id', db.Integer(), primary_key=True,
+                   autoincrement=True)
+    vehicle_id = db.Column('vehicle_id',
+                        db.ForeignKey('scheduler_vehicle_resources.id'),
+                        nullable=False)
+    vehicle = db.relationship('VehicleResource', backref=db.backref('bookings'))
+    title = db.Column('title', db.String(255), nullable=False)
+    init_milage = db.Column('init_milage', db.Integer, nullable=True)
+    end_milage = db.Column('end_milage', db.Integer, nullable=True)
+    distance = db.Column('distance', db.Integer, nullable=True)
+    init_location = db.Column('init_location', db.String(255), nullable=True)
+    destination = db.Column('destination', db.String(255), nullable=True)
+    start = db.Column('start', db.DateTime(timezone=True), nullable=False)
+    end = db.Column('end', db.DateTime(timezone=True), nullable=False)
+    iocode_id = db.Column('iocode_id', db.ForeignKey('iocodes.id'))
+    iocode = db.relationship('IOCode', backref=db.backref('vehicle_bookings'))
+    org_id = db.Column('org_id', db.ForeignKey('orgs.id'))
+    org = db.relationship('Org', backref=db.backref('vehicle_bookings'))
+    num_passengers = db.Column('num_passengers', db.Integer())
+    approved = db.Column('approved', db.Boolean(), default=False)
+    created_at = db.Column('created_at', db.DateTime(timezone=True), server_default=func.now())
+    created_by = db.Column('created_by', db.ForeignKey('staff_account.id'))
+    updated_at = db.Column('updated_at', db.DateTime(timezone=True), server_default=None)
+    updated_by = db.Column('updated_by', db.ForeignKey('staff_account.id'))
+    cancelled_at = db.Column('cancelled_at', db.DateTime(timezone=True), server_default=None)
+    cancelled_by = db.Column('cancelled_by', db.ForeignKey('staff_account.id'))
+    approved_by = db.Column('approved_by', db.ForeignKey('staff_account.id'))
+    approved_at = db.Column('approved_at', db.DateTime(timezone=True), server_default=None)
+    desc = db.Column('desc', db.Text())
+    google_event_id = db.Column('google_event_id', db.String(64))
+    google_calendar_id = db.Column('google_calendar_id', db.String(255))
--- app/vehicle_scheduler/views.py
+++ app/vehicle_scheduler/views.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 from . import vehiclebp as vehicle
 import pytz
+from sqlalchemy.exc import SQLAlchemyError
 import google.auth
 from datetime import datetime
 from googleapiclient.discovery import build
 from flask import jsonify, render_template, redirect, flash, url_for, request
 from models import *
-from ..models import Org
+from ..models import Org, IOCode
+
 
 CALENDAR_ID = 'anatjgngk7bcv9kte15p38l7mg@group.calendar.google.com'
 
@@ -110,16 +112,30 @@ def vehicle_list():
 def vehicle_reserve(license):
     if request.method == 'POST':
         license = request.form.get('license', None)
-        location = request.form.get('location', None)
+        iocode_id = request.form.get('iocode', None)
+        if iocode_id:
+            iocode = IOCode.query.get(iocode_id) 
+        else:
+            iocode = None
+
+        destination = request.form.get('destination', None)
         desc = request.form.get('desc', None)
+        num_passengers = request.form.get('num_passengers', 0)
         distance = request.form.get('distance', None)
         startdate = request.form.get('startdate', None)
         starttime = request.form.get('starttime', None)
         enddate = request.form.get('enddate', None)
         endtime = request.form.get('endtime', None)
         title = request.form.get('title', None)
-        org = request.form.get('org', None)
+        org_id = request.form.get('org', None)
+        if org_id:
+            org = Org.query.get(int(org_id))
+        else:
+            org = None
         vehicle = VehicleResource.query.filter_by(license=license).first()
+        if not vehicle:
+            flash('Vehicle with license plate="{}" not found.'.format(license))
+            return redirect(url_for('vehicle.index'))
         tz = pytz.timezone('Asia/Bangkok')
         if startdate and starttime:
             startdatetime = datetime.strptime(
@@ -141,7 +157,7 @@ def vehicle_reserve(license):
             else:
                 event = {
                     'summary': title,
-                    'location': location,
+                    'location': destination,
                     'sendUpdates': 'all',
                     'status': 'tentative',
                     'description': desc,
@@ -156,23 +172,50 @@ def vehicle_reserve(license):
                     'extendedProperties': {
                         'private': {
                             'license': license,
-                            'location': location,
+                            'destination': destination,
                             'distance': distance,
-                            'orgId': org
+                            'orgId': org_id,
+                            'iocode': iocode_id,
                         }
                     }
                 }
                 print(event)
+                reservation = VehicleBooking(
+                    vehicle=vehicle,
+                    title=title,
+                    start=startdatetime,
+                    end=enddatetime,
+                    destination=destination,
+                    distance=distance,
+                    iocode=iocode,
+                    org=org,
+                    num_passengers=num_passengers,
+                    approved=False,
+                    desc=desc,
+                )
                 credentials, project_id = google.auth.default()
                 scoped_credentials = credentials.with_scopes([
                     'https://www.googleapis.com/auth/calendar',
                     'https://www.googleapis.com/auth/calendar.events'
                 ])
-                calendar_service = build('calendar', 'v3', credentials=scoped_credentials)
+                calendar_service = build('calendar', 'v3',
+                                            credentials=scoped_credentials)
                 event = calendar_service.events().insert(
                     calendarId=CALENDAR_ID,
                     body=event).execute()
-                flash('Reservation has been made. {}'.format(event.get('htmlLink')))
+                if event:
+                    reservation.google_event_id = event.get('id')
+                    reservation.google_calendar_id = CALENDAR_ID
+                    try:
+                        db.session.add(reservation)
+                        db.session.commit()
+                    except SQLAlchemyError as e:
+                        db.session.rollback()
+                        flash('Reservation failed; cannot save data to the database.')
+                    else:
+                        flash('Reservation has been made. {}'.format(event.get('id')))
+                else:
+                    flash('Reservation failed; cannot save data to Google calendar.')
                 return redirect(url_for('vehicle.index'))
     if license:
         vehicle = VehicleResource.query.filter_by(license=license).first()
