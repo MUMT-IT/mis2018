8f9daa49dae6b0e5269747479003be94c76429f4
Likit Preeyanon <likit.pre@mahidol.edu>
2020-03-04 14:24:53 +0700

    added merged migration version

---
+++ migrations/versions/43d55755cca4_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 43d55755cca4
+Revises: 2d328d2c799c, 0c9dcd620644, f0113e6de149
+Create Date: 2020-03-04 13:51:22.336355
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '43d55755cca4'
+down_revision = ('2d328d2c799c', '0c9dcd620644', 'f0113e6de149')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
