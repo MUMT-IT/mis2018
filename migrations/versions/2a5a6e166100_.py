fb4acdcdb3684d53df967014d98d5035bdd50368
napatsakorn <napatsakorn.pis@mahidol.ac.th>
2020-06-30 10:07:07 +0700

    added merge head

---
+++ migrations/versions/2a5a6e166100_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 2a5a6e166100
+Revises: f4ad2ec4ef04, d84377653786
+Create Date: 2020-06-24 08:56:50.860994
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2a5a6e166100'
+down_revision = ('f4ad2ec4ef04', 'd84377653786')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
