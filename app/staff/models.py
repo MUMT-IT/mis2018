ebf56f3348ea33663acd1cd51f79afda702cbf65
napatsakorn <napatsakorn.pis@mahidol.ac.th>
2020-08-03 13:31:40 +0700

    edited column approval_comment and checked_at (move from table WFHCheckedJob tot table WFHApproval) and create function check_comment in table WFH checkwdJob

--- app/staff/models.py
+++ app/staff/models.py
@@ -347,6 +347,8 @@ class StaffWorkFromHomeApproval(db.Model):
     approver_id = db.Column('approver_id', db.ForeignKey('staff_work_from_home_approvers.id'))
     is_approved = db.Column('is_approved', db.Boolean(), default=False)
     updated_at = db.Column('updated_at', db.DateTime(timezone=True))
+    approval_comment = db.Column('approval_comment', db.String())
+    checked_at = db.Column('check_at', db.DateTime(timezone=True))
     request = db.relationship('StaffWorkFromHomeRequest', backref=db.backref('wfh_approvals'))
     approver = db.relationship('StaffWorkFromHomeApprover',
                                backref=db.backref('wfh_approved_requests'))
@@ -356,15 +358,14 @@ class StaffWorkFromHomeCheckedJob(db.Model):
     __tablename__ = 'staff_work_from_home_checked_job'
     id = db.Column('id', db.Integer(), primary_key=True, autoincrement=True)
     overall_result = db.Column('overall_result', db.String())
-    approval_comment = db.Column('approval_comment', db.String())
     request_id = db.Column('request_id', db.ForeignKey('staff_work_from_home_requests.id'))
-    approver_id = db.Column('approver_id', db.ForeignKey('staff_account.id'))
     finished_at = db.Column('finish_at', db.DateTime(timezone=True))
-    checked_at = db.Column('check_at', db.DateTime(timezone=True))
-    wfh_request_id = db.relationship('StaffWorkFromHomeRequest',
-                            backref=db.backref('wfh_request'))
-    approval_account = db.relationship('StaffAccount',
-                              foreign_keys=[approver_id])
+    request = db.relationship('StaffWorkFromHomeRequest', backref=db.backref('checked_jobs'))
+
+    def check_comment(self, account_id):
+        for approval in self.request.wfh_approvals:
+            if approval.approver.account.id == account_id:
+                return approval.approval_comment
 
 
 class StaffWorkFromHomeRequestSchema(ma.ModelSchema):
