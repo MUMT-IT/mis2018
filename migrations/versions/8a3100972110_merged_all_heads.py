ffa25a93d1d0a0ea15d640c422733f57c9396515
Likit Preeyanon <likit.pre@mahidol.edu>
2021-01-15 12:05:24 +0700

    added merge heads

---
+++ migrations/versions/8a3100972110_merged_all_heads.py
@@ -0,0 +1,24 @@
+"""merged all heads
+
+Revision ID: 8a3100972110
+Revises: 399a89b918e2, a3fbc888f472
+Create Date: 2021-01-15 11:56:52.061952
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8a3100972110'
+down_revision = ('399a89b918e2', 'a3fbc888f472')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
