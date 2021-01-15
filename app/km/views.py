d7bd969d38b5068b888aa24d174aa40545e8765f
Likit Preeyanon <likit.pre@mahidol.edu>
2021-01-15 14:34:23 +0700

    added an index page and a new topic page. added a navbar. added a view for adding new topic.

--- app/km/views.py
+++ app/km/views.py
@@ -1,6 +1,15 @@
+from flask import render_template, url_for, redirect
+from flask_login import current_user, login_required
 from . import km_bp as km
 
 
 @km.route('/')
+@login_required
 def index():
-    return 'Recently Added Topics'
+    return render_template('km/index.html')
+
+
+@km.route('/topics/add')
+@login_required
+def add_topic():
+    return render_template('km/add.html')
