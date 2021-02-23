d8830218932b8978e415c796e08256423939b29f
Likit Preeyanon <likit.pre@mahidol.edu>
2021-02-23 23:47:23 +0700

    added pages/views for display services and adding a new one

--- app/health_service_scheduler/forms.py
+++ app/health_service_scheduler/forms.py
@@ -1,6 +1,7 @@
 from app.main import db
 from wtforms_alchemy import model_form_factory, QuerySelectField
-from wtforms.widgets import ListWidget, CheckboxInput
+from wtforms.validators import Required
+from wtforms.widgets import Select
 from flask_wtf import FlaskForm
 from .models import *
 
@@ -22,3 +23,18 @@ class ServiceSiteForm(ModelForm):
 class ServiceForm(ModelForm):
     class Meta:
         model = HealthServiceService
+
+
+class ServiceSlotForm(ModelForm):
+    class Meta:
+        model = HealthServiceTimeSlot
+    service = QuerySelectField(label='Service',
+                            validators=[Required()], widget=Select,
+                            query_factory=lambda: HealthServiceService.query.all(),
+                            get_label=lambda x: x.name,
+                            blank_text='Please specify the service')
+    site = QuerySelectField(label='Site',
+                            validators=[Required()], widget=Select,
+                            query_factory=lambda: HealthServiceSite.query.all(),
+                            get_label=lambda x: x.name,
+                            blank_text='Please specify the site')
--- app/health_service_scheduler/views.py
+++ app/health_service_scheduler/views.py
@@ -31,3 +31,26 @@ def add_site():
 def get_sites():
     sites = HealthServiceSite.query.all()
     return render_template('health_service_scheduler/sites.html', sites=sites)
+
+
+@hs.route('/services')
+def get_services():
+    services = HealthServiceService.query.all()
+    return render_template('health_service_scheduler/services.html', services=services)
+
+
+@hs.route('/services/add', methods=['GET', 'POST'])
+def add_service():
+    form = ServiceForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            service = HealthServiceService()
+            form.populate_obj(service)
+            db.session.add(service)
+            db.session.commit()
+            flash(u'New service has been added.', 'success')
+            return redirect(url_for('health_service_scheduler.get_services'))
+        else:
+            for field, err in form.errors:
+                flash(u'{}:{}'.format(field, err), 'danger')
+    return render_template('health_service_scheduler/service_form.html', form=form)
