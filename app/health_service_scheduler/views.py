d2d920b7e02289c474aa121ed110a777211ee71c
Likit Preeyanon <likit.pre@mahidol.edu>
2021-02-23 23:08:50 +0700

    added code to save data from the form

--- app/health_service_scheduler/views.py
+++ app/health_service_scheduler/views.py
@@ -1,4 +1,5 @@
-from flask import render_template
+from flask import render_template, request, flash, redirect, url_for
+from models import *
 from forms import *
 
 from . import health_service_blueprint as hs
@@ -9,9 +10,20 @@ def index():
     return render_template('health_service_scheduler/index.html')
 
 
-@hs.route('/sites/add')
+@hs.route('/sites/add', methods=['GET', 'POST'])
 def add_site():
     form = ServiceSiteForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            site = HealthServiceSite()
+            form.populate_obj(site)
+            db.session.add(site)
+            db.session.commit()
+            flash(u'New site has been added.', 'success')
+            return redirect(url_for('health_service_scheduler.get_sites'))
+        else:
+            for field, err in form.errors:
+                flash(u'{}:{}'.format(field, err), 'danger')
     return render_template('health_service_scheduler/site_form.html', form=form)
 
 
