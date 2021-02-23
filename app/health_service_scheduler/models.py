63183955fe7ebc0f63ed2cc196c279e4379cb876
Likit Preeyanon <likit.pre@mahidol.edu>
2021-02-23 23:00:58 +0700

    added models to health service app

--- app/health_service_scheduler/models.py
+++ app/health_service_scheduler/models.py
@@ -1,22 +1,23 @@
+# -*- coding:utf-8 -*-
 from app.main import db
 
 
-class HeathServiceSite(db.Model):
-    __table__ = 'health_service_sites'
+class HealthServiceSite(db.Model):
+    __tablename__ = 'health_service_sites'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(255), nullable=False, info={'label': 'Site Name'})
-    lat = db.Column(db.Float())
-    lon = db.Column(db.Float())
+    lat = db.Column(db.Float(), info={'label': 'Latitude'})
+    lon = db.Column(db.Float(), info={'label': 'Longitude'})
 
 
 class HealthServiceService(db.Model):
-    __table__ = 'health_service_services'
+    __tablename__ = 'health_service_services'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(255), nullable=False, info={'label': 'Service'})
 
 
 class HealthServiceTimeSlot(db.Model):
-    __table__ = 'health_service_timeslots'
+    __tablename__ = 'health_service_timeslots'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     start = db.Column('start', db.DateTime(timezone=True), nullable=False, info={'label': u'เริ่ม'})
     end = db.Column('end', db.DateTime(timezone=True), nullable=False, info={'label': u'สิ้นสุด'})
@@ -24,13 +25,13 @@ class HealthServiceTimeSlot(db.Model):
 
 
 class HealthServiceBooking(db.Model):
-    __table__ = 'health_service_timeslots'
+    __tablename__ = 'health_service_bookings'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     slot_id = db.Column(db.ForeignKey('health_service_timeslots.id'))
     service_id = db.Column(db.ForeignKey('health_service_services.id'))
     site_id = db.Column(db.ForeignKey('health_service_sites.id'))
     slot = db.relationship(HealthServiceTimeSlot, backref=db.backref('bookings'))
-    site = db.relationship(HeathServiceSite, backref=db.backref('bookings'))
+    site = db.relationship(HealthServiceSite, backref=db.backref('bookings'))
     service = db.relationship(HealthServiceService, backref=db.backref('bookings'))
     cancelled = db.Column(db.DateTime(timezone=True))
     created_at = db.Column(db.DateTime(timezone=True))
