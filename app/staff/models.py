cdda9ae8ac87f58df19ee85e1291dfdf33e25882
napatsakorn <napatsakorn.pis@mahidol.ac.th>
2021-04-08 16:01:23 +0700

    added backreference from table staff_leave_approver to table staff_account

--- app/staff/models.py
+++ app/staff/models.py
@@ -333,8 +333,8 @@ class StaffLeaveApprover(db.Model):
     staff_account_id = db.Column('staff_account_id', db.ForeignKey('staff_account.id'))
     approver_account_id = db.Column('approver_account_id', db.ForeignKey('staff_account.id'))
     is_active = db.Column('is_active', db.Boolean(), default=True)
-    requester = db.relationship('StaffAccount', backref=db.backref('leave_approvers'), foreign_keys=[staff_account_id])
-    account = db.relationship('StaffAccount', foreign_keys=[approver_account_id])
+    requester = db.relationship('StaffAccount', backref=db.backref('leave_requesters'), foreign_keys=[staff_account_id])
+    account = db.relationship('StaffAccount', backref=db.backref('leave_approvers'), foreign_keys=[approver_account_id])
     notified_by_line = db.Column('notified_by_line', db.Boolean(), default=True)
 
     def __str__(self):
