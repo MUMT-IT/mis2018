3d22aae17b22b815e6454c60a3d02cc3f956ea17
Likit Preeyanon <likit.pre@mahidol.edu>
2021-05-18 22:45:25 +0700

    added some forms, views and pages for an admin

---
+++ app/doc_circulation/forms.py
@@ -0,0 +1,31 @@
+from app.main import db
+from flask_wtf import FlaskForm
+from wtforms import FileField
+from wtforms_alchemy import (model_form_factory, QuerySelectField)
+from wtforms_components import DateTimeField
+from wtforms.widgets import Select
+from .models import *
+
+
+BaseModelForm = model_form_factory(FlaskForm)
+
+
+class ModelForm(BaseModelForm):
+    @classmethod
+    def get_session(self):
+        return db.session
+
+
+class RoundForm(ModelForm):
+    class Meta:
+        model = DocRound
+
+
+class DocumentForm(ModelForm):
+    class Meta:
+        model = DocDocument
+
+    category = QuerySelectField('Category', query_factory=lambda: DocCategory.query.all(),
+                                get_label='name', blank_text='Select category..', allow_blank=False)
+
+    upload = FileField('File Upload')
--- app/doc_circulation/views.py
+++ app/doc_circulation/views.py
@@ -1,7 +1,9 @@
+# -*- coding:utf-8 -*-
 from . import docbp
 from flask_login import current_user
-from flask import render_template, url_for, request
+from flask import render_template, url_for, request, flash, redirect
 from models import *
+from forms import *
 
 
 @docbp.route('/')
@@ -25,3 +27,35 @@ def view_round(round_id):
 def view_recv_record(rec_id):
     rec = DocReceiveRecord.query.get(rec_id)
     return render_template('documents/recv_record.html', rec=rec)
+
+
+@docbp.route('/admin')
+def admin_index():
+    rounds = DocRound.query.all()
+    return render_template('documents/admin_index.html', rounds=rounds)
+
+
+@docbp.route('/admin/rounds', methods=['GET', 'POST'])
+def add_round():
+    form = RoundForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            new_round = DocRound()
+            form.populate_obj(new_round)
+            db.session.add(new_round)
+            db.session.commit()
+            flash(u'บันทึกข้อมูลเรียบร้อยแล้ว', 'success')
+            return redirect(url_for('doc.admin_index'))
+        else:
+            for err in form.errors:
+                flash('{}'.format(err), 'danger')
+    return render_template('documents/round_edit.html', form=form)
+
+
+@docbp.route('/admin/docs', methods=['GET', 'POST'])
+def add_document():
+    form = DocumentForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            pass
+    return render_template('documents/document_form.html', form=form)
