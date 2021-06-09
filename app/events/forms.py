886f66cf3024eb5b69f6caa8f070feb5e7e35296
napatsakorn <napatsakorn.pis@mahidol.ac.th>
2021-06-09 09:53:41 +0700

    added more details of event record in function EventForm

--- app/events/forms.py
+++ app/events/forms.py
@@ -1,11 +1,22 @@
 # -*- coding:utf-8 -*-
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, DateTimeField
+from wtforms import StringField, DateTimeField, TextAreaField
+from wtforms_alchemy import QuerySelectField
 from wtforms.validators import DataRequired
+
+from app.models import Org
 
 
 class EventForm(FlaskForm):
     title = StringField(u'ชื่อกิจกรรม', validators=[DataRequired()])
+    desc = TextAreaField(u'รายละเอียด')
     start = DateTimeField(u'เริ่ม', validators=[DataRequired()])
     end = DateTimeField(u'สิ้นสุด', validators=[DataRequired()])
+    location = StringField(u'สถานที่')
+    organiser = QuerySelectField(query_factory=lambda:Org.query.all(),
+                                 get_label='name',
+                                 label=u'ผู้จัดงาน')
+    registration = StringField(u'ช่องทางลงทะเบียน')
+    poster = StringField(u'รหัสภาพ poster')
+
