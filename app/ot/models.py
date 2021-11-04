26d560bdbf0834cedbcae86bb085ec267efbee2a
napatsakorn <napatsakorn.pis@mahidol.ac.th>
2021-11-04 09:45:22 +0700

    added new columns (ot_start_datetime, ot_end_datetime) into ot_compensation_rate table and added new column (file_name) into ot_payment_announce table and added info label in each column

--- app/ot/models.py
+++ app/ot/models.py
@@ -10,13 +10,14 @@ from datetime import datetime
 class OtPaymentAnnounce(db.Model):
     __tablename__ = 'ot_payment_announce'
     id = db.Column('id', db.Integer(), primary_key=True, autoincrement=True)
-    topic = db.Column('topic', db.String())
+    topic = db.Column('topic', db.String(), info={'label':u'เรื่อง'})
+    file_name = db.Column('file_name', db.String())
     upload_file_url = db.Column('upload_file_url', db.String())
     created_account_id = db.Column('created_account_id', db.ForeignKey('staff_account.id'))
     staff = db.relationship(StaffAccount, backref=db.backref('ot_announcement'))
     created_at = db.Column('created_at', db.DateTime(timezone=True), default=datetime.now())
-    announce_at = db.Column('announce_at', db.DateTime(timezone=True))
-    start_datetime = db.Column('start_datetime', db.DateTime(timezone=True))
+    announce_at = db.Column('announce_at', db.DateTime(timezone=True), info={'label':u'ประกาศเมื่อ'})
+    start_datetime = db.Column('start_datetime', db.DateTime(timezone=True), info={'label':u'เริ่มใช้ตั้งแต่'})
     cancelled_at = db.Column('cancelled_at', db.DateTime(timezone=True))
 
 
@@ -28,16 +29,19 @@ class OtCompensationRate(db.Model):
     work_for_org_id = db.Column('work_for_org_id', db.ForeignKey('orgs.id'))
     work_at_org = db.relationship(Org, backref=db.backref('ot_work_at_rate'), foreign_keys=[work_at_org_id])
     work_for_org = db.relationship(Org, backref=db.backref('ot_work_for_rate'), foreign_keys=[work_for_org_id])
-    role = db.Column('role', db.String())
-    per_period = db.Column('per_period', db.Integer())
-    per_hour = db.Column('per_hour', db.Integer())
-    per_day = db.Column('per_day', db.Integer())
-    start_datetime = db.Column('start_datetime', db.DateTime(timezone=True))
-    end_datetime = db.Column('end_datetime', db.DateTime(timezone=True))
-    is_faculty_emp = db.Column('is_faculty_emp', db.Boolean())
-    is_workday = db.Column('is_workday', db.Boolean(), default=True, nullable=False)
-    max_hour = db.Column('max_hour', db.Integer())
-    double_payment = db.Column('double_payment', db.Boolean(), default=True, nullable=False)
+    role = db.Column('role', db.String(), info={'label':u'ตำแหน่ง'})
+    per_period = db.Column('per_period', db.Integer(), info={'label':u'ต่อคาบ'})
+    per_hour = db.Column('per_hour', db.Integer(), info={'label':u'ต่อชั่วโมง'})
+    per_day = db.Column('per_day', db.Integer(), info={'label':u'ต่อวัน'})
+    start_datetime = db.Column('start_datetime', db.DateTime(timezone=True), info={'label':u'วันที่เริ่มต้น'})
+    end_datetime = db.Column('end_datetime', db.DateTime(timezone=True), info={'label':u'วันที่สิ้นสุด'})
+    ot_start_datetime = db.Column('ot_start_datetime', db.DateTime(timezone=True))
+    ot_end_datetime = db.Column('ot_end_datetime', db.DateTime(timezone=True))
+    is_faculty_emp = db.Column('is_faculty_emp', db.Boolean(), info={'label':u'บุคลากรสังกัดคณะ'})
+    is_workday = db.Column('is_workday', db.Boolean(), default=True, nullable=False, info={'label':u'นอกเวลาราชการ'})
+    max_hour = db.Column('max_hour', db.Integer(), info={'label':u'จำนวนชั่วโมงสูงสุดที่สามารถทำได้'})
+    double_payment = db.Column('double_payment', db.Boolean(), default=True, nullable=False,
+                               info={'label':u'เบิกซ้ำกับอันอื่นได้'})
 
 
 # class OtDocumentApproval(db.Model):
@@ -46,8 +50,8 @@ class OtCompensationRate(db.Model):
 #     approval_no = db.Column('approval_no', db.String())
 #     approved_date = db.Column('approved_date', db.Date(), nullable=True)
 #     created_at = db.Column('created_at',db.DateTime(timezone=True),default=datetime.now())
-#     start_date = db.Column('start_date', db.Date(), nullable=True)
-#     end_date = db.Column('end_date', db.Date(), nullable=True)
+#     start_datetime = db.Column('start_datetime', db.DateTime(), nullable=False)
+#     end_datetime = db.Column('end_datetime', db.DateTime())
 #     cancelled_at = db.Column('cancelled_at', db.DateTime(timezone=True))
 #     org_id = db.Column('orgs_id', db.ForeignKey('orgs.id'))
 #     org = db.relationship(Org, backref=db.backref('document_approval'))
