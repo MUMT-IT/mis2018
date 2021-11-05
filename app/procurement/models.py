fd8866ad469b7a206480486d7934f6e504770637
YADA <91609845+AIMTYADA@users.noreply.github.com>
2021-11-05 15:46:07 +0700

    Updated new procurement tables

--- app/procurement/models.py
+++ app/procurement/models.py
@@ -5,5 +5,8 @@ from app.main import db
 class ProcurementDetail(db.Model):
     __tablename__ = 'procurement_details'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(255), nullable=False)
-    number = db.Column(db.Integer(), nullable=False)
+    list = db.Column(db.String(255), nullable=False)
+    type = db.Column(db.String(255), nullable=False)
+    code = db.Column(db.String(255), nullable=False)
+    location = db.Column(db.String(length=10))
+    available = db.Column(db.String(255), nullable=False)
