c2878aff602046bebc91c142264840c62b3a30f4
YADA <91609845+AIMTYADA@users.noreply.github.com>
2021-11-11 12:37:43 +0700

    Updated view procurement and added record of using log in by fixing id staff

--- app/procurement/views.py
+++ app/procurement/views.py
@@ -1,9 +1,14 @@
 # -*- coding:utf-8 -*-
 from flask import render_template, request, flash, redirect, url_for, session, jsonify, Flask
-
+from flask_login import current_user
 from . import procurementbp as procurement
 from .models import ProcurementDetail
 from ..main import db
+from .forms import *
+from datetime import datetime
+from pytz import timezone
+
+bangkok = timezone('Asia/Bangkok')
 
 
 @procurement.route('/add', methods=['GET','POST'])
@@ -36,9 +41,7 @@ def view_procurement():
         record = {}
         record["id"] = procurement.id
         record["list"] = procurement.list
-        record["type"] = procurement.type
         record["code"] = procurement.code
-        record["location"] = procurement.location
         record["available"] = procurement.available
         procurement_list.append(record)
     return render_template('procurement/view_all_data.html', procurement_list=procurement_list)
@@ -77,6 +80,24 @@ def edit_procurement(procurement_id):
 def view_qrcode(procurement_id):
     item = ProcurementDetail.query.get(procurement_id)
     return render_template('procurement/view_qrcode.html',
+                           model=ProcurementRecord,
                            item=item,
                            code=item.code)
 
+
+@procurement.route('/items/<int:item_id>/records/add', methods=['GET', 'POST'])
+def add_record(item_id):
+    form = ProcurementRecordForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            new_record = ProcurementRecord()
+            form.populate_obj(new_record)
+            new_record.item_id = item_id
+            # TODO: use logged in user instead
+            new_record.staff_id = 1
+            new_record.updated_at = datetime.now(tz=bangkok)
+            db.session.add(new_record)
+            db.session.commit()
+            flash('New Record Has Been Added.', 'success')
+            return redirect(url_for('procurement.view_procurement'))
+    return render_template('procurement/record_form.html', form=form)
