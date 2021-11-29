f06a682abdc708df7d90cb2be2ae239a673cf815
YADA <91609845+AIMTYADA@users.noreply.github.com>
2021-11-29 16:29:38 +0700

    Updated Purchase Tracker Contact in admin view

--- app/main.py
+++ app/main.py
@@ -191,6 +191,7 @@ from app.purchase_tracker.models import *
 
 admin.add_views(ModelView(PurchaseTrackerAccount, db.session, category='PurchaseTracker'))
 admin.add_views(ModelView(PurchaseTrackerStatus, db.session, category='PurchaseTracker'))
+admin.add_views(ModelView(PurchaseTrackerContact, db.session, category='PurchaseTracker'))
 
 from staff import staffbp as staff_blueprint
 
