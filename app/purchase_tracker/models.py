19701166f4d0d7c5c999934880ec7dd216203199
YADA <91609845+AIMTYADA@users.noreply.github.com>
2021-11-29 16:28:27 +0700

    Updated labels and added Purchase Tracker Contact information

--- app/purchase_tracker/models.py
+++ app/purchase_tracker/models.py
@@ -1,6 +1,5 @@
 # -*- coding:utf-8 -*-
 from app.main import db
-from app.room_scheduler.models import RoomResource
 from app.staff.models import StaffAccount
 
 
@@ -8,14 +7,14 @@ class PurchaseTrackerAccount(db.Model):
     __tablename__ = 'tracker_accounts'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     subject = db.Column(db.String(255), nullable=False, info={'label': u"ชื่อเรื่อง"})
-    section = db.Column(db.String(255), nullable=False)
-    number = db.Column(db.String(255), nullable=False)
+    section = db.Column(db.String(255), nullable=False, info={'label': u"หัวข้อ"})
+    number = db.Column(db.String(255), nullable=False, info={'label': u"เลขที่"})
     creation_date = db.Column('creation_date', db.DateTime(timezone=True), nullable=False)
     staff_id = db.Column('staff_id', db.ForeignKey('staff_account.id'),
                          nullable=False)
     staff = db.relationship(StaffAccount)
-    desc = db.Column('desc', db.String())
-    comment = db.Column('comment', db.String())
+    desc = db.Column('desc', db.Text(), info={'label': u"รายละเอียด"})
+    comment = db.Column('comment', db.Text(), info={'label': u"หมายเหตุ"})
 
 
     def __str__(self):
@@ -26,17 +25,34 @@ class PurchaseTrackerStatus(db.Model):
     __tablename__ = 'tracker_statuses'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     status = db.Column('status', db.String(), info={'label': u'สถานะ', 'choices': [(c, c) for c in [u'รออนุมัติ', u'รับเรื่อง']]})
-    creation_date = db.Column('creation_date', db.DateTime(timezone=True), nullable=False)
-    status_date = db.Column('status_date', db.DateTime(timezone=True), nullable=False)
+    creation_date = db.Column('creation_date', db.DateTime(timezone=True), nullable=False, info={'label': u"วันที่สร้าง"})
+    status_date = db.Column('status_date', db.DateTime(timezone=True), nullable=False, info={'label': u"รายละเอียด"})
     staff_id = db.Column('staff_id', db.ForeignKey('staff_account.id'),
                          nullable=False)
     staff = db.relationship(StaffAccount)
-    comment = db.Column('comment', db.String())
+    comment = db.Column('comment', db.Text(), info={'label': u"หมายเหตุ"})
 
     def __str__(self):
         return self.status
 
 
+class PurchaseTrackerContact(db.Model):
+    __tablename__ = 'tracker_contacts'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, info={'label': u"ชื่อ"})
+    age = db.Column(db.Integer, index=True, info={'label': u"อายุ"})
+    address = db.Column(db.String(256), info={'label': u"ที่อยู่"})
+    phone = db.Column(db.String(20), info={'label': u"เบอร์โทรศัพท์"})
+    email = db.Column(db.String(120), info={'label': u"อีเมล"})
+
+    def to_dict(self):
+        return {
+            'name': self.name,
+            'age': self.age,
+            'address': self.address,
+            'phone': self.phone,
+            'email': self.email
+        }
 
 
 
