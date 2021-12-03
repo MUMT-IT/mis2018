326ffc7ef67b71a6187703c06769b8abf8c44c32
YADA <91609845+AIMTYADA@users.noreply.github.com>
2021-12-03 16:23:14 +0700

    Updated information in templates

--- app/purchase_tracker/views.py
+++ app/purchase_tracker/views.py
@@ -1,14 +1,22 @@
 # -*- coding:utf-8 -*-
+import requests, os
 from flask import render_template, request, flash, redirect, url_for, session, jsonify, Flask
 from flask_login import current_user, login_required
+from oauth2client.service_account import ServiceAccountCredentials
+from pydrive.auth import GoogleAuth
+from werkzeug.utils import secure_filename
 from . import purchase_tracker_bp as purchase_tracker
+from ..doc_circulation.models import DocDocument, DocDocumentReach
 from ..main import db
 from .forms import *
 from datetime import datetime
 from pytz import timezone
-
+from pydrive.drive import GoogleDrive
 from .models import PurchaseTrackerAccount
+# Upload images for Google Drive
+FOLDER_ID = "1JYkU2kRvbvGnmpQ1Tb-TcQS-vWQKbXvy"
 
+json_keyfile = requests.get(os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')).json()
 
 bangkok = timezone('Asia/Bangkok')
 
@@ -22,38 +30,65 @@ def index():
 
 @purchase_tracker.route('/create', methods=['GET', 'POST'])
 def add_account():
-    form = RegisterAccountForm()
+    form = CreateAccountForm()
     if request.method == 'POST':
         if form.validate_on_submit():
-            flash(u'บันทึกข้อมูลสำเร็จ.', 'success')
-        else:
+            filename = ''
             purchase_tracker = PurchaseTrackerAccount()
             form.populate_obj(purchase_tracker)
             purchase_tracker.creation_date = bangkok.localize(datetime.now())
             purchase_tracker.staff = current_user
+            drive = initialize_gdrive()
+            if form.upload.data:
+                if not filename or (form.upload.data.filename != filename):
+                    upfile = form.upload.data
+                    filename = secure_filename(upfile.filename)
+                    upfile.save(filename)
+                    file_drive = drive.CreateFile({'title': filename,
+                                                   'parents': [{'id': FOLDER_ID, "kind": "drive#fileLink"}]})
+                    file_drive.SetContentFile(filename)
+                    try:
+                        file_drive.Upload()
+                        permission = file_drive.InsertPermission({'type': 'anyone',
+                                                                  'value': 'anyone',
+                                                                  'role': 'reader'})
+                    except:
+                        flash('Failed to upload the attached file to the Google drive.', 'danger')
+                    else:
+                        flash('The attached file has been uploaded to the Google drive', 'success')
+                        purchase_tracker.url = file_drive['id']
+
             db.session.add(purchase_tracker)
             db.session.commit()
             flash(u'บันทึกข้อมูลสำเร็จ.', 'success')
-        return render_template('purchase_tracker/index.html')
+            return render_template('purchase_tracker/index.html')
+        # Check Error
+        else:
+            for er in form.errors:
+                flash(er, 'danger')
     return render_template('purchase_tracker/create_account.html', form=form)
+
+def initialize_gdrive():
+    gauth = GoogleAuth()
+    scopes = ['https://www.googleapis.com/auth/drive']
+    gauth.credentials = ServiceAccountCredentials.from_json_keyfile_dict(json_keyfile, scopes)
+    return GoogleDrive(gauth)
 
 
 @purchase_tracker.route('/track')
 def track():
-    return render_template('purchase_tracker/tracking.html')
+    trackers = PurchaseTrackerAccount.query.filter_by().first()
+    return render_template('purchase_tracker/tracking.html', trackers=trackers)
 
 
 @purchase_tracker.route('/supplies')
 def supplies():
-    purchase_tracker_list = []
-    purchase_tracker_query = PurchaseTrackerAccount.query.all()
-    for purchase_tracker in purchase_tracker_query:
-        record = {}
-        record["id"] = purchase_tracker.id
-        record["subject"] = purchase_tracker.subject
-        record["number"] = purchase_tracker.number
-        purchase_tracker_list.append(record)
-    return render_template('purchase_tracker/procedure_supplies.html', purchase_tracker_list=purchase_tracker_list)
+    from sqlalchemy import desc
+    purchase_trackers = PurchaseTrackerAccount.query.all()
+    return render_template('purchase_tracker/procedure_supplies.html',
+                           purchase_trackers=purchase_trackers,
+                           desc=desc,
+                           PurchaseTrackerStatus=PurchaseTrackerStatus)
 
 
 @purchase_tracker.route('/description')
@@ -65,41 +100,25 @@ def description():
 def contact():
     return render_template('purchase_tracker/contact_us.html')
 
+
 @purchase_tracker.route('/items/<int:item_id>/records/update', methods=['GET', 'POST'])
-def update_items(item_id):
-    form = RegisterAccountForm()
-    if request.method == 'POST':
-        if form.validate_on_submit():
-            flash(u'บันทึกข้อมูลสำเร็จ.', 'success')
-        else:
-            new_record = RegisterAccountForm()
-            form.populate_obj(new_record)
-            new_record.item_id = item_id
-            new_record.staff = current_user
-            new_record.creation_date = bangkok.localize(datetime.now())
-            new_record.status_date = bangkok.localize(datetime.now())
-            db.session.add(new_record)
-            db.session.commit()
-            flash(u'บันทึกข้อมูลสำเร็จ.', 'success')
-        return redirect(url_for('purchase_tracker.supplies'))
-    return render_template('purchase_tracker/update_record.html', form=form)
+def view_items(item_id):
+    form = EditAccountForm(obj=purchase_tracker)
+    purchase_tracker.staff = current_user
+    tracker = PurchaseTrackerAccount.query.get(item_id)
+    return render_template('purchase_tracker/update_record.html', tracker=tracker, form=form)
 
 
 @purchase_tracker.route('/update/<int:purchase_tracker_id>', methods=['GET', 'POST'])
 @login_required
 def update_status(purchase_tracker_id):
     purchase_tracker = PurchaseTrackerAccount.query.get(purchase_tracker_id)
-    form = RegisterAccountForm(obj=purchase_tracker)
+    form = EditAccountForm(obj=purchase_tracker)
     if request.method == 'POST':
         pur_edit = PurchaseTrackerAccount()
         form.populate_obj(pur_edit)
         db.session.add(pur_edit)
         db.session.commit()
         flash(u'แก้ไขข้อมูลเรียบร้อย', 'success')
-        return redirect(url_for('purchase_tracker.update'))
+        return redirect(url_for('purchase_tracker.supplies'))
     return render_template('purchase_tracker/update_record.html', form=form, purchase_tracker=purchase_tracker)
-
-
-@purchase_tracker.route('/choice')
-def choice():
-    return render_template('purchase_tracker/choice_fiscal.html')
