4ba424ef08ee1a4ea4202b198e726bdcf9906679
YADA <91609845+AIMTYADA@users.noreply.github.com>
2021-12-07 18:27:32 +0700

    Updated account_status, end_datetime on account model and start, end, update date, activity on status model

--- app/purchase_tracker/models.py
+++ app/purchase_tracker/models.py
@@ -9,14 +9,14 @@ class PurchaseTrackerAccount(db.Model):
     subject = db.Column(db.String(255), nullable=False, info={'label': u"ชื่อเรื่อง"})
     section = db.Column(db.String(255), nullable=False, info={'label': u"หัวข้อ"})
     number = db.Column(db.String(255), nullable=False, info={'label': u"เลขที่"})
-    creation_date = db.Column('creation_date', db.DateTime(timezone=True), nullable=False)
-    staff_id = db.Column('staff_id', db.ForeignKey('staff_account.id'),
-                         nullable=False)
+    creation_date = db.Column('creation_datetime', db.DateTime(timezone=True), nullable=False)
+    staff_id = db.Column('staff_id', db.ForeignKey('staff_account.id'), nullable=False)
     staff = db.relationship(StaffAccount)
     desc = db.Column('desc', db.Text(), info={'label': u"รายละเอียด"})
     comment = db.Column('comment', db.Text(), info={'label': u"หมายเหตุ"})
     url = db.Column(db.String(255), nullable=True)
-
+    account_status = db.Column(db.String(255), info={'label': u'สถานะ', 'choices': [(c, c) for c in [u'สิ้นสุด', u'ยกเลิก']]})
+    end_datetime = db.Column('end_datetime', db.DateTime(timezone=True), nullable=True, info={'label': u'วันที่สิ้นสุด'})
 
     def __str__(self):
         return u'{}: {}'.format(self.subject, self.number)
@@ -26,18 +26,20 @@ class PurchaseTrackerStatus(db.Model):
     __tablename__ = 'tracker_statuses'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     account_id = db.Column('item_id', db.ForeignKey('tracker_accounts.id'))
-    account = db.relationship('PurchaseTrackerAccount',
-                           backref=db.backref('records', lazy='dynamic'))
+    account = db.relationship('PurchaseTrackerAccount', backref=db.backref('records', lazy='dynamic'))
     status = db.Column('status', db.String(), info={'label': u'สถานะ', 'choices': [(c, c) for c in [u'รออนุมัติ', u'รับเรื่อง']]})
     creation_date = db.Column('creation_date', db.DateTime(timezone=True), nullable=False, info={'label': u"วันที่สร้าง"})
     status_date = db.Column('status_date', db.DateTime(timezone=True), nullable=False, info={'label': u"วันที่สถานะ"})
-    staff_id = db.Column('staff_id', db.ForeignKey('staff_account.id'),
-                         nullable=False)
+    staff_id = db.Column('staff_id', db.ForeignKey('staff_account.id'), nullable=False)
     staff = db.relationship(StaffAccount)
     comment = db.Column('comment', db.Text(), info={'label': u"หมายเหตุ"})
+    start_date = db.Column('start_date', db.Date(), nullable=False, info={'label': u'วันที่เริ่มต้น'})
+    end_date = db.Column('end_date', db.Date(), nullable=False, info={'label': u'วันที่สิ้นสุด'})
+    update_datetime = db.Column('update_date', db.DateTime(timezone=True), info={'label': u'วันที่แก้ไข'})
+    activity = db.Column('activity', db.String(), nullable=False, info={'label': u"ชื่อกิจกรรม"})
 
     def __str__(self):
-        return self.status
+        return u'{}:{}'.format(self.status, self.activity)
 
 
 
