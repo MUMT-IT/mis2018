0bdc03461abc7240d351b6d8a0890839491bc1fd
YADA <91609845+AIMTYADA@users.noreply.github.com>
2021-12-09 17:35:18 +0700

    For model, deleted period in status model and updated information in templates

--- app/purchase_tracker/models.py
+++ app/purchase_tracker/models.py
@@ -14,8 +14,10 @@ class PurchaseTrackerAccount(db.Model):
     desc = db.Column('desc', db.Text(), info={'label': u"รายละเอียด"})
     comment = db.Column('comment', db.Text(), info={'label': u"หมายเหตุ"})
     url = db.Column(db.String(255), nullable=True)
-    account_status = db.Column(db.String(255), info={'label': u'สถานะ', 'choices': [(c, c) for c in [u'สิ้นสุด', u'ยกเลิก']]})
-    end_datetime = db.Column('end_datetime', db.DateTime(timezone=True), nullable=True, info={'label': u'วันที่สิ้นสุด'})
+    account_status = db.Column(db.String(255),
+                               info={'label': u'สถานะ', 'choices': [(c, c) for c in [u'สิ้นสุด', u'ยกเลิก']]})
+    end_datetime = db.Column('end_datetime', db.DateTime(timezone=True), nullable=True,
+                             info={'label': u'วันที่สิ้นสุด'})
 
     def __str__(self):
         return u'{}: {}'.format(self.subject, self.number)
@@ -27,8 +29,10 @@ class PurchaseTrackerStatus(db.Model):
     account_id = db.Column('account_id', db.ForeignKey('tracker_accounts.id'))
     account = db.relationship('PurchaseTrackerAccount', backref=db.backref('records', lazy='dynamic'))
     status = db.Column('status', db.String(), info={'label': u'สถานะ',
-                       'choices': [(c, c) for c in [u'กำลังดำเนินการ', u'ดำเนินการเสร็จสิ้น', u'ยกเลิก']]})
-    creation_date = db.Column('creation_date', db.DateTime(timezone=True), nullable=False, info={'label': u"วันที่สร้าง"})
+                                                    'choices': [(c, c) for c in
+                                                                [u'กำลังดำเนินการ', u'ดำเนินการเสร็จสิ้น', u'ยกเลิก']]})
+    creation_date = db.Column('creation_date', db.DateTime(timezone=True), nullable=False,
+                              info={'label': u"วันที่สร้าง"})
     status_date = db.Column('status_date', db.DateTime(timezone=True), nullable=False, info={'label': u"วันที่สถานะ"})
     staff_id = db.Column('staff_id', db.ForeignKey('staff_account.id'), nullable=False)
     staff = db.relationship(StaffAccount)
@@ -41,7 +45,3 @@ class PurchaseTrackerStatus(db.Model):
 
     def __str__(self):
         return u'{}:{}'.format(self.status, self.activity)
-
-
-
-
