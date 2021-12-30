d3ec628b18b0b31c070b5bfaa22af9b62f509bf9
YADA <91609845+AIMTYADA@users.noreply.github.com>
2021-12-30 11:24:10 +0700

    Updated information in ProcurementDetail and change list is equal to name and code is equal to procurement_no

--- app/procurement/models.py
+++ app/procurement/models.py
@@ -7,18 +7,58 @@ from app.staff.models import StaffAccount
 class ProcurementDetail(db.Model):
     __tablename__ = 'procurement_details'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    list = db.Column(db.String(255), nullable=False)
-    code = db.Column(db.String(255), nullable=False)
-    available = db.Column(db.String(255), nullable=False)
+    name = db.Column('name', db.String(255), info={'label': u'ชื่อครุภัณฑ์'})
+    procurement_no = db.Column('procurement_no', db.String(), info={'label': u'เลขครุภัณฑ์'})
+    registration_no = db.Column('registration_no', db.String(), info={'label': u'เลขคุมทะเบียน'})
+    university_no = db.Column('university_no', db.String(), info={'label': u'หมายเลขมหาวิทยาลัย'})
+    document_no = db.Column('document_no', db.String(), info={'label': u'เอกสารสั่งซื้อเลขที่'})
+    erp_code = db.Column('erp_code', db.String(), info={'label': u'รหัส ERP'})
+    serial_no = db.Column('serial_no', db.String(), info={'label': u'Serial Number'})
+    purchasing_type = db.Column('purchasing_type', db.String(), info={'label': u'จัดซื้อด้วยเงิน',
+                                                    'choices': [(c, c) for c in
+                                                                [u'งบประมาณ', u'รายได้คณะ', u'เงินบริจาค', u'อื่นๆ']]})
+    bought_by = db.Column('bought_by', db.String(), info={'label': u'วิธีการจัดซื้อ', 'choices': [(c, c) for c in
+                                                                                  [u'ตกลงราคา', u'สอบราคา',
+                                                                                   u'ประกวดราคา', u'วิธีพิเสษ',
+                                                                                   u'รับบริจาค', u'e-Auction',
+                                                                                   u'วิธีคัดเลือก', u'อื่นๆ']]})
+    budget_year = db.Column('budget_year', db.String(), info={'label': u'ปีงบประมาณ'})
+    price = db.Column('price', db.String(), info={'label': u'ราคา'})
+    quantity = db.Column('quantity', db.String(), info={'label': u'จำนวน'})
+    received_date = db.Column('received_date', db.Date(), info={'label': u'วันที่ได้รับ'})
+    available = db.Column('available', db.String(), nullable=False, info={'label': u'ความสามารถการใช้งาน'})
     category_id = db.Column('category_id', db.ForeignKey('procurement_categories.id'))
     category = db.relationship('ProcurementCategory',
                                backref=db.backref('items', lazy='dynamic'))
-    model = db.Column('model', db.String())
-    maker = db.Column('maker', db.String())
-    size = db.Column('size', db.String())
-    desc = db.Column('desc', db.String())
-    comment = db.Column('comment', db.String())
-
+    guarantee = db.Column('guarantee', db.String(), info={'label': u'ประกัน'})
+    model = db.Column('model', db.String(), info={'label': u'รุ่น'})
+    maker = db.Column('maker', db.String(), info={'label': u'ยี่ห้อ'})
+    size = db.Column('size', db.String(), info={'label': u'ขนาด'})
+    desc = db.Column('desc', db.String(), info={'label': u'รายละเอียด'})
+    comment = db.Column('comment', db.String(), info={'label': u'หมายเหตุ'})
+    department = db.Column('department', db.String(), info={'label': u'ภาควิชา',
+                                                                'choices': [(c, c) for c in
+                                                                      [u'-', u'Lab กลาง', u'ภาควิชาเคมีคลินิก',
+                                                                       u'สถานเวชศาสตร์ชันสูตร',
+                                                                       u'ศูนย์เหมืองข้อมูลและชีวการแพทย์สารสนเทศ',
+                                                                       u'ศูนย์พัฒนามาตรฐานและประเมินผลิตภัณฑ์',
+                                                                       u'ศูนย์วิจัยพัฒนานวัตกรรม',
+                                                                       u'ภาควิชาจุลชีววิทยาคลินิก',
+                                                                       u'ภาควิชาจุลทรรศนศาสตร์คลินิก',
+                                                                       u'สำนักงานคณบดี', u'เทคนิคการแพทย์ชุมชน',
+                                                                       u'ภาควิชารังสีเทคนิค',
+                                                                       u'ศูนย์เทคนิคการแพทย์และรังสีเทคนิคนานาชาติ']]})
+    building = db.Column('building', db.String(), info={'label': u'อาคาร',
+                                                            'choices': [(c, c) for c in
+                                                                        [u'ยังไม่ระบุอาคาร',
+                                                                         u'ตึกคณะเทคนิคการแพทย์ ศิริราช',
+                                                                         u'อาคารวิทยาศาตร์และเทคโนโลยีการแพทย์ ศาลายา',
+                                                                         u'อาคารห้องเอ็กซเรย์คอมพิวเตอร์ ศาลายา',
+                                                                         u'OPD ชั้น 4 รพ.ศิริราช',
+                                                                         u'ศูนย์การแพทย์กาญจนาภิเษก']]})
+    floor = db.Column('floor', db.String(), info={'label': u'ชั้น'})
+    room = db.Column('room', db.String(), info={'label': u'ห้อง'})
+    responsible_person = db.Column('responsible_person', db.String(), info={'label': u'ผู้ดูแลครุภัณฑ์'})
     def __str__(self):
         return u'{}: {}'.format(self.list, self.code)
 
@@ -59,6 +99,5 @@ class ProcurementRecord(db.Model):
     status = db.relationship('ProcurementStatus',
                              backref=db.backref('records', lazy='dynamic'))
 
-#
 
 
