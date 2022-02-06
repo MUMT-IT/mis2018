ba2277a881935aca397a4e47e124c2bd16aebbcd
Likit Preeyanon <likit.pre@mahidol.edu>
2022-02-07 02:30:34 +0700

    updated the UI
    added log in requirement

--- app/room_scheduler/views.py
+++ app/room_scheduler/views.py
@@ -6,26 +6,17 @@ import pytz
 from datetime import datetime
 from dateutil import parser
 from flask import render_template, jsonify, request, flash, redirect, url_for
-from flask_login import login_required
-from googleapiclient.discovery import build
-from google.oauth2.service_account import Credentials
+from flask_login import login_required, current_user
 from ..main import db
 from . import roombp as room
 from .models import RoomResource, RoomEvent, EventCategory
 from ..models import IOCode
 
-bangkok = pytz.timezone('Asia/Bangkok')
-
-if os.environ.get('FLASK_ENV') == 'production':
-    CALENDAR_ID = '9hur49up24fdcbicdbggvpu77k@group.calendar.google.com'
-else:
-    CALENDAR_ID = 'rsrlpk6sbr0ntbq9ukd6vkpkbc@group.calendar.google.com'
-
-service_account_info = requests.get(os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')).json()
-credentials = Credentials.from_service_account_info(service_account_info)
+tz = pytz.timezone('Asia/Bangkok')
 
 
 @room.route('/api/iocodes')
+@login_required
 def get_iocodes():
     codes = []
     for code in IOCode.query.all():
@@ -35,6 +26,7 @@ def get_iocodes():
 
 
 @room.route('/api/rooms')
+@login_required
 def get_rooms():
     rooms = RoomResource.query.all()
     resources = []
@@ -53,10 +45,17 @@ def get_rooms():
 
 
 @room.route('/api/events')
+@login_required
 def get_events():
-    tz = pytz.timezone('Asia/Bangkok')
+    cal_start = request.args.get('start')
+    cal_end = request.args.get('end')
+    if cal_start:
+        cal_start = parser.isoparse(cal_start)
+    if cal_end:
+        cal_end = parser.isoparse(cal_end)
     all_events = []
-    for event in RoomEvent.query.all():
+    for event in RoomEvent.query.filter(RoomEvent.start >= cal_start)\
+            .filter(RoomEvent.end <= cal_end).filter_by(cancelled_at=None):
         # The event object is a dict object with a 'summary' key.
         start = event.start
         end = event.end
@@ -73,8 +72,8 @@ def get_events():
             'location': room.number,
             'title': u'(Rm{}) {}'.format(room.number, event.title),
             'description': event.note,
-            'start': start.astimezone(bangkok).isoformat(),
-            'end': end.astimezone(bangkok).isoformat(),
+            'start': start.astimezone(tz).isoformat(),
+            'end': end.astimezone(tz).isoformat(),
             'resourceId': room.number,
             'status': event.approved,
             'borderColor': border_color,
@@ -92,18 +91,20 @@ def index():
 
 
 @room.route('/events/<list_type>')
+@login_required
 def event_list(list_type='timelineDay'):
     return render_template('scheduler/event_list.html', list_type=list_type)
 
 
 @room.route('/events/new')
+@login_required
 def new_event():
     return render_template('scheduler/new_event.html')
 
 
 @room.route('/events/<int:event_id>', methods=['POST', 'GET'])
+@login_required
 def show_event_detail(event_id=None):
-    tz = pytz.timezone('Asia/Bangkok')
     if event_id:
         event = RoomEvent.query.get(event_id)
         if event:
@@ -116,38 +117,28 @@ def show_event_detail(event_id=None):
 
 
 @room.route('/events/cancel/<int:event_id>')
+@login_required
 def cancel(event_id=None):
     if not event_id:
         return redirect(url_for('room.index'))
 
-    tz = pytz.timezone('Asia/Bangkok')
     cancelled_datetime = tz.localize(datetime.utcnow(), is_dst=None)
     event = RoomEvent.query.get(event_id)
     event.cancelled_at = cancelled_datetime
-    event.cancelled_by = 1
+    event.cancelled_by = current_user.id
     db.session.add(event)
     db.session.commit()
-
-    scoped_credentials = credentials.with_scopes([
-        'https://www.googleapis.com/auth/calendar',
-        'https://www.googleapis.com/auth/calendar.events'
-    ])
-
-    calendar_service = build('calendar', 'v3', credentials=scoped_credentials)
-    event_ = calendar_service.events().patch(
-        calendarId=event.google_calendar_id,
-        eventId=event.google_event_id, body={'status': 'cancelled'}).execute()
-
-    if event_:
-        flash('Reservation ID={} has been updated.'.format(event_.get('id')))
 
     return redirect(url_for('room.index'))
 
 
 @room.route('/events/approve/<int:event_id>')
+@login_required
 def approve_event(event_id):
     event = RoomEvent.query.get(event_id)
     event.approved = True
+    event.approved_by = current_user.id
+    event.approved_at = tz.localize(datetime.utcnow(), is_dst=None)
     db.session.add(event)
     db.session.commit()
     flash(u'อนุมัติการจองห้องเรียบร้อยแล้ว', 'success')
@@ -155,8 +146,8 @@ def approve_event(event_id):
 
 
 @room.route('/events/edit/<int:event_id>', methods=['POST', 'GET'])
+@login_required
 def edit_detail(event_id):
-    tz = pytz.timezone('Asia/Bangkok')
     if request.method == 'POST':
         event_id = request.form.get('event_id')
         category_id = request.form.get('category_id')
@@ -181,12 +172,12 @@ def edit_detail(event_id):
             event.title = title
         if startdt:
             startdatetime = parser.isoparse(startdt)
-            startdatetime = startdatetime.astimezone(bangkok)
+            startdatetime = startdatetime.astimezone(tz)
         else:
             startdatetime = None
         if enddt:
             enddatetime = parser.isoparse(enddt)
-            enddatetime = enddatetime.astimezone(bangkok)
+            enddatetime = enddatetime.astimezone(tz)
         else:
             enddatetime = None
         event.start = startdatetime
@@ -197,11 +188,12 @@ def edit_detail(event_id):
         event.request = desc
         event.extra_items = extra_items
         event.note = note
+        event.updated_by = current_user.id
         event.updated_at = tz.localize(datetime.utcnow())
         db.session.add(event)
         db.session.commit()
 
-        flash('Reservation has been updated.', 'success')
+        flash(u'อัพเดตรายการเรียบร้อย', 'success')
         return redirect(url_for('room.index'))
 
     if event_id:
@@ -211,14 +203,16 @@ def edit_detail(event_id):
             event.start = event.start.astimezone(tz)
             event.end = event.end.astimezone(tz)
             event.extra_items = event.extra_items.split('|') if event.extra_items else []
+            iocode = event.iocode.to_dict() if event.iocode_id else {'id': ''}
             return render_template('scheduler/event_edit.html',
-                                   iocode=event.iocode.to_dict(),
+                                   iocode=iocode,
                                    event=event, categories=categories)
     else:
         return 'No room ID specified.'
 
 
 @room.route('/list', methods=['POST', 'GET'])
+@login_required
 def room_list():
     if request.method == 'GET':
         rooms = RoomResource.query.all()
@@ -236,24 +230,26 @@ def room_list():
 
 
 @room.route('/reserve/<room_id>', methods=['GET', 'POST'])
+@login_required
 def room_reserve(room_id):
     if request.method == 'POST':
         category_id = request.form.get('category_id', None)
         startdt = request.form.get('startdate', None)
         enddt = request.form.get('enddate', None)
         title = request.form.get('title', ''),
+        desc = request.form.get('desc', '')
         participants = request.form.get('participants', 0)
 
         room = RoomResource.query.get(room_id)
         tz = pytz.timezone('Asia/Bangkok')
         if startdt:
             startdatetime = parser.isoparse(startdt)
-            startdatetime = startdatetime.astimezone(bangkok)
+            startdatetime = startdatetime.astimezone(tz)
         else:
             startdatetime = None
         if enddt:
             enddatetime = parser.isoparse(enddt)
-            enddatetime = enddatetime.astimezone(bangkok)
+            enddatetime = enddatetime.astimezone(tz)
         else:
             enddatetime = None
 
@@ -264,6 +260,8 @@ def room_reserve(room_id):
                                   start=startdatetime,
                                   end=enddatetime,
                                   created_at=tz.localize(datetime.utcnow()),
+                                  created_by=current_user.id,
+                                  request=desc,
                                   title=title,
                                   occupancy=int(participants),
                                   approved=approval_needed,
@@ -271,8 +269,8 @@ def room_reserve(room_id):
 
             db.session.add(new_event)
             db.session.commit()
-            flash('Reservation has been made.')
-            return redirect(url_for('room.index'))
+            flash(u'บันทึกการจองห้องเรียบร้อยแล้ว', 'success')
+            return redirect(url_for('room.show_event_detail', event_id=new_event.id))
 
     if room_id:
         room = RoomResource.query.get(room_id)
