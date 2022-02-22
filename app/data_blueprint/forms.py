4b4e96cf057893a47ace4f7ca16767f22b305a54
Likit Preeyanon <likit.pre@mahidol.edu>
2022-02-23 06:28:03 +0700

    updated the navbar color
    added a page for data form
    
    replaced buttons with tags for the autowrap
    
    updated the views so that a user can edit service and data info

--- app/data_blueprint/forms.py
+++ app/data_blueprint/forms.py
@@ -29,3 +29,17 @@ class CoreServiceForm(BaseModelForm):
         only = ['service']
     mission = QuerySelectField(u'พันธกิจ', query_factory=lambda: Mission.query.all(),
                                 get_label='name', blank_text='Select mission..', allow_blank=False)
+    data = QuerySelectMultipleField(u'ข้อมูลที่ใช้', get_label='name',
+                                     query_factory=lambda: Data.query.all(),
+                                     widget=widgets.ListWidget(prefix_label=False),
+                                     option_widget=widgets.CheckboxInput())
+
+
+class DataForm(BaseModelForm):
+    class Meta:
+        model = Data
+        only = ['name']
+    core_services = QuerySelectMultipleField(u'บริการที่ใช้ข้อมูลนี้', get_label='service',
+                                     query_factory=lambda: CoreService.query.all(),
+                                     widget=widgets.ListWidget(prefix_label=False),
+                                     option_widget=widgets.CheckboxInput())
--- app/data_blueprint/views.py
+++ app/data_blueprint/views.py
@@ -2,28 +2,64 @@
 
 from . import data_bp
 from app.main import db
-from forms import CoreServiceForm
+from forms import *
 from models import CoreService
-from flask import url_for, render_template, redirect, flash
+from flask import url_for, render_template, redirect, flash, request
 from flask_login import current_user, login_required
 
 
 @data_bp.route('/')
 def index():
+    data = Data.query.all()
     core_services = CoreService.query.all()
-    return render_template('data_blueprint/index.html', core_services=core_services)
+    return render_template('data_blueprint/index.html', core_services=core_services, data=data)
 
 
 @data_bp.route('/core-services/new', methods=['GET', 'POST'])
+@data_bp.route('/core-services/<int:service_id>/edit', methods=['GET', 'POST'])
 @login_required
-def core_service_form():
-    form = CoreServiceForm()
-    if form.validate_on_submit():
-        new_service = CoreService()
-        form.populate_obj(new_service)
-        new_service.creator_id = current_user.id
-        db.session.add(new_service)
-        db.session.commit()
-        flash(u'บันทึกข้อมูลเรียบร้อยแล้ว', 'success')
-        return redirect(url_for('data_bp.index'))
+def core_service_form(service_id=None):
+    if service_id:
+        service_ = CoreService.query.get(service_id)
+        form = CoreServiceForm(obj=service_)
+    else:
+        form = CoreServiceForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            if not service_id:
+                new_service = CoreService()
+                form.populate_obj(new_service)
+                new_service.creator_id = current_user.id
+                db.session.add(new_service)
+            else:
+                form.populate_obj(service_)
+                db.session.add(service_)
+            db.session.commit()
+            flash(u'บันทึกข้อมูลเรียบร้อยแล้ว', 'success')
+            return redirect(url_for('data_bp.index'))
     return render_template('data_blueprint/core_services.html', form=form)
+
+
+@data_bp.route('/data/new', methods=['GET', 'POST'])
+@data_bp.route('/data/<int:data_id>/edit', methods=['GET', 'POST'])
+@login_required
+def data_form(data_id=None):
+    if data_id:
+        data_ = Data.query.get(data_id)
+        form = DataForm(obj=data_)
+    else:
+        form = DataForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            if not data_id:
+                new_data = Data()
+                form.populate_obj(new_data)
+                new_data.creator_id = current_user.id
+                db.session.add(new_data)
+            else:
+                form.populate_obj(data_)
+                db.session.add(data_)
+            db.session.commit()
+            flash(u'บันทึกข้อมูลเรียบร้อยแล้ว', 'success')
+            return redirect(url_for('data_bp.index'))
+    return render_template('data_blueprint/data_form.html', form=form)
