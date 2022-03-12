9e2c63267efb52bbdc42ac52a67bc885e675df26
YADA <91609845+AIMTYADA@users.noreply.github.com>
2022-03-13 00:15:17 +0700

    Updated activity in StatusForm.

--- app/purchase_tracker/forms.py
+++ app/purchase_tracker/forms.py
@@ -1,12 +1,12 @@
 # -*- coding:utf-8 -*-
 
 from flask_wtf import FlaskForm
-from wtforms import FileField, SelectField, IntegerField, RadioField
+from wtforms import FileField, IntegerField, RadioField
 from wtforms.validators import DataRequired
-from wtforms_alchemy import model_form_factory
+from wtforms_alchemy import model_form_factory, QuerySelectField
 
 from app.main import db
-from app.purchase_tracker.models import PurchaseTrackerAccount, PurchaseTrackerStatus
+from app.purchase_tracker.models import PurchaseTrackerAccount, PurchaseTrackerStatus, PurchaseTrackerActivity
 
 BaseModelForm = model_form_factory(FlaskForm)
 
@@ -21,38 +21,22 @@ class CreateAccountForm(ModelForm):
     class Meta:
         model = PurchaseTrackerAccount
         exclude = ['creation_date', 'end_datetime']
+
     upload = FileField(u'อัพโหลดไฟล์')
     formats = RadioField(u'รูปแบบหลักการ',
-                           choices=[(c, c) for c in [u'หลักการตามขั้นตอนปกติ', u'รายงานผลจัดซื้อกรณีจำเป็นเร่งด่วนฯ']],
-                           coerce=unicode,
-                           validators=[DataRequired()])
-
-ACTIVITIES = [u'รับเรื่องขออนุมัติหลักการ/ใบเบิก',
-              u'จัดทำแผน/แต่งตั้งคณะกรรมการกำหนด TOR',
-              u'กำหนด TOR',
-              u'นำ TOR/SPEC รับฟังความคิดเห็น/พิจารณาความคิดเห็น',
-              u'จัดทำรายงานขอซื้อขอจ้าง/PR/แต่งตั้งคณะกรรมการ',
-              u'ขอใบจองงบประมาณ+A3',
-              u'ประกาศเชิญชวน/เชิญยื่นข้อเสนอ',
-              u'พิจารณาผลการจัดซื้อจัดจ้าง/รายงานผลการพิจารณา',
-              u'เชิญมาทำสัญญา/ร่างสัญญา',
-              u'จัดทำ PO สั่งซื้อสั่งจ้าง/สัญญา',
-              u'บริษัทจัดส่งพัสดุ/ตรวจรับพัสดุในระบบ MU-ERP/ระบบ EGP',
-              u'รายงานผลตรวจรับ/ขออนุมัติเบิกจ่ายเงิน',
-              u'ส่งหน่วยการเงินและบัญชี ตั้งฎีกาเบิกจ่าย']
+                         choices=[(c, c) for c in [u'หลักการตามขั้นตอนปกติ', u'รายงานผลจัดซื้อกรณีจำเป็นเร่งด่วนฯ']],
+                         coerce=unicode,
+                         validators=[DataRequired()])
 
 
 class StatusForm(ModelForm):
     class Meta:
         model = PurchaseTrackerStatus
         exclude = ['creation_date', 'status_date', 'end_date']
+
     days = IntegerField(u'ระยะเวลา')
-    activity = SelectField(u'กิจกรรม',
-                           choices=[(c, c) for c in ACTIVITIES],
-                           coerce=unicode,
-                           validators=[DataRequired()])
-        # field_args = {'desc': {'widget': TextAreaField()}}
-
-
-
+    activity = QuerySelectField(u'กิจกรรม',
+                                query_factory=lambda: PurchaseTrackerActivity.query.all(),
+                                get_label='activity', blank_text='Select category..', allow_blank=False,
+                                validators=[DataRequired()])
 
