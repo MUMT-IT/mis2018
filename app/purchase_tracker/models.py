61b33a548e9f78ef5e3b771e5c11d5917f90d054
YADA <91609845+AIMTYADA@users.noreply.github.com>
2022-03-18 16:58:48 +0700

    Updated order by start_date of account and added other_activity in PurchaseTrackerStatus model.

--- app/purchase_tracker/models.py
+++ app/purchase_tracker/models.py
@@ -11,16 +11,16 @@ from sqlalchemy import cast
 class PurchaseTrackerAccount(db.Model):
     __tablename__ = 'tracker_accounts'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    subject = db.Column(db.String(255), nullable=False, info={'label': u"ชื่อเรื่อง"})
-    number = db.Column(db.String(255), nullable=False, info={'label': u"เลขที่หนังสือ"})
+    subject = db.Column(db.String(255), nullable=False, info={'label': u'ชื่อเรื่อง'})
+    number = db.Column(db.String(255), nullable=False, info={'label': u'เลขที่หนังสือ'})
     booking_date = db.Column('booking_date', db.Date(), nullable=False, info={'label': u'วันที่หนังสือ'})
     amount = db.Column('amount', db.Float(), info={'label': u'วงเงินหลักการ'})
     formats = db.Column('formats', db.String(255), info={'label': u'รูปแบบหลักการ'})
     creation_date = db.Column('creation_datetime', db.DateTime(timezone=True), nullable=False)
     staff_id = db.Column('staff_id', db.ForeignKey('staff_account.id'), nullable=False)
     staff = db.relationship(StaffAccount)
-    desc = db.Column('desc', db.Text(), info={'label': u"รายละเอียด"})
-    comment = db.Column('comment', db.Text(), info={'label': u"หมายเหตุ"})
+    desc = db.Column('desc', db.Text(), info={'label': u'รายละเอียด'})
+    comment = db.Column('comment', db.Text(), info={'label': u'หมายเหตุ'})
     url = db.Column(db.String(255), nullable=True)
     account_status = db.Column(db.String(255),
                                info={'label': u'สถานะ', 'choices': [(c, c) for c in [u'สิ้นสุด', u'ยกเลิก']]})
@@ -44,16 +44,17 @@ class PurchaseTrackerStatus(db.Model):
                                                     'choices': [(c, c) for c in
                                                                 [u'กำลังดำเนินการ', u'ดำเนินการเสร็จสิ้น', u'ยกเลิก']]})
     creation_date = db.Column('creation_date', db.DateTime(timezone=True), nullable=False,
-                              info={'label': u"วันที่สร้าง"})
-    status_date = db.Column('status_date', db.DateTime(timezone=True), nullable=False, info={'label': u"วันที่สถานะ"})
+                              info={'label': u'วันที่สร้าง'})
+    status_date = db.Column('status_date', db.DateTime(timezone=True), nullable=False, info={'label': u'วันที่สถานะ'})
     staff_id = db.Column('staff_id', db.ForeignKey('staff_account.id'), nullable=False)
     staff = db.relationship(StaffAccount)
-    comment = db.Column('comment', db.Text(), info={'label': u"หมายเหตุ"})
+    comment = db.Column('comment', db.Text(), info={'label': u'หมายเหตุ'})
     start_date = db.Column('start_date', db.Date(), nullable=False, info={'label': u'วันที่เริ่มต้น'})
     end_date = db.Column('end_date', db.Date(), nullable=False, info={'label': u'วันที่สิ้นสุด'})
     update_datetime = db.Column('update_date', db.DateTime(timezone=True), info={'label': u'วันที่แก้ไข'})
-    activity = db.relationship("PurchaseTrackerActivity")
+    activity = db.relationship('PurchaseTrackerActivity')
     activity_id = db.Column('activity_id', db.ForeignKey('tracker_activities.id'))
+    other_activity = db.Column('other_activity', db.String(), info={'label': u'กิจกรรมอื่นๆ'})
 
     def __str__(self):
         return u'{}:{}'.format(self.status, self.activity)
