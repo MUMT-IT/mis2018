5c0df622841c8c78bf7d3d8bb29dcd0f799a663d
genocide20 <changesak@gmail.com>
2022-06-16 22:53:29 +0700

    Add Division model to Admin page , Update add many Employee view to accept more data

--- app/main.py
+++ app/main.py
@@ -389,7 +389,7 @@ admin.add_view(ModelView(ComHealthReceipt, db.session, category='Com Health'))
 admin.add_view(ModelView(ComHealthInvoice, db.session, category='Com Health'))
 admin.add_view(ModelView(ComHealthFinanceContactReason, db.session, category='Com Health'))
 admin.add_view(ModelView(ComHealthCustomerGroup, db.session, category='Com Health'))
-
+admin.add_view(ModelView(ComHealthDivision, db.session, category='Com Health'))
 
 class ComHealthTestModelView(ModelView):
     form_args = {
