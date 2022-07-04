73de5817c2b9ed733b5ac9446fab18dd3da118b4
Likit Preeyanon <likit.pre@mahidol.edu>
2022-07-04 20:44:41 +0700

    added a module name

--- app/scb_payment_service/__init__.py
+++ app/scb_payment_service/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-scb_payment = Blueprint('scb_payment', url_prefix='/scb_payment')
+scb_payment = Blueprint('scb_payment', __name__, url_prefix='/scb_payment')
 
 from . import views
