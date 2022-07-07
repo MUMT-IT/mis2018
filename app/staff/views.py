9c943e5bd1be4a5e22ef53726ce048f10c822409
Likit Preeyanon <likit.pre@mahidol.edu>
2022-07-07 12:47:57 +0700

    added login required to login scan view
    updated the UI with a modal alert

--- app/staff/views.py
+++ app/staff/views.py
@@ -1441,6 +1441,7 @@ def for_hr():
 
 @staff.route('/login-scan', methods=['GET', 'POST'])
 @csrf.exempt
+@login_required
 def login_scan():
     office_starttime = '09:00'
     office_endtime = '16:30'
