8f7e19eaeff510be528138dbeaf00c692c5769ff
YADA <91609845+AIMTYADA@users.noreply.github.com>
2022-07-26 16:30:41 +0700

    Merged heads from purchase tracker to master.

---
+++ migrations/versions/a1ae02b71bdd_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: a1ae02b71bdd
+Revises: 8b2e6d7eddda, 942184eddf89
+Create Date: 2022-07-26 16:29:08.532000
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a1ae02b71bdd'
+down_revision = ('8b2e6d7eddda', '942184eddf89')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
