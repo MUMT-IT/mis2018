b4ba3b511b5feadc1cba6556af39c833acd9e5db
YADA <91609845+AIMTYADA@users.noreply.github.com>
2022-08-03 11:28:56 +0700

    Merged master with purchase tracker.

---
+++ migrations/versions/aeb275954132_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: aeb275954132
+Revises: 31a7f831c907, e013618cb653
+Create Date: 2022-08-03 11:24:25.080000
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'aeb275954132'
+down_revision = ('31a7f831c907', 'e013618cb653')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
