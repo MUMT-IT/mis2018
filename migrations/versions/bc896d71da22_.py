5defe4c330a872db8b60201c5a5c1363664d4bbc
YADA <91609845+AIMTYADA@users.noreply.github.com>
2022-11-03 22:43:33 +0700

    Merged heads

---
+++ migrations/versions/bc896d71da22_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: bc896d71da22
+Revises: 700be2a134f5, c25f850d64b2
+Create Date: 2022-11-03 22:42:39.648000
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'bc896d71da22'
+down_revision = ('700be2a134f5', 'c25f850d64b2')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
