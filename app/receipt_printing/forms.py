58050c483b8401099faf69cacaaa7dba70b0bee0
YADA <91609845+AIMTYADA@users.noreply.github.com>
2022-11-15 15:32:36 +0700

    Added GL, cost center, and internal order field.

--- app/receipt_printing/forms.py
+++ app/receipt_printing/forms.py
@@ -25,7 +25,8 @@ class ReceiptDetailForm(ModelForm):
     class Meta:
         model = ElectronicReceiptDetail
         only = ['number', 'copy_number', 'book_number', 'comment', 'paid', 'cancelled', 'cancel_comment',
-                'payment_method', 'paid_amount', 'card_number', 'cheque_number', 'other_payment_method', 'address', 'received_from']
+                'payment_method', 'paid_amount', 'card_number', 'cheque_number', 'other_payment_method', 'address',
+                'received_from', 'gl', 'cost_center', 'internal_order']
 
     items = FieldList(FormField(ReceiptListForm, default=ElectronicReceiptItem), min_entries=1)
     issuer = QuerySelectField( query_factory=lambda: ElectronicReceiptCashier.query.all(),
