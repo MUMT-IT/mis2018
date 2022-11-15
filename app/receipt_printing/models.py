b5caa4a32eacca124eb09fc631798d6ec48a3fe3
YADA <91609845+AIMTYADA@users.noreply.github.com>
2022-11-15 19:13:50 +0700

    Removed electronic receipt cashier model, issuer column and cashier column and relatio nship. Added bank name in electronic receipt detail model and removed comment column in electronic receipt item model.

--- app/receipt_printing/models.py
+++ app/receipt_printing/models.py
@@ -4,22 +4,6 @@ from sqlalchemy import func
 from app.main import db
 
 
-class ElectronicReceiptCashier(db.Model):
-    __tablename__ = 'electronic_receipt_cashier'
-    id = db.Column('id', db.Integer, autoincrement=True, primary_key=True)
-    staff_id = db.Column('staff_id', db.ForeignKey('staff_account.id'))
-    staff = db.relationship('StaffAccount', backref=db.backref('cashier',
-                                                               cascade='all, delete-orphan',
-                                                               uselist=False))
-    position = db.Column('position', db.String(255))
-
-    @property
-    def fullname(self):
-        return self.staff.personal_info.fullname
-
-    def __str__(self):
-        return self.fullname
-
 
 class ElectronicReceiptDetail(db.Model):
     __tablename__ = 'electronic_receipt_details'
@@ -32,12 +16,6 @@ class ElectronicReceiptDetail(db.Model):
     paid = db.Column('paid', db.Boolean(), default=False)
     cancelled = db.Column('cancelled', db.Boolean(), default=False)
     cancel_comment = db.Column('cancel_comment', db.Text())
-    issuer_id = db.Column('issuer_id', db.ForeignKey('electronic_receipt_cashier.id'))
-    issuer = db.relationship('ElectronicReceiptCashier',
-                             foreign_keys=[issuer_id],
-                             backref=db.backref('issued_receipts'))
-    cashier_id = db.Column('cashier_id', db.ForeignKey('electronic_receipt_cashier.id'))
-    cashier = db.relationship('ElectronicReceiptCashier', foreign_keys=[cashier_id])
     payment_method = db.Column('payment_method', db.String(), info={'label': u'ช่องทางการชำระเงิน',
                                 'choices': [(c, c) for c in
                                 ['Select..', u'เงินสด', u'บัตรเครดิต', u'Scan QR Code', u'โอนผ่านระบบธนาคารอัตโนมัติ', u'เช็คสั่งจ่าย', u'อื่นๆ' ]]})
@@ -50,6 +28,7 @@ class ElectronicReceiptDetail(db.Model):
     gl = db.Column('gl', db.Integer(), info={'label': u'รหัสบัญชี'})
     cost_center = db.Column('cost_center', db.String(8), info={'label': u'ศูนย์ต้นทุน'})
     internal_order = db.Column('internal_order', db.Integer(), info={'label': 'Internal Order/IO'})
+    bank_name = db.Column('bank_name', db.String(), info={'label': u'ชื่อธนาคาร'})
 
 
 class ElectronicReceiptItem(db.Model):
@@ -60,4 +39,4 @@ class ElectronicReceiptItem(db.Model):
     receipt_detail = db.relationship('ElectronicReceiptDetail',
                            backref=db.backref('items', cascade='all, delete-orphan'))
     price = db.Column('price', db.Numeric(), default=0.0, info={'label': u'จำนวนเงิน'})
-    comment = db.Column('comment', db.Text(), info={'label': u'หมายเหตุ'})
+
