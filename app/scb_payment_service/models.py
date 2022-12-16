f9de3572c4d6ae240c5f6646033423b10a436b3f
YADA <91609845+AIMTYADA@users.noreply.github.com>
2022-12-16 14:39:44 +0700

    added ScbPaymentRecord model

--- app/scb_payment_service/models.py
+++ app/scb_payment_service/models.py
@@ -37,3 +37,35 @@ class ScbPaymentServiceApiClientAccount(db.Model):
         secret = ''.join(secrets.choice(alphabet) for i in range(32))
         self._secret_hash = generate_password_hash(secret)
         print('The client secret is {}. Please keep it safe.'.format(secret))
+
+
+class ScbPaymentRecord(db.Model):
+    __tablename__ = 'scb_payment_records'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    payer_account_number = db.Column('payer_account_number', db.String())
+    payee_proxy_type = db.Column('payee_proxy_type', db.String())
+    sending_bank_code = db.Column('send_bank_code', db.String())
+    payee_proxy_id = db.Column('payee_proxy_id', db.String())
+    bill_payment_ref3 = db.Column('bill_payment_ref3', db.String())
+    currency_code = db.Column('currency_code', db.String())
+    transaction_type = db.Column('transaction_type', db.String())
+    transaction_date_time = db.Column('transaction_date_time', db.DateTime(timezone=True))
+    channel_code = db.Column('channel_code', db.String())
+    bill_payment_ref1 = db.Column('bill_payment_ref1', db.String())
+    amount = db.Column('amount', db.Float(asdecimal=True))
+    payer_proxy_type = db.Column('payer_proxy_type', db.String())
+    payee_name = db.Column('payee_name', db.String())
+    receiving_bank_code = db.Column('receiveing_bank_code', db.String())
+    payee_account_number = db.Column('payee_account_number', db.String())
+    payer_proxy_id = db.Column('payer_proxy_id', db.String())
+    bill_payment_ref2 = db.Column('bill_payment_ref2', db.String())
+    transaction_id = db.Column('transaction_id', db.String())
+    payer_name = db.Column('payer_name', db.String())
+    customer1 = db.Column('customer1', db.String())
+    customer2 = db.Column('customer2', db.String())
+    service = db.Column('service', db.String())
+    created_datetime = db.Column('created_datetime', db.DateTime(timezone=True), server_default=func.now())
+
+
+    def __str__(self):
+        return u'{}: {}:{}'.format(self.payee_name, self.payer_name, self.amount)
