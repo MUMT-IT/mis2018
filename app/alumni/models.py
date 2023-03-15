df3296181336fb8132ae19d50b9d82471e55e480
YADA <91609845+AIMTYADA@users.noreply.github.com>
2023-03-15 15:47:30 +0700

    Added to_dict and property of fullname.

--- app/alumni/models.py
+++ app/alumni/models.py
@@ -13,3 +13,23 @@ class AlumniInformation(db.Model):
     occupation = db.Column('occupation', db.String(), info={'label': u'อาชีพ'})
     workplace = db.Column('workplace', db.String(), info={'label': u'สถานที่ทำงาน'})
     province = db.Column('province', db.String(), info={'label': u'จังหวัดที่ทำงาน'})
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'student_id': self.student_id,
+            'th_title': self.th_title,
+            'th_firstname': self.th_firstname,
+            'th_lastname': self.th_lastname,
+            'contact': self.contact,
+            'occupation': self.occupation,
+            'workplace': self.workplace,
+            'province': self.province
+        }
+
+    @property
+    def fullname(self):
+        return u'{}{} {}'.format(self.th_title, self.th_firstname, self.th_lastname)
+
+    def __str__(self):
+        return u'{}: {}'.format(self.student_id, self.fullname)
