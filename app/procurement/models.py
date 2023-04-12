ae96fc381c66ccc8acffe76ff0b2f09389b2dee5
YADA <91609845+AIMTYADA@users.noreply.github.com>
2023-04-12 08:54:21 +0700

    Added is_audio_visual_equipment column in ProcurementDetail model

--- app/procurement/models.py
+++ app/procurement/models.py
@@ -53,6 +53,7 @@ class ProcurementDetail(db.Model):
     is_reserved = db.Column('is_reserved', db.Boolean(), default=False)
     company_support = db.Column('company_support', db.String(), info={'label': u'ติดต่อบริษัท'})
     is_instruments = db.Column('is_instruments', db.Boolean(), default=False)
+    is_audio_visual_equipment = db.Column('is_audio_visual_equipment', db.Boolean(), default=False)
 
     def __str__(self):
         return u'{}: {}'.format(self.name, self.procurement_no)
