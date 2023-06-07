bd387d2d7066a32e6ac1e84c6911245fd62a04a4
Likit Preeyanon <likit.pre@mahidol.edu>
2023-06-08 02:38:50 +0700

    added views and pages for disaplying assignments for a specific curriculum revision and student year

--- app/studs/views.py
+++ app/studs/views.py
@@ -1,9 +1,42 @@
-from ..models import Student
+from dateutil import parser
+from flask import render_template, request, jsonify
+from flask_login import login_required
+
 from . import studbp as stud
+from app.eduqa.models import EduQACourseAssignmentSession, EduQACurriculumnRevision
 
 
 @stud.route('/')
 def index():
-    stud = Student.query.first()
-    print(stud.th_first_name)
-    return '<h2>Welcome to Students Index Page.</h2>'
+    return render_template('studs/index.html')
+
+
+@stud.route('/workload/revisions/<int:revision_id>/assignments')
+def show_assignments(revision_id):
+    revision = EduQACurriculumnRevision.query.get(revision_id)
+    student_year = request.args.get('student_year', 'ปี 1')
+    return render_template('studs/assignments.html',
+                           student_year=student_year, revision_id=revision_id, revision=revision)
+
+
+@stud.route('/api/assignments')
+@login_required
+def get_assignments():
+    print(request.args)
+    student_year = request.args.get('student_year', 'ปี 1')
+    revision_id = request.args.get('revision_id')
+    events = []
+    end = request.args.get('end')
+    start = request.args.get('start')
+    if start:
+        start = parser.isoparse(start)
+    if end:
+        end = parser.isoparse(end)
+    if revision_id:
+        for evt in EduQACourseAssignmentSession.query \
+                .filter(EduQACourseAssignmentSession.start >= start,
+                        EduQACourseAssignmentSession.start <= end,
+                        EduQACourseAssignmentSession.course.has(revision_id=int(revision_id)),
+                        EduQACourseAssignmentSession.course.has(student_year=student_year)):
+            events.append(evt.to_event())
+    return jsonify(events)
