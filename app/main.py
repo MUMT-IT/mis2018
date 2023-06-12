85d193c54ec138cf285eef1cbd1b9be9aec1a463
Likit Preeyanon <likit.pre@mahidol.edu>
2023-06-12 21:49:35 +0700

    fixed dialect mismatch problem

--- app/main.py
+++ app/main.py
@@ -71,7 +71,7 @@ def create_app():
     """Create app based on the config setting
     """
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace('://', 'ql://', 1)
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['LINE_CLIENT_ID'] = os.environ.get('LINE_CLIENT_ID')
