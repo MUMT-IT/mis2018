87738131020d5ac8c7444962b120ca4bd8e24495
YADA <91609845+AIMTYADA@users.noreply.github.com>
2023-06-17 14:56:43 +0700

    Added PAKPIForm.

--- app/PA/forms.py
+++ app/PA/forms.py
@@ -15,21 +15,6 @@ class ModelForm(BaseModelForm):
         return db.session
 
 
-class PAKPIForm(ModelForm):
-    class Meta:
-        model = PAKPI
-
-
-class PAItemForm(ModelForm):
-    class Meta:
-        model = PAItem
-
-    items = FieldList(FormField(PAKPIForm, default=PAKPI), min_entries=1)
-    type = SelectField('ประเภท',
-                       choices=[(c, c) for c in ['ปริมาณ', 'คุณภาพ', 'เวลา', 'ความคุ้มค่า', 'ความพึงพอใจ']],
-                       validators=[DataRequired()])
-
-
 class PAKPIItemForm(ModelForm):
     class Meta:
         model = PAKPIItem
@@ -38,6 +23,22 @@ class PAKPIItemForm(ModelForm):
                            get_label='level',
                            label=u'เกณฑ์การประเมิน',
                            blank_text='กรุณาเลือกเกณฑ์การประเมิน..', allow_blank=True)
+
+
+class PAKPIForm(ModelForm):
+    class Meta:
+        model = PAKPI
+    pa_kpi_items = FieldList(FormField(PAKPIItemForm, default=PAKPIItem), min_entries=5)
+
+
+class PAItemForm(ModelForm):
+    class Meta:
+        model = PAItem
+
+    items = FieldList(FormField(PAKPIForm, default=PAKPI), min_entries=1)
+
+
+
 
 
 
