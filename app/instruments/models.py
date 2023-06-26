eeba013c6608a9b5cad90dd57c014cf267194acc
YADA <91609845+AIMTYADA@users.noreply.github.com>
2023-06-26 16:16:27 +0700

    Removed location column, google_event_id, and google_calendar_id column. Added creator relationship with StaffAccount.

--- app/instruments/models.py
+++ app/instruments/models.py
@@ -10,8 +10,6 @@ tz = pytz.timezone('Asia/Bangkok')
 class InstrumentsBooking(db.Model):
     __tablename__ = 'instrument_bookings'
     id = db.Column('id', db.Integer(), primary_key=True, autoincrement=True)
-    location_id = db.Column('location_id', db.ForeignKey('scheduler_room_resources.id'))
-    location = db.relationship(RoomResource, backref=db.backref('instruments_location', lazy='dynamic'))
     detail_id = db.Column('detail_id', db.ForeignKey('procurement_details.id'))
     detail = db.relationship('ProcurementDetail', backref=db.backref('instruments_detail'))
     title = db.Column('title', db.String(), nullable=False, info={'label': u'กิจกรรม'})
@@ -19,13 +17,12 @@ class InstrumentsBooking(db.Model):
     end = db.Column('end', db.DateTime(timezone=True), nullable=False, info={'label': u'วันและเวลาสิ้นสุด'})
     created_at = db.Column('created_at', db.DateTime(timezone=True), server_default=func.now())
     created_by = db.Column('created_by', db.ForeignKey('staff_account.id'))
+    creator = db.relationship('StaffAccount', foreign_keys=[created_by])
     updated_at = db.Column('updated_at', db.DateTime(timezone=True), server_onupdate=func.now())
     updated_by = db.Column('updated_by', db.ForeignKey('staff_account.id'))
     cancelled_at = db.Column('cancelled_at', db.DateTime(timezone=True))
     cancelled_by = db.Column('cancelled_by', db.ForeignKey('staff_account.id'))
     desc = db.Column('desc', db.Text(), info={'label': u'รายละเอียด'})
-    google_event_id = db.Column('google_event_id', db.String(64))
-    google_calendar_id = db.Column('google_calendar_id', db.String(255))
 
     def to_dict(self):
         return {'id': self.id,
