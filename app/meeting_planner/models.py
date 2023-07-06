56402f0c6d7b333e2d27d9b74baed7852f1c0155
Likit Preeyanon <likit.pre@mahidol.edu>
2023-07-06 20:57:14 +0700

    added meeting agenda and meeting agenda note models

--- app/meeting_planner/models.py
+++ app/meeting_planner/models.py
@@ -21,7 +21,8 @@ class MeetingEvent(db.Model):
     updated_by = db.Column('updated_by', db.ForeignKey('staff_account.id'))
     cancelled_at = db.Column('cancelled_at', db.DateTime(timezone=True), server_default=None)
     cancelled_by = db.Column('cancelled_by', db.ForeignKey('staff_account.id'))
-    notify_participants = db.Column('notify_participants', db.Boolean(), default=True, info={'label': 'แจ้งเตือนผู้เข้าร่วมประชุม'})
+    notify_participants = db.Column('notify_participants', db.Boolean(), default=True,
+                                    info={'label': 'แจ้งเตือนผู้เข้าร่วมประชุม'})
     meeting_events = db.relationship('RoomEvent')
     meeting_url = db.Column('meeting_url', db.Text(), info={'label': 'ลิงค์ประชุมออนไลน์'})
 
@@ -58,3 +59,32 @@ class MeetingInvitation(db.Model):
     responded_at = db.Column('responded_at', db.DateTime(timezone=True))
     response = db.Column('reponse', db.String(), info={'label': 'ตอบรับ'})
 
+
+class MeetingAgenda(db.Model):
+    __tablename__ = 'meeting_agendas'
+    id = db.Column('id', db.Integer, autoincrement=True, primary_key=True)
+    group = db.Column('group', db.String(), nullable=False,
+                      info={'label': 'วาระ', 'choices': [(c, c) for c in ['แจ้งเพื่อทราบ',
+                                                                          'เพื่อพิจารณา',
+                                                                          'เรื่องสืบเนื่อง',
+                                                                          'อื่นๆ']]})
+    number = db.Column('number', db.String(), info={'label': 'ลำดับ'})
+    detail = db.Column('detail', db.Text(), info={'label': 'หัวข้อ'})
+    consensus = db.Column('consensus', db.Text())
+    updated_at = db.Column('updated_at', db.DateTime(timezone=True),
+                           onupdate=func.now())
+    consensus_updated_at = db.Column('consensus_updated_at', db.DateTime(timezone=True),
+                                     onupdate=func.now())
+    meeting_id = db.Column('meeting_id', db.ForeignKey('meeting_events.id'))
+    meeting = db.relationship(MeetingEvent, backref=db.backref('agendas',
+                                                               cascade='all, delete-orphan'))
+
+
+class MeetingAgendaNote(db.Model):
+    __tablename__ = 'meeting_agenda_notes'
+    id = db.Column('id', db.Integer, autoincrement=True, primary_key=True)
+    note = db.Column('note', db.Text())
+    updated_at = db.Column('updated_at', db.DateTime(timezone=True), onupdate=func.now)
+    staff_id = db.Column('staff_id', db.ForeignKey('staff_account.id'))
+    staff = db.relationship(StaffAccount, backref=db.backref('meeting_agenda_notes',
+                                                             lazy='dynamic'))
