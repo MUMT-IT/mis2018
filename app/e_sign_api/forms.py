cb0c4952edc821b292cf9318570deed2b8b3cd22
Likit Preeyanon <preeyano@msu.edu>
2023-08-29 15:44:47 +0700

    added test view, page for testing

--- app/e_sign_api/forms.py
+++ app/e_sign_api/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField
+from wtforms import PasswordField, BooleanField
 from wtforms.validators import DataRequired
 from wtforms_alchemy import model_form_factory
 from app.main import db
-from app.e_sign_api.models import CertificateFile
 
 BaseModelForm = model_form_factory(FlaskForm)
 
@@ -17,3 +17,8 @@ class ModelForm(BaseModelForm):
 class CertificateFileForm(FlaskForm):
     file_upload = FileField('PFX file', validators=[DataRequired()])
     image_upload = FileField('Image file')
+
+
+class TestPdfSignForm(FlaskForm):
+    doc = FileField('Test Document', validators=[DataRequired()])
+    passphrase = PasswordField('Passphrase', validators=[DataRequired()])
