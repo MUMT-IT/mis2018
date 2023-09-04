de4b17a131ffbfd5e48f7529348a988fc5b27398
Likit Preeyanon <preeyano@msu.edu>
2023-09-04 11:59:22 +0700

    changed the number of occupancy
    show the number of participants and occupancy on the detail page

--- app/room_scheduler/views.py
+++ app/room_scheduler/views.py
@@ -163,8 +163,6 @@ def edit_detail(event_id):
     form = RoomEventForm(obj=event)
     if form.validate_on_submit():
         form.populate_obj(event)
-        if event.participants:
-            event.occupancy = len(event.participants)
         event.start = arrow.get(form.start.data, 'Asia/Bangkok').datetime
         event.end = arrow.get(form.end.data, 'Asia/Bangkok').datetime
         event.updated_at = arrow.now('Asia/Bangkok').datetime
