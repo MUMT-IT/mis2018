d951ed8919e107eb60210c7d27dd0e76c4143e2d
Romnarin Rungroj <somchai081643@gmail.com>
2023-10-05 11:31:34 +0700

    Added StaffPositionForm, StaffGroupDetailForm in form

--- app/staff/forms.py
+++ app/staff/forms.py
@@ -1,10 +1,10 @@
 # -*- coding:utf-8 -*-
 
 from flask_wtf import FlaskForm
-from wtforms import widgets
-from wtforms.validators import DataRequired
+from wtforms import widgets, FieldList, FormField
 from wtforms_alchemy import (model_form_factory, QuerySelectField, QuerySelectMultipleField)
-from .models import StaffSeminar, StaffSeminarMission, StaffSeminarAttend, StaffSeminarObjective, StaffLeaveApprover
+from .models import (StaffSeminar, StaffSeminarMission, StaffSeminarAttend, StaffSeminarObjective, StaffLeaveApprover
+, StaffAccount, StaffGroupPosition, StaffGroupDetail, StaffGroupAssociation)
 from app.main import db
 
 BaseModelForm = model_form_factory(FlaskForm)
@@ -40,7 +40,24 @@ def create_seminar_attend_form(current_user):
                                     get_label='approver_name',
                                     allow_blank=True,
                                     blank_text=u'กรุณาเลือกผู้อนุมัติ',
-                                    query_factory=lambda: StaffLeaveApprover.\
+                                    query_factory=lambda: StaffLeaveApprover.
                                     query.filter_by(staff_account_id=current_user.id).all())
 
     return StaffSeminarAttendForm
+
+
+class StaffPositionForm(ModelForm):
+    class Meta:
+        model = StaffGroupAssociation
+
+    staff = QuerySelectField('ชื่อ', query_factory=lambda: StaffAccount.get_active_accounts(), get_label='fullname',
+                             allow_blank=True, blank_text='กรุณาเลือกชื่อ')
+    position = QuerySelectField('ตำแหน่ง', query_factory=lambda: StaffGroupPosition.query.all(),
+                                allow_blank=True, blank_text='กรุณาเลือกตำแหน่ง')
+
+
+class StaffGroupDetailForm(ModelForm):
+    class Meta:
+        model = StaffGroupDetail
+
+    group_members = FieldList(FormField(StaffPositionForm, default=StaffGroupAssociation), min_entries=0)
