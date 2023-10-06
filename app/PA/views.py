897ef90e6fbe6b504402e58efc47734723f7aac4
napatsakorn <napatsakorn.pis@mahidol.edu>
2023-10-06 15:34:12 +0700

    edited consensus_scoresheets_for_hr function(created emplyment and round filtering and download data to excel)

--- app/PA/views.py
+++ app/PA/views.py
@@ -6,6 +6,8 @@ from statistics import mean
 
 import pytz
 import arrow
+import os
+from pandas import DataFrame
 from sqlalchemy import exc
 from . import pa_blueprint as pa
 
@@ -15,7 +17,7 @@ from app.main import mail, StaffEmployment, StaffLeaveUsedQuota
 
 tz = pytz.timezone('Asia/Bangkok')
 
-from flask import render_template, flash, redirect, url_for, request, make_response, current_app, jsonify
+from flask import render_template, flash, redirect, url_for, request, make_response, current_app, jsonify, send_from_directory
 from flask_login import login_required, current_user
 from flask_mail import Message
 
@@ -318,12 +320,74 @@ def show_commitee():
                            departments=[{'id': d.id, 'name': d.name} for d in departments])
 
 
-@pa.route('/hr/all-consensus-scoresheets')
+@pa.route('/hr/all-consensus-scoresheets', methods=['GET', 'POST'])
 @login_required
 def consensus_scoresheets_for_hr():
-    approved_scoresheets = PAScoreSheet.query.filter_by(is_consolidated=True, is_final=True, is_appproved=True)
-    return render_template('staff/HR/PA/hr_all_consensus_scores.html',
-                           approved_scoresheets=approved_scoresheets)
+    if request.method == "POST":
+        form = request.form
+        round_id = int(form.get('round'))
+        records = []
+        scoresheets = PAScoreSheet.query.filter_by(is_consolidated=True, is_final=True, is_appproved=True)
+        columns = [u'รอบการประเมิน', u'ประเภทการจ้าง', u'ชื่อ-นามสกุล',
+                   u'สังกัด', u'Performance Score', u'Competency Score', u'คะแนนรวม', u'ระดับ']
+        for scoresheet in scoresheets:
+            if scoresheet.pa.round_id == round_id:
+                if scoresheet.pa.performance_score and scoresheet.pa.competency_score:
+                    sum_score = scoresheet.pa.performance_score + scoresheet.pa.competency_score
+                    total = round(sum_score, 2)
+                    if total >= 90:
+                        level = 'ดีเด่น'
+                    elif 80 <= total <= 89.99:
+                        level = 'ดีมาก'
+                    elif 70 <= total <= 79.99:
+                        level = 'ดี'
+                    elif 60 <= total <= 69.99:
+                        level = 'พอใช้'
+                    else:
+                        level = 'ควรปรับปรุง'
+                records.append({
+                    columns[0]: u"{}".format(scoresheet.pa.round),
+                    columns[1]: u"{}".format(scoresheet.pa.staff.personal_info.employment),
+                    columns[2]: u"{}".format(scoresheet.pa.staff.personal_info.fullname),
+                    columns[3]: u"{}".format(scoresheet.pa.staff.personal_info.org),
+                    columns[4]: u"{}".format(scoresheet.pa.performance_score if scoresheet.pa.performance_score else ""),
+                    columns[5]: u"{}".format(scoresheet.pa.competency_score if scoresheet.pa.competency_score else ""),
+                    columns[6]: u"{}".format(sum_score if sum_score else ""),
+                    columns[7]: u"{}".format(level)
+                })
+        df = DataFrame(records, columns=columns)
+        df.to_excel('pa_score.xlsx', index=False, columns=columns)
+        return send_from_directory(os.getcwd(), 'pa_score.xlsx')
+    else:
+        all_rounds = PARound.query.all()
+        rounds = PARound.query.all()
+        employment_id = request.args.get('empid', type=int)
+        round_id = request.args.get('roundid', type=int)
+        employments = StaffEmployment.query.all()
+        if employment_id is None:
+            scoresheet_list = PAScoreSheet.query.filter_by(is_consolidated=True, is_final=True, is_appproved=True)
+        else:
+            emplyment_scoresheets = []
+            scoresheet_list = PAScoreSheet.query.filter_by(is_consolidated=True, is_final=True, is_appproved=True).all()
+            for scoresheet in scoresheet_list:
+                if scoresheet.pa.staff.personal_info.employment_id == employment_id:
+                    emplyment_scoresheets.append(scoresheet)
+            scoresheet_list = emplyment_scoresheets
+        if round_id:
+            scoresheets = []
+            for scoresheet in scoresheet_list:
+                if scoresheet.pa.round_id == round_id:
+                    scoresheets.append(scoresheet)
+            scoresheet_list = scoresheets
+        else:
+            scoresheet_list = scoresheet_list
+
+        return render_template('staff/HR/PA/hr_all_consensus_scores.html', all_rounds=all_rounds,
+                               sel_emp=employment_id,
+                               scoresheet_list=scoresheet_list,
+                               employments=[{'id': e.id, 'title': e.title} for e in employments],
+                               round=round_id,
+                               rounds=[{'id': r.id, 'round': r.start.strftime('%d/%m/%Y')+'-'+r.end.strftime('%d/%m/%Y')} for r in rounds])
 
 
 @pa.route('/hr/all-consensus-scoresheetss/<int:scoresheet_id>')
