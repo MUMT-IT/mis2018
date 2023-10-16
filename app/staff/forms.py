71d789980786e357ba5d87d742114dee47123c13
Romnarin Rungroj <somchai081643@gmail.com>
2023-10-16 13:51:07 +0700

    Added QuerySelectAppendable and Edited StaffPositionForm

--- app/staff/forms.py
+++ app/staff/forms.py
@@ -46,14 +46,36 @@ def create_seminar_attend_form(current_user):
     return StaffSeminarAttendForm
 
 
+class QuerySelectFieldAppendable(QuerySelectField):
+    def __init__(self, label, validators=None, **kwargs):
+        super(QuerySelectFieldAppendable, self).__init__(label, validators, **kwargs)
+
+    def process_formdata(self, valuelist):
+        if valuelist:
+            if self.allow_blank and valuelist[0] == '__None':
+                self.data = None
+            else:
+                self._data = None
+                value = valuelist[0]
+                if not value.isdigit():
+                    position = StaffGroupPosition.query.filter_by(position=value).first()
+                    if not position:
+                        position = StaffGroupPosition(position=value)
+                        db.session.add(position)
+                        db.session.commit()
+                        self._formdata = str(position.id)
+                else:
+                    self._formdata = value
+
+
 class StaffPositionForm(ModelForm):
     class Meta:
         model = StaffGroupAssociation
 
     staff = QuerySelectField('ชื่อ', query_factory=lambda: StaffAccount.get_active_accounts(), get_label='fullname',
                              allow_blank=True, blank_text='กรุณาเลือกชื่อ')
-    position = QuerySelectField('ตำแหน่ง', query_factory=lambda: StaffGroupPosition.query.all(),
-                                allow_blank=True, blank_text='กรุณาเลือกตำแหน่ง')
+    position = QuerySelectFieldAppendable('ตำแหน่ง', query_factory=lambda: StaffGroupPosition.query.all(),
+                                allow_blank=True, blank_text='กรุณาเลือกตำแหน่ง', get_label='position')
 
 
 class StaffGroupDetailForm(ModelForm):
