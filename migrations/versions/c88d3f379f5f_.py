fa5276146c6850d18c7010027acd4bad82fec202
Likit Preeyanon <preeyano@msu.edu>
2023-11-28 05:55:20 +0700

    merged heads

---
+++ migrations/versions/c88d3f379f5f_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: c88d3f379f5f
+Revises: 2e4c6ec52035, 5e52352bf7e3
+Create Date: 2023-11-28 05:52:24.154592
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c88d3f379f5f'
+down_revision = ('2e4c6ec52035', '5e52352bf7e3')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
