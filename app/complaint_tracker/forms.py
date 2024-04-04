3863b7c816ca13f2e8670c471233cf94f3ef5096
Romnarin Rungroj <somchai081643@gmail.com>
2024-04-04 14:20:55 +0700

    Added agreement in create_record_form forms and changes class ComplaintRecordForm become def create_record_form in complaint_tracker forms

--- app/complaint_tracker/forms.py
+++ app/complaint_tracker/forms.py
@@ -2,6 +2,7 @@
 
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField
+from wtforms import RadioField
 from wtforms_alchemy import model_form_factory, QuerySelectField, QuerySelectMultipleField
 from app.complaint_tracker.models import *
 from wtforms.validators import DataRequired
@@ -20,16 +21,21 @@ class ComplaintActionRecordForm(ModelForm):
         model = ComplaintActionRecord
 
 
-class ComplaintRecordForm(ModelForm):
-    class Meta:
-        model = ComplaintRecord
-    status = QuerySelectField('สถานะ', query_factory=lambda: ComplaintStatus.query.all(), allow_blank=True)
-    priority = QuerySelectField('ระดับความสำคัญ', query_factory=lambda: ComplaintPriority.query.all(), allow_blank=True)
-    topic = QuerySelectField('หัวข้อ', query_factory=lambda: ComplaintTopic.query.all(), allow_blank=True)
-    subtopic = QuerySelectField('พันธกิจ', query_factory=lambda: ComplaintSubTopic.query.all(), allow_blank=True,
-                                blank_text='กรุณาเลือกพันธกิจ', get_label='subtopic')
-    file_upload = FileField('File Upload')
-
+def create_record_form(record_id):
+    class ComplaintRecordForm(ModelForm):
+        class Meta:
+            model = ComplaintRecord
+        status = QuerySelectField('สถานะ', query_factory=lambda: ComplaintStatus.query.all(), allow_blank=True)
+        priority = QuerySelectField('ระดับความสำคัญ', query_factory=lambda: ComplaintPriority.query.all(), allow_blank=True)
+        topic = QuerySelectField('หัวข้อ', query_factory=lambda: ComplaintTopic.query.all(), allow_blank=True)
+        subtopic = QuerySelectField('พันธกิจ', query_factory=lambda: ComplaintSubTopic.query.all(), allow_blank=True,
+                                    blank_text='กรุณาเลือกพันธกิจ', get_label='subtopic')
+        file_upload = FileField('File Upload')
+        if record_id is None:
+            agreement = RadioField(u'ความยินยอมในการเก็บรวบรวม ใช้ และเปิดเผยข้อมูล',
+                                   choices=[(c, c) for c in ['ยินยอม', 'ไม่ยินยอม']],
+                                   validators=[DataRequired()])
+    return ComplaintRecordForm
 
 class ComplaintInvestigatorForm(ModelForm):
     class Meta:
