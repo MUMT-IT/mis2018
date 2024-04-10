e2ed12bdfbe6560c01612b65c1abfb3060694f83
Likit Preeyanon <preeyano@msu.edu>
2024-04-10 12:07:25 +0700

    merged db heads

---
+++ migrations/versions/ecc48a986b25_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: ecc48a986b25
+Revises: 890b56564a68, fa350ef679e0
+Create Date: 2024-04-10 12:02:53.786186
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ecc48a986b25'
+down_revision = ('890b56564a68', 'fa350ef679e0')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
