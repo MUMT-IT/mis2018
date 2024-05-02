8c4020066b4567d24e5ec5c9a3de537944a5832f
napatsakorn <napatsakorn.pis@mahidol.edu>
2024-05-02 21:23:25 +0700

    merge heads

---
+++ migrations/versions/826c095482b3_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 826c095482b3
+Revises: 30e7d1825ad4, 521d5032b9d2
+Create Date: 2024-05-02 21:21:39.528674
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '826c095482b3'
+down_revision = ('30e7d1825ad4', '521d5032b9d2')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
