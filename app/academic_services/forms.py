c08ab1579159abfeba5561212b6e84d0e2c7cb59
Romnarin Rungroj <somchai081643@gmail.com>
2024-08-27 15:56:55 +0700

    Added CheckboxField, create_field_group_form_factory and create_request_form forms in academic_services forms

--- app/academic_services/forms.py
+++ app/academic_services/forms.py
@@ -1,13 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import DecimalField, FormField, StringField, BooleanField, TextAreaField, DateField, SelectField, \
-    SelectMultipleField, HiddenField, PasswordField, SubmitField
+    SelectMultipleField, HiddenField, PasswordField, SubmitField, widgets, RadioField
 from wtforms.validators import DataRequired, EqualTo
 from wtforms_alchemy import model_form_factory, QuerySelectField
 from app.academic_services.models import *
 from flask_login import current_user
 from collections import defaultdict, namedtuple
 from flask_wtf.csrf import generate_csrf
-import gspread
 
 FieldTuple = namedtuple('FieldTuple', ['type_', 'class_'])
 BaseModelForm = model_form_factory(FlaskForm)
@@ -88,3 +87,65 @@ class ServiceCustomerAccountForm(ModelForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password',
                                                                                              message='รหัสผ่านไม่ตรงกัน')])
+
+
+class CheckboxField(SelectMultipleField):
+    widget = widgets.ListWidget(prefix_label=False)
+    option_widget = widgets.CheckboxInput()
+
+
+def custom_string_input(field, ul_class="", **kwargs):
+    return f'''<div class="field">
+    <div class="control">
+    <input id="{field.id}" class="input" type="string" name="{field.name}" placeholder="custom input">
+    </div>
+    </div>'''
+
+
+class CustomStringField(StringField):
+    widget = custom_string_input
+
+
+field_types = {
+    'string': FieldTuple(StringField, 'input'),
+    'text': FieldTuple(TextAreaField, 'textarea'),
+    'number': FieldTuple(DecimalField, 'input'),
+    'boolean': FieldTuple(BooleanField, ''),
+    'date': FieldTuple(DateField, 'input'),
+    'choice': FieldTuple(RadioField, ''),
+    'multichoice': FieldTuple(CheckboxField, 'checkbox')
+  }
+
+
+def create_field_group_form_factory(field_group):
+    class GroupForm(FlaskForm):
+        form_html = ''
+        for field in field_group:
+            _field = field_types[field['fieldType']]
+            _field_type = f"{field['fieldType']}"
+            _field_label = f"{field['fieldLabel']}"
+            _field_placeholder = f"{field['fieldPlaceHolder']}"
+            if field['fieldType'] == 'choice' or field['fieldType'] == 'multichoice':
+                choices = field['items'].split(', ') if field['items'] else field['fieldChoice'].split(', ')
+                vars()[f"{field['fieldName']}"] = _field.type_(label=_field_label,
+                                                               choices=[(c, c) for c in choices],
+                                                               render_kw={'class': _field.class_,
+                                                                          'placeholder': current_user})
+            else:
+                vars()[f"{field['fieldName']}"] = _field.type_(label=_field_label, render_kw={'class': _field.class_,
+                                                                                              'placeholder': _field_placeholder})
+    return GroupForm
+
+
+def create_request_form(table):
+    field_groups = defaultdict(list)
+    for idx,row in table.iterrows():
+        field_groups[row['fieldGroup']].append(row)
+
+    class MainForm(FlaskForm):
+        for group_name, field_group in field_groups.items():
+            vars()[f"{group_name}"] = FormField(create_field_group_form_factory(field_group))
+        vars()["csrf_token"] = HiddenField(default=generate_csrf())
+        vars()['submit'] = SubmitField('Submit', render_kw={'class': 'button is-success',
+                                                            'style': 'display: block; margin: 0 auto;'})
+    return MainForm
