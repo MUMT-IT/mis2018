d727f99a8d18dcc3af46d9533edd206c4c71f3a3
napatsakorn <napatsakorn.pis@mahidol.edu>
2024-11-06 10:19:49 +0700

    deleted is_expired column in Process table

--- app/models.py
+++ app/models.py
@@ -412,7 +412,6 @@ class Process(db.Model):
                             backref=db.backref('processes', lazy=True))
     parent_id = db.Column('parent_id', db.ForeignKey('db_processes.id'))
     subprocesses = db.relationship('Process', backref=db.backref('parent', remote_side=[id]))
-    is_expired = db.Column('is_focused', db.Boolean(), default=False)
     def __str__(self):
         return self.name
 
