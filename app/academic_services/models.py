46f25b6b30f7f8ff1910d93640388496ce80538c
Romnarin Rungroj <somchai081643@gmail.com>
2024-11-14 08:39:49 +0700

    Added fax_no column in ServiceCustomerInfo model of academic_services models

--- app/academic_services/models.py
+++ app/academic_services/models.py
@@ -45,6 +45,7 @@ class ServiceCustomerInfo(db.Model):
     id = db.Column('id', db.Integer(), primary_key=True, autoincrement=True)
     firstname = db.Column('firstname', db.String(), info={'label': 'ชื่อ'})
     lastname = db.Column('lastname', db.String(), info={'label': 'นามสกุล'})
+    fax_no = db.Column('fax_no', db.String(), info={'label': 'fax'})
     phone_number = db.Column('phone_number', db.String(), info={'label': 'เบอร์โทรศัพท์'})
     organization_id = db.Column('organization_id', db.ForeignKey('service_customer_organizations.id'))
     organization = db.relationship('ServiceCustomerOrganization', backref=db.backref("info"), foreign_keys=[organization_id])
