1be326902d6bb3aa84b6d041e994f8ec042707ef
Romnarin Rungroj <somchai081643@gmail.com>
2024-11-18 10:27:52 +0700

    Edited url in logout view and deleted login view in academic_Services views

--- app/academic_services/views.py
+++ app/academic_services/views.py
@@ -46,40 +46,6 @@ def second_lab_index():
     return render_template('academic_services/second_lab_index.html', lab=lab)
 
 
-@academic_services.route('/login', methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        next_url = request.args.get('next', url_for('academic_services.customer_account'))
-        if is_safe_url(next_url):
-            return redirect(next_url)
-        else:
-            return abort(400)
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = db.session.query(ServiceCustomerAccount).filter_by(email=form.email.data).first()
-        if user:
-            pwd = form.password.data
-            if user.verify_password(pwd):
-                login_user(user)
-                identity_changed.send(current_app._get_current_object(), identity=Identity(user.id))
-                next_url = request.args.get('next', url_for('index'))
-                if not is_safe_url(next_url):
-                    return abort(400)
-                else:
-                    flash('ลงทะเบียนเข้าใช้งานสำเร็จ', 'success')
-                    return redirect(url_for('academic_services.customer_account', menu='view'))
-            else:
-                flash('รหัสผ่านไม่ถูกต้อง กรุณาลองอีกครั้ง', 'danger')
-                return redirect(url_for('academic_services.login'))
-        else:
-            flash('ไม่พบบัญชีผู้ใช้งาน', 'danger')
-            return redirect(url_for('academic_services.login'))
-    else:
-        for er in form.errors:
-            flash("{} {}".format(er, form.errors[er]), 'danger')
-    return render_template('academic_services/login.html', form=form)
-
-
 @academic_services.route('/logout')
 @login_required
 def logout():
@@ -88,7 +54,7 @@ def logout():
         session.pop(key, None)
     identity_changed.send(current_app._get_current_object(), identity=AnonymousIdentity())
     flash('ออกจากระบบเรียบร้อย', 'success')
-    return redirect(url_for('academic_services.login'))
+    return redirect(url_for('academic_services.customer_index'))
 
 
 @academic_services.route('/forget_password', methods=['GET', 'POST'])
