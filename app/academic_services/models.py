42156d2e4149b49c8478570f652791d6a0a2342b
Romnarin Rungroj <somchai081643@gmail.com>
2024-11-22 10:35:46 +0700

    Added quotation_no, created_at column in ServiceQuotation model of academic_services models

--- app/academic_services/models.py
+++ app/academic_services/models.py
@@ -219,15 +219,18 @@ class ServiceRequest(db.Model):
             'id': self.id,
             'created_at': self.created_at,
             'sender': self.customer_account.display_name,
-            'product': [product]
+            'product': [product],
+            'quotation_status': [quotation.status for quotation in self.quotations] if self.quotations else None
         }
 
 
 class ServiceQuotation(db.Model):
     __tablename__ = 'service_quotations'
     id = db.Column('id', db.Integer(), primary_key=True, autoincrement=True)
+    quotation_no = db.Column('quotation_no', db.String())
     total_price = db.Column('total_price', db.Float(), nullable=False)
     status = db.Column('status', db.Boolean(), default=False)
+    created_at = db.Column('created_at', db.DateTime(timezone=True))
     request_id = db.Column('request_id', db.ForeignKey('service_requests.id'))
     request = db.relationship(ServiceRequest, backref=db.backref('quotations'))
 
