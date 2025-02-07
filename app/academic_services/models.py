fce65f7a404e4ce82ecee59fe9cc27ab2f089f5a
Romnarin Rungroj <somchai081643@gmail.com>
2025-02-07 13:46:28 +0700

    Added total_price column in ServiceInvoice model of academic_services model

--- app/academic_services/models.py
+++ app/academic_services/models.py
@@ -308,6 +308,7 @@ class ServiceInvoice(db.Model):
     __tablename__ = 'service_invoices'
     id = db.Column('id', db.Integer(), primary_key=True, autoincrement=True)
     invoice_no = db.Column('invoice_no', db.String())
+    total_price = db.Column('total_price', db.Float(), nullable=False)
     status = db.Column('status', db.String())
     created_at = db.Column('created_at', db.DateTime(timezone=True))
     creator_id = db.Column('creator_id', db.ForeignKey('staff_account.id'))
