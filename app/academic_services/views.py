d30dfad675d3b08ab13bdb90522d64f495cdcb53
Romnarin Rungroj <somchai081643@gmail.com>
2025-02-11 11:24:41 +0700

    Added menu value in view_invoice view of academic_services views

--- app/academic_services/views.py
+++ app/academic_services/views.py
@@ -1369,7 +1369,8 @@ def get_invoices():
 @academic_services.route('/invoice/view/<int:invoice_id>')
 @login_required
 def view_invoice(invoice_id):
-    return render_template('academic_services/view_invoice.html', invoice_id=invoice_id)
+    menu = request.args.get('menu')
+    return render_template('academic_services/view_invoice.html', invoice_id=invoice_id, menu=menu)
 
 
 def generate_invoice_pdf(invoice, sign=False, cancel=False):
