51186cf8dcdc36d9ecb71fd5d5d95e5c4a1ae0d4
Romnarin Rungroj <somchai081643@gmail.com>
2025-03-13 08:41:14 +0700

    Added ModelForm and BaseModelForm in forms of software_req

--- app/software_request/forms.py
+++ app/software_request/forms.py
@@ -1,1 +1,11 @@
+from flask_wtf import FlaskForm
 from app.software_request.models import *
+from wtforms_alchemy import model_form_factory
+
+BaseModelForm = model_form_factory(FlaskForm)
+
+
+class ModelForm(BaseModelForm):
+    @classmethod
+    def get_session(self):
+        return db.session
