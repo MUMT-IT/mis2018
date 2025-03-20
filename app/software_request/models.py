2835b9b305258d4c71d8cab4b8d522fb134b8a6a
Romnarin Rungroj <somchai081643@gmail.com>
2025-03-20 14:48:18 +0700

    Deleted SoftwareRequestType model in software_request models

--- app/software_request/models.py
+++ app/software_request/models.py
@@ -1,7 +1,3 @@
 from app.main import db
 
 
-class SoftwareRequestType(db.Model):
-    __tablename__ = 'software_request_type'
-    id = db.Column('id', db.Integer(), primary_key=True, autoincrement=True)
-    type = db.Column('type', db.String(), info={'label': 'ประเภทคำขอ'})
