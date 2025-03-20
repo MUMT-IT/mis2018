aa9ef476ec737222819ffeffa4d5938e3e6928e4
Romnarin Rungroj <somchai081643@gmail.com>
2025-03-20 08:51:31 +0700

    Added index, condition_for_service_request and create_request views in software_request views

--- app/software_request/views.py
+++ app/software_request/views.py
@@ -1,1 +1,19 @@
+from flask import render_template, redirect, flash, url_for
+from flask_login import login_required
 from app.software_request import software_request
+
+
+@software_request.route('/')
+@login_required
+def index():
+    return render_template('software_request/index.html')
+
+
+@software_request.route('/condition')
+def condition_for_service_request():
+    return render_template('software_request/condition_page.html')
+
+
+@software_request.route('/request/add')
+def create_request():
+    return render_template('software_request/create_request.html')
