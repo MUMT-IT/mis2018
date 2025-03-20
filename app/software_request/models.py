460489f0dc5160dc9360a6d2c5e4c201420a5438
Romnarin Rungroj <somchai081643@gmail.com>
2025-03-20 09:56:54 +0700

    Added SoftwareRequestType model in software_request models

--- app/software_request/models.py
+++ app/software_request/models.py
@@ -1,1 +1,7 @@
 from app.main import db
+
+
+class SoftwareRequestType(db.Model):
+    __tablename__ = 'software_request_type'
+    id = db.Column('id', db.Integer(), primary_key=True, autoincrement=True)
+    type = db.Column('type', db.String(), info={'label': 'ประเภทคำขอ'})
