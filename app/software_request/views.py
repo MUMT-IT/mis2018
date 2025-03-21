8dad83cfb14512deb577fdc83eba582e4dcbc686
Romnarin Rungroj <somchai081643@gmail.com>
2025-03-21 11:02:15 +0700

    Added get_systems view in software_request views

--- app/software_request/views.py
+++ app/software_request/views.py
@@ -1,10 +1,9 @@
 import arrow
-from flask import render_template, redirect, flash, url_for
+from flask import render_template, redirect, flash, url_for, jsonify, request
 from flask_login import login_required, current_user
-from app.main import db
 from app.software_request import software_request
 from app.software_request.forms import SoftwareRequestDetailForm
-from app.software_request.models import SoftwareRequestDetail
+from app.software_request.models import *
 
 
 @software_request.route('/')
@@ -37,6 +36,23 @@ def create_request():
         flash('ส่งคำขอสำเร็จ', 'success')
         return redirect(url_for('software_request.index'))
     return render_template('software_request/create_request.html', form=form)
+
+
+@software_request.route('/api/system', methods=['GET'])
+@login_required
+def get_systems():
+    search_term = request.args.get('term', '')
+    key = request.args.get('key', 'id')
+    results = []
+    systems = SoftwareRequestSystem.query.all()
+    for system in systems:
+        if search_term in system.system:
+            index_ = getattr(system, key) if hasattr(system, key) else getattr(system.system, key)
+            results.append({
+                "id": index_,
+                "text": system.system
+            })
+    return jsonify({'results': results})
 
 
 @software_request.route('/admin/request/update')
