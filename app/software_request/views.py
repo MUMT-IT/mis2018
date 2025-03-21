3b6db73b772bb07907859b3a545780833f614c1d
Romnarin Rungroj <somchai081643@gmail.com>
2025-03-21 16:05:12 +0700

    Edited create_request view in software_request views

--- app/software_request/views.py
+++ app/software_request/views.py
@@ -1,9 +1,33 @@
+import os
 import arrow
+import requests
 from flask import render_template, redirect, flash, url_for, jsonify, request
 from flask_login import login_required, current_user
 from app.software_request import software_request
 from app.software_request.forms import SoftwareRequestDetailForm
 from app.software_request.models import *
+from werkzeug.utils import secure_filename
+from pydrive.auth import ServiceAccountCredentials, GoogleAuth
+from pydrive.drive import GoogleDrive
+
+gauth = GoogleAuth()
+keyfile_dict = requests.get(os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')).json()
+scopes = ['https://www.googleapis.com/auth/drive']
+gauth.credentials = ServiceAccountCredentials.from_json_keyfile_dict(keyfile_dict, scopes)
+drive = GoogleDrive(gauth)
+
+FOLDER_ID = '1832el0EAqQ6NVz2wB7Ade6wRe-PsHQsu'
+
+json_keyfile = requests.get(os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')).json()
+
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+
+def initialize_gdrive():
+    gauth = GoogleAuth()
+    scopes = ['https://www.googleapis.com/auth/drive']
+    gauth.credentials = ServiceAccountCredentials.from_json_keyfile_dict(json_keyfile, scopes)
+    return GoogleDrive(gauth)
 
 
 @software_request.route('/')
@@ -23,19 +47,54 @@ def condition_for_service_request():
     return render_template('software_request/condition_page.html')
 
 
-@software_request.route('/request/add')
-def create_request():
-    form = SoftwareRequestDetailForm()
+@software_request.route('/request/add', methods=['GET', 'POST'])
+@software_request.route('/request/edit/<int:detail_id>', methods=['GET', 'POST'])
+def create_request(detail_id=None):
+    if detail_id:
+        detail = SoftwareRequestDetail.query.get(detail_id)
+        form = SoftwareRequestDetailForm(obj=detail)
+    else:
+        form = SoftwareRequestDetailForm()
     if form.validate_on_submit():
-        detail = SoftwareRequestDetail()
+        if detail_id is None:
+            detail = SoftwareRequestDetail()
         form.populate_obj(detail)
-        detail.created_date = arrow.now('Asia/Bangkok').datetime
-        detail.created_id = current_user.id
+        file = form.file_upload.data
+        drive = initialize_gdrive()
+        if file:
+            file_name = secure_filename(file.filename)
+            file.save(file_name)
+            file_drive = drive.CreateFile({'title': file_name,
+                                           'parents': [{'id': FOLDER_ID, "kind": "drive#fileLink"}]})
+            file_drive.SetContentFile(file_name)
+            file_drive.Upload()
+            file_drive.InsertPermission({'type': 'anyone', 'value': 'anyone', 'role': 'reader'})
+            detail.url = file_drive['id']
+            detail.file_name = file_name
+        if detail_id:
+            detail.updated_date = arrow.now('Asia/Bangkok').datetime
+        else:
+            detail.created_date = arrow.now('Asia/Bangkok').datetime
+            detail.created_id = current_user.id
+        if request.form.get('systems'):
+            system_id = request.form.get('systems')
+            system = SoftwareRequestSystem.query.get(system_id)
+            detail.title = system.system
+        if 'draft' in request.form:
+            detail.status = 'ร่าง'
+        else:
+            detail.status = 'ส่งคำขอแล้ว'
         db.session.add(detail)
         db.session.commit()
-        flash('ส่งคำขอสำเร็จ', 'success')
+        if 'draft' in request.form:
+            flash('บันทึกแบบร่างสำเร็จ', 'success')
+        else:
+            flash('ส่งคำขอสำเร็จ', 'success')
         return redirect(url_for('software_request.index'))
-    return render_template('software_request/create_request.html', form=form)
+    else:
+        for er in form.errors:
+            flash(er, 'danger')
+    return render_template('software_request/create_request.html', form=form, detail_id=detail_id)
 
 
 @software_request.route('/api/system', methods=['GET'])
