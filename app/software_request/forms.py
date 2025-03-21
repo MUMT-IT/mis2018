5c1d13a97f417620b09fe3947add12cda9678c2f
Romnarin Rungroj <somchai081643@gmail.com>
2025-03-21 16:06:30 +0700

    Added SoftwareRequestDetailForm in software_request forms

--- app/software_request/forms.py
+++ app/software_request/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField
 from app.software_request.models import *
-from wtforms_alchemy import model_form_factory
-
+from wtforms_alchemy import model_form_factory, QuerySelectField
 BaseModelForm = model_form_factory(FlaskForm)
 
 
@@ -9,3 +9,12 @@ class ModelForm(BaseModelForm):
     @classmethod
     def get_session(self):
         return db.session
+
+
+class SoftwareRequestDetailForm(ModelForm):
+    class Meta:
+        model = SoftwareRequestDetail
+
+    file_upload = FileField('File Upload')
+    work_process = QuerySelectField('กระบวนการทำงาน/โครงการที่เกี่ยวข้อง', query_factory=lambda: Process.query.all(), allow_blank=True,
+                            blank_text='กรุณาเลือกกระบวนการทำงาน/โครงการที่เกี่ยวข้อง', get_label='name')
