b67ce76a2add56039f9f6d06ad0a4e9e3e2f737c
Romnarin Rungroj <somchai081643@gmail.com>
2025-03-24 15:18:09 +0700

    Added system value in SoftwareRequestDetailForm of software_request forms

--- app/software_request/forms.py
+++ app/software_request/forms.py
@@ -16,5 +16,7 @@ class SoftwareRequestDetailForm(ModelForm):
         model = SoftwareRequestDetail
 
     file_upload = FileField('File Upload')
+    system = QuerySelectField('ระบบที่ต้องการปรับปรุง', query_factory=lambda: SoftwareRequestSystem.query.all(), allow_blank=True,
+                              blank_text='กรุณาเลือกระบบี่ต้องการปรับปรุง', get_label='system')
     work_process = QuerySelectField('กระบวนการทำงาน/โครงการที่เกี่ยวข้อง', query_factory=lambda: Process.query.all(), allow_blank=True,
                             blank_text='กรุณาเลือกกระบวนการทำงาน/โครงการที่เกี่ยวข้อง', get_label='name')
