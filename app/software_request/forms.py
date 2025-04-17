78aa0aa8922d76b4cf70f84218bdfa55e4494e28
Romnarin Rungroj <somchai081643@gmail.com>
2025-04-17 14:12:29 +0700

    Added activity value in SoftwareRequestDetail of software_request forms

--- app/software_request/forms.py
+++ app/software_request/forms.py
@@ -21,10 +21,12 @@ class SoftwareRequestDetailForm(ModelForm):
     file_upload = FileField('File Upload')
     system = QuerySelectField('ระบบที่ต้องการปรับปรุง', query_factory=lambda: SoftwareRequestSystem.query.all(), allow_blank=True,
                               blank_text='กรุณาเลือกระบบี่ต้องการปรับปรุง', get_label='system')
-    work_process = QuerySelectField('กระบวนการทำงาน/โครงการที่เกี่ยวข้อง', allow_blank=True, get_label='name',
+    work_process = QuerySelectField('กระบวนการทำงาน', allow_blank=True, get_label='name',
                                     query_factory=lambda: Process.query.filter(or_(Process.staff.contains(current_user),
                                                                                    Process.staff.any(StaffAccount.personal_info.has(org=current_user.personal_info.org)))),
-                                    blank_text='กรุณาเลือกกระบวนการทำงาน/โครงการที่เกี่ยวข้อง')
+                                    blank_text='กรุณาเลือกกระบวนการทำงาน')
+    activity = QuerySelectField('โครงการที่เกี่ยวข้อง', query_factory=lambda: StrategyActivity.query.all(), allow_blank=True,
+                                blank_text='กรุณาเลือกโครงการที่เกี่ยวข้อง', get_label='content')
 
 
 class SoftwareRequestTimelineForm(ModelForm):
