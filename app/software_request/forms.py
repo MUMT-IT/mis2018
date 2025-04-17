c1967c13518173d2a2942414fb675f79f496efce
Romnarin Rungroj <somchai081643@gmail.com>
2025-04-17 10:06:04 +0700

    Edited work_process value in SoftwareRequestDetailForm in software_request forms

--- app/software_request/forms.py
+++ app/software_request/forms.py
@@ -1,7 +1,8 @@
+from sqlalchemy import or_
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField
 from wtforms import FieldList, FormField
-
+from flask_login import current_user
 from app.software_request.models import *
 from wtforms_alchemy import model_form_factory, QuerySelectField
 BaseModelForm = model_form_factory(FlaskForm)
@@ -21,16 +22,14 @@ class SoftwareRequestTimelineForm(ModelForm):
                              blank_text='กรุณาเลือกผู้รับผิดชอบ', get_label='fullname')
 
 
-def create_request_form(status):
-    class SoftwareRequestDetailForm(ModelForm):
-        class Meta:
-            model = SoftwareRequestDetail
+class SoftwareRequestDetailForm(ModelForm):
+    class Meta:
+        model = SoftwareRequestDetail
 
-        file_upload = FileField('File Upload')
-        system = QuerySelectField('ระบบที่ต้องการปรับปรุง', query_factory=lambda: SoftwareRequestSystem.query.all(), allow_blank=True,
-                                  blank_text='กรุณาเลือกระบบี่ต้องการปรับปรุง', get_label='system')
-        work_process = QuerySelectField('กระบวนการทำงาน/โครงการที่เกี่ยวข้อง', query_factory=lambda: Process.query.all(), allow_blank=True,
-                                blank_text='กรุณาเลือกกระบวนการทำงาน/โครงการที่เกี่ยวข้อง', get_label='name')
-        if status:
-            timelines = FieldList(FormField(SoftwareRequestTimelineForm, default=SoftwareRequestTimeline), min_entries=1)
-    return SoftwareRequestDetailForm
+    file_upload = FileField('File Upload')
+    system = QuerySelectField('ระบบที่ต้องการปรับปรุง', query_factory=lambda: SoftwareRequestSystem.query.all(), allow_blank=True,
+                              blank_text='กรุณาเลือกระบบี่ต้องการปรับปรุง', get_label='system')
+    work_process = QuerySelectField('กระบวนการทำงาน/โครงการที่เกี่ยวข้อง', allow_blank=True, get_label='name',
+                                    query_factory=lambda: Process.query.filter(or_(Process.staff.contains(current_user),
+                                                                                   Process.staff.any(StaffAccount.personal_info.has(org=current_user.personal_info.org)))),
+                                    blank_text='กรุณาเลือกกระบวนการทำงาน/โครงการที่เกี่ยวข้อง')
