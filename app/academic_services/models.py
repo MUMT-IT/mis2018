79ac6f06904984ac956373dbb35650d8be9f8dc2
Romnarin Rungroj <somchai081643@gmail.com>
2025-05-26 09:19:35 +0700

    Added discount column in ServiceQuotationItem model of academic_services models

--- app/academic_services/models.py
+++ app/academic_services/models.py
@@ -281,6 +281,7 @@ class ServiceQuotationItem(db.Model):
     quantity = db.Column('quantity', db.Integer(), nullable=False)
     unit_price = db.Column('unit_price', db.Float(), nullable=False)
     total_price = db.Column('total_price', db.Float(), nullable=False)
+    discount = db.Column('discount', db.String())
 
 
 class ServiceSample(db.Model):
