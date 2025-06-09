e2c8c8685560a4a6a706dbc43b423b0e9f4adbb6
Romnarin Rungroj <somchai081643@gmail.com>
2025-06-09 10:18:40 +0700

    Deleted discount column in ServiceQuotationItem model of academic_services models

--- app/academic_services/models.py
+++ app/academic_services/models.py
@@ -288,7 +288,7 @@ class ServiceQuotationItem(db.Model):
     quantity = db.Column('quantity', db.Integer(), nullable=False)
     unit_price = db.Column('unit_price', db.Float(), nullable=False)
     total_price = db.Column('total_price', db.Float(), nullable=False)
-    discount = db.Column('discount', db.String())
+    # discount = db.Column('discount', db.String())
 
 
 class ServiceSample(db.Model):
