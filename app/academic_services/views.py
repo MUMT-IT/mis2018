31b13a16bec8a0fba91670d8d439b7488a986780
Romnarin Rungroj <somchai081643@gmail.com>
2025-07-07 21:37:12 +0700

    Edited email in request_quotation view of academic_services views

--- app/academic_services/views.py
+++ app/academic_services/views.py
@@ -1009,12 +1009,12 @@ def request_quotation(request_id):
     send_mail([a.admin.email + '@mahidol.ac.th' for a in admins if not a.is_supervisor], title, message)
     title_name = 'คุณ' if current_user.customer_info.type.type == 'บุคคล' else ''
     title_for_customer = f'''แจ้งสถานะคำขอใบเสนอราคา – คณะเทคนิคการแพทย์ มหาวิทยาลัยมหิดล'''
-    message_for_customer = f'''เรียน {title_name} {current_user.customer_info.cus_name}\n\n\n'''
-    message_for_customer += f'''ตามที่ท่านได้แจ้งความประสงค์ขอรับบริการตรวจวิเคราะห์จากคณะเทคนิคการแพทย์ มหาวิทยาลัยมหิดล ขณะนี้ทางคณะฯ ได้รับข้อมูลคำขอและดำเนินการส่งคำขอใบเสนอราคาเป็นที่เรียบร้อยแล้ว\n\n'''
-    message_for_customer += f'''ทางเจ้าหน้าที่จะพิจารณารายละเอียดและจัดทำใบเสนอราคาอย่างเป็นทางการต่อไป เมื่อใบเสนอราคาออกเรียบร้อยแล้ว ท่านจะได้รับอีเมลแจ้งอีกครั้งหนึ่ง พร้อมลิงก์สำหรับตรวจสอบและยืนยันใบเสนอราคา\n\n'''
+    message_for_customer = f'''เรียน {title_name}{current_user.customer_info.cus_name}\n\n'''
+    message_for_customer += f'''ตามที่ท่านได้แจ้งความประสงค์ขอรับบริการตรวจวิเคราะห์จากคณะเทคนิคการแพทย์ มหาวิทยาลัยมหิดล ขณะนี้ทางคณะฯ ได้รับข้อมูลคำขอและดำเนินการส่งคำขอใบเสนอราคาเป็นที่เรียบร้อยแล้ว\n'''
+    message_for_customer += f'''ทางเจ้าหน้าที่จะพิจารณารายละเอียดและจัดทำใบเสนอราคาอย่างเป็นทางการต่อไป เมื่อใบเสนอราคาออกเรียบร้อยแล้ว ท่านจะได้รับอีเมลแจ้งอีกครั้งหนึ่ง พร้อมลิงก์สำหรับตรวจสอบและยืนยันใบเสนอราคา\n'''
     message_for_customer += f'''ขอขอบพระคุณที่ใช้บริการจากคณะเทคนิคการแพทย์ มหาวิทยาลัยมหิดล\n\n'''
     message_for_customer += f'''ขอแสดงความนับถือ\n'''
-    message_for_customer += f'''คณะเทคนิคการแพทย์, มหาวิทยาลัยมหิดล'''
+    message_for_customer += f'''คณะเทคนิคการแพทย์ มหาวิทยาลัยมหิดล'''
     send_mail([current_user.email], title_for_customer, message_for_customer)
     flash('ส่งใบคำขอรับบริการสำเร็จ', 'send_request')
     return redirect(url_for('academic_services.request_index', menu=menu))
