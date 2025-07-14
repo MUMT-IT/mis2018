c85b1d7760cd1a997dd98a516d859484b643cb83
Romnarin Rungroj <somchai081643@gmail.com>
2025-07-14 10:34:10 +0700

    Edited item is purpose in item_detail value of generate_repair_approval_pdf view at complaint_tracker views

--- app/complaint_tracker/views.py
+++ app/complaint_tracker/views.py
@@ -1249,7 +1249,7 @@ def generate_repair_approval_pdf(repair_approval):
             .format(checkbox=checkbox, item=repair_approval.item))
 
         item_detail = '''ด้วย {org} มีความประสงค์จะจัดซื้อหรือจ้าง {purpose} รายละเอียดดังนี้'''.format(org=organization,
-                                                                                        item=repair_approval.purpose)
+                                                                                        purpose=repair_approval.purpose)
 
         reason_title = '<font name="SarabunBold">1. เหตุผลและความจำเป็นต้องซื้อ</font>'
 
