b4e7ab93d26fc5175a53eb34c7a598b90c41d6da
Romnarin Rungroj <somchai081643@gmail.com>
2025-09-04 15:26:59 +0700

    Added condition in get_requests view of academic_services views

--- app/academic_services/views.py
+++ app/academic_services/views.py
@@ -889,7 +889,6 @@ def get_requests():
                 if i.final_file:
                     download_file = url_for('service_admin.download_file', key=i.final_file,
                                             download_filename=f"{i.report_language} (ฉบับจริง).pdf")
-                    print('s', item.status.status_id)
                     if item.status.status_id == 20:
                         html = f'''
                                 <div class="field has-addons">
