151df17cb41f04de48d7478d47fbb1c823901c8f
Romnarin Rungroj <somchai081643@gmail.com>
2025-09-08 09:35:08 +0700

    Edited province, district and subdistrict values in create_address_form of service_admin forms

--- app/service_admin/forms.py
+++ app/service_admin/forms.py
@@ -67,16 +67,16 @@ def crate_address_form(use_type=False):
                               validators=[DataRequired()])
         name = StringField(validators=[DataRequired()])
         address = StringField('ที่อยู่', validators=[DataRequired()])
-        province = QuerySelectField('จังหวัด', query_factory=lambda: Province.query.order_by(Province.name).all(),
+        province = QuerySelectField('จังหวัด', query_factory=lambda: Province.query.order_by(Province.name),
                                     allow_blank=True,
                                     blank_text='กรุณาเลือกจังหวัด', get_label='name',
                                     validators=[DataRequired(message='กรุณาเลือกจังหวัด')])
-        district = QuerySelectField('เขต/อำเภอ', query_factory=lambda: District.query.order_by(District.name).all(),
+        district = QuerySelectField('เขต/อำเภอ', query_factory=lambda: District.query.order_by(District.name),
                                     allow_blank=True,
                                     blank_text='กรุณาเลือกเขต/อำเภอ', get_label='name',
                                     validators=[DataRequired(message='กรุณาเลือกเขต/อำเภอ')])
         subdistrict = QuerySelectField('แขวง/ตำบล',
-                                       query_factory=lambda: Subdistrict.query.order_by(Subdistrict.name).all(),
+                                       query_factory=lambda: Subdistrict.query.order_by(Subdistrict.name),
                                        allow_blank=True,
                                        blank_text='กรุณาเลือกแขวง/ตำบล', get_label='name',
                                        validators=[DataRequired(message='กรุณาเลือกแขวง/ตำบล')])
