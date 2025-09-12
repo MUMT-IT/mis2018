1dbd0ec1d510a8c05364564f0a51bbff0ced58f4
Romnarin Rungroj <somchai081643@gmail.com>
2025-09-12 14:53:13 +0700

    Added quotation_issue_address column in ServiceRequest model of academic_services models

--- app/academic_services/models.py
+++ app/academic_services/models.py
@@ -360,15 +360,15 @@ class ServiceRequest(db.Model):
     receive_address = db.Column('receive_address', db.String())
     receive_phone_number = db.Column('receive_phone_number', db.String())
     quotation_name = db.Column('quotation_name', db.String())
-    # quotation_address = db.Column('quotation_address', db.String())
+    quotation_issue_address = db.Column('quotation_issue_address', db.String())
     taxpayer_identification_no = db.Column('taxpayer_identification_no', db.String())
     quotation_phone_number = db.Column('quotation_phone_number', db.String())
     document_address_id = db.Column('document_address_id', db.ForeignKey('service_customer_addresses.id'))
     document_address = db.relationship(ServiceCustomerAddress, backref=db.backref("document_address_for_requests"),
                                        foreign_keys=[document_address_id])
-    # quotation_address_id = db.Column('quotation_address_id', db.ForeignKey('service_customer_addresses.id'))
-    # quotation_address = db.relationship(ServiceCustomerAddress, backref=db.backref("quotation_address_for_requests"),
-    #                                     foreign_keys=[quotation_address_id])
+    quotation_address_id = db.Column('quotation_address_id', db.ForeignKey('service_customer_addresses.id'))
+    quotation_address = db.relationship(ServiceCustomerAddress, backref=db.backref("quotation_address_for_requests"),
+                                        foreign_keys=[quotation_address_id])
     created_at = db.Column('created_at', db.DateTime(timezone=True))
     modified_at = db.Column('modified_at', db.DateTime(timezone=True))
     status_id = db.Column('status_id', db.ForeignKey('service_statuses.id'))
