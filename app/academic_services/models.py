31c777d82b0fea7e4e5eb99ea6cb970fa811b6e7
Romnarin Rungroj <somchai081643@gmail.com>
2025-11-04 14:03:47 +0700

    Deleted product column in invoice_index, sample_index, receipt_index and result_index templates of service_admin templates

--- app/academic_services/models.py
+++ app/academic_services/models.py
@@ -404,9 +404,6 @@ class ServiceRequest(db.Model):
             'id': self.id,
             'request_no': self.request_no,
             'created_at': self.created_at,
-            'product': ", ".join(
-                [p.strip().strip('"') for p in self.product.strip("{}").split(",") if p.strip().strip('"')])
-            if self.product else None,
             'sender': self.customer.customer_info.cus_name if self.customer else None,
             'status_id': self.status.status_id if self.status else None,
             'admin_status': self.status.admin_status if self.status else None,
@@ -707,9 +704,6 @@ class ServiceQuotation(db.Model):
             'quotation_no': self.quotation_no,
             'name': self.name,
             'customer_name': self.customer_name,
-            'product': ", ".join(
-                [p.strip().strip('"') for p in self.request.product.strip("{}").split(",") if p.strip().strip('"')])
-            if self.request else None,
             'created_at': self.created_at,
             'total_price': '{:,.2f}'.format(self.grand_total()),
             'status_id': self.request.status.status_id if self.request.status else None,
