392b206b609786bb9082a111d71c8071eb59c7c6
Romnarin Rungroj <somchai081643@gmail.com>
2025-11-24 14:32:14 +0700

    Edited ref1 value and ref2 value in export_invoice_pdf view of service_admin views

--- app/service_admin/views.py
+++ app/service_admin/views.py
@@ -2746,15 +2746,13 @@ def generate_invoice_pdf(invoice, qr_image_base64=None):
 def export_invoice_pdf(invoice_id):
     invoice = ServiceInvoice.query.get(invoice_id)
     sub_lab = ServiceSubLab.query.filter_by(code=invoice.quotation.request.sub_lab.code).first()
-    # ref1 = re.sub(r'[^A-Z0-9]', '', invoice.invoice_no.upper())
-    # ref2 = re.sub(r'[^A-Z0-9]', '', sub_lab.sub_lab.upper())
-    ref1 = 'BIO25690001'
-    ref2 = 'BIO'
+    ref1 = invoice.invoice_no
+    ref2 = sub_lab.ref.upper()
     qrcode_data = generate_qrcode(amount=invoice.grand_total(), ref1=ref1, ref2=ref2, ref3=None)
-    # if qrcode_data:
-    #     qr_image_base64 = qrcode_data['qrImage']
-    # else:
-    qr_image_base64 = None
+    if qrcode_data:
+        qr_image_base64 = qrcode_data['qrImage']
+    else:
+        qr_image_base64 = None
     buffer = generate_invoice_pdf(invoice, qr_image_base64=qr_image_base64)
     return send_file(buffer, download_name='Invoice.pdf', as_attachment=True)
 
