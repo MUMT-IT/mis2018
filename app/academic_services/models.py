e3958f686b3522d57257c4fb7a08bca41f45324a
Romnarin Rungroj <somchai081643@gmail.com>
2025-11-30 15:36:45 +0700

    Edited to_dict function in ServiceRequest model of academic_services models

--- app/academic_services/models.py
+++ app/academic_services/models.py
@@ -839,7 +839,7 @@ class ServiceInvoice(db.Model):
             'file_attached_at': self.file_attached_at if self.file_attached_at else None,
             'assistant_approved_at': self.assistant_approved_at if self.assistant_approved_at else None,
             'payment_type': [payment.payment_type for payment in self.payments] if self.payments else None,
-            'payment_date': ', '.join(payment.paid_at if payment.paid_at else '' for payment in self.payments)
+            'payment_date': ', '.join(str(payment.paid_at) if payment.paid_at else '' for payment in self.payments)
                             if self.payments else None,
             'paid_at': self.paid_at if self.paid_at else None,
             'is_paid': self.is_paid if self.is_paid else None,
