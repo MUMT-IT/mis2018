ee387969d986a3f19cf706acb468eb85bb957810
Likit Preeyanon <likit.pre@mahidol.ac.th>
2025-12-23 09:42:11 +0700

    added created at and creator

---
+++ migrations/versions/737d74e5f655_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 737d74e5f655
+Revises: 64d7c7d03aeb, 9816f539c85a
+Create Date: 2025-12-23 09:26:46.633885
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '737d74e5f655'
+down_revision = ('64d7c7d03aeb', '9816f539c85a')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
