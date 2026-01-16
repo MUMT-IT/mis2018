3c32e7d6a77575dd91d7c50ae93b281c4159d0f8
Vikornsak R <vikornsak@gmail.com>
2026-01-16 09:38:59 +0700

    Fixed the import-time crash blocking migrations: Flask no longer exports Markup, so views.py now imports it from markupsafe instead.

---
+++ migrations/versions/a1bcebe7b26f_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: a1bcebe7b26f
+Revises: 45439ea481ad, b1c2d3e4f5a6
+Create Date: 2026-01-16 09:28:46.962797
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a1bcebe7b26f'
+down_revision = ('45439ea481ad', 'b1c2d3e4f5a6')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
