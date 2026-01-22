3539ae19f7f6adce9fee62dcf53017b9bec03372
Likit Preeyanon <likit.pre@mahidol.ac.th>
2026-01-22 08:11:22 +0800

    merged heads

---
+++ migrations/versions/40b9dab7f7f4_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 40b9dab7f7f4
+Revises: a29fec8b282e, b1ddddbc52e4
+Create Date: 2026-01-22 08:10:29.203379
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '40b9dab7f7f4'
+down_revision = ('a29fec8b282e', 'b1ddddbc52e4')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
