b64c151874564eaa46fb226176a29efdaef2dcc9
Romnarin Rungroj <somchai081643@gmail.com>
2026-01-29 16:24:05 +0700

    Added coat_surface_type value and coat_surface_type_other value in VirusCoatConditionForm of academic_services forms

--- app/academic_services/forms.py
+++ app/academic_services/forms.py
@@ -527,7 +527,10 @@ class VirusCoatConditionForm(FlaskForm):
                                render_kw={'class': 'input is-danger'})
     coat_test_method = RadioField('วิธีทดสอบ', choices=[(c, c) for c in ['วิธีทดสอบ Modified ASTM E1053-20']],
                                      validators=[Optional()])
-    coat_specify_surface_type = StringField('ระบุชนิดของพื้นผิว', render_kw={'class': 'input'})
+    coat_surface_type = RadioField('ชนิดพื้นผิว', choices=[('ลามิเนต', 'ลามิเนต'), ('สิ่งทอ', 'สิ่งทอ'),
+                                                             ('พื้นผิวอื่นๆ โปรดระบุ', 'พื้นผิวอื่นๆ โปรดระบุ')],
+                                     validators=[Optional()])
+    coat_surface_type_other = StringField('ระบุ', render_kw={'class': 'input'})
     coat_organism_fields = FieldList(FormField(VirusCoatTestConditionForm), min_entries=len(virus_liquid_organisms))
 
 
