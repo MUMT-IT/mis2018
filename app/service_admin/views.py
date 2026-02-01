fdf30104a207ab4341bf4924547fbc894b09f80b
Romnarin Rungroj <somchai081643@gmail.com>
2026-02-01 19:20:50 +0700

    Edited condition in generate_bacteria_quotation view and generate_virus_disinfection_quotation of service_admin views

--- app/service_admin/views.py
+++ app/service_admin/views.py
@@ -5531,86 +5531,86 @@ def generate_virus_disinfection_quotation():
     request_id = request.args.get('request_id')
     service_request = ServiceRequest.query.get(request_id)
     quotation = ServiceQuotation.query.filter_by(request_id=request_id, disapproved_at=None).first()
-    # if not quotation:
-    sheet_price_id = '1hX0WT27oRlGnQm997EV1yasxlRoBSnhw3xit1OljQ5g'
-    gc = get_credential(json_keyfile)
-    wksp = gc.open_by_key(sheet_price_id)
-    sheet_price = wksp.worksheet(service_request.sub_lab.code)
-    df_price = pandas.DataFrame(sheet_price.get_all_records())
-    quote_column_names = {}
-    quote_details = {}
-    quote_prices = {}
-    data = service_request.data
-    form = VirusDisinfectionRequestForm(data=data)
+    if not quotation:
+        sheet_price_id = '1hX0WT27oRlGnQm997EV1yasxlRoBSnhw3xit1OljQ5g'
+        gc = get_credential(json_keyfile)
+        wksp = gc.open_by_key(sheet_price_id)
+        sheet_price = wksp.worksheet(service_request.sub_lab.code)
+        df_price = pandas.DataFrame(sheet_price.get_all_records())
+        quote_column_names = {}
+        quote_details = {}
+        quote_prices = {}
+        data = service_request.data
+        form = VirusDisinfectionRequestForm(data=data)
 
-    for _, row in df_price.iterrows():
-        if row['field_group'] not in quote_column_names:
-            quote_column_names[row['field_group']] = set()
-        for field_name in row['field_name'].split(','):
-            quote_column_names[row['field_group']].add(field_name.strip())
-        sorted_field_group = ''.join(sorted(row['field_group'])).replace(' ', '')
-        key = sorted_field_group + ''.join(sorted(row[4:].str.cat())).replace(' ', '')
-        if service_request.customer.customer_info.type.type == 'หน่วยงานรัฐ':
-            quote_prices[key] = row['government_price']
-        else:
-            quote_prices[key] = row['other_price']
+        for _, row in df_price.iterrows():
+            if row['field_group'] not in quote_column_names:
+                quote_column_names[row['field_group']] = set()
+            for field_name in row['field_name'].split(','):
+                quote_column_names[row['field_group']].add(field_name.strip())
+            sorted_field_group = ''.join(sorted(row['field_group'])).replace(' ', '')
+            key = sorted_field_group + ''.join(sorted(row[4:].str.cat())).replace(' ', '')
+            if service_request.customer.customer_info.type.type == 'หน่วยงานรัฐ':
+                quote_prices[key] = row['government_price']
+            else:
+                quote_prices[key] = row['other_price']
 
-    for field in form:
-        if field.label.text not in quote_column_names:
-            continue
-        keys = []
-        keys = walk_form_fields(field, quote_column_names[field.label.text], keys=keys)
-        for key in list(itertools.combinations(keys, len(quote_column_names[field.label.text]))):
-            sorted_field_label = ''.join(sorted(field.label.text)).replace(' ', '')
-            print('f', [k[1] for k in key])
-            sorted_key_ = sorted(''.join([k[1] for k in key]))
-            p_key = sorted_field_label + ''.join(sorted_key_).replace(' ', '')
-            values = ', '.join(
-                [f"<i>{k[1]}</i>" if "organism" in k[0] and k[1] != "None" else k[1] for k in key])
-            if p_key in quote_prices:
-                prices = quote_prices[p_key]
-                if p_key in quote_details:
-                    quote_details[p_key]["quantity"] += 1
-                else:
-                    quote_details[p_key] = {"value": values, "price": prices, "quantity": 1}
-    quotation_no = ServiceNumberID.get_number('Quotation', db, lab=service_request.sub_lab.ref)
-    quotation = ServiceQuotation(quotation_no=quotation_no.number, request_id=request_id,
-                                 name=service_request.quotation_name,
-                                 address=service_request.quotation_issue_address,
-                                 taxpayer_identification_no=service_request.taxpayer_identification_no,
-                                 creator=current_user, created_at=arrow.now('Asia/Bangkok').datetime)
-    db.session.add(quotation)
-    quotation_no.count += 1
-    status_id = get_status(3)
-    service_request.status_id = status_id
-    db.session.add(service_request)
-    db.session.commit()
-    sequence_no = ServiceSequenceQuotationID.get_number('QT', db, quotation='quotation_' + str(quotation.id))
-    for _, (_, item) in enumerate(quote_details.items()):
-        quotation_item = ServiceQuotationItem(sequence=sequence_no.number, quotation_id=quotation.id,
-                                              item=item['value'],
-                                              quantity=item['quantity'],
-                                              unit_price=item['price'],
-                                              total_price=int(item['quantity']) * item['price'])
-        sequence_no.count += 1
-        db.session.add(quotation_item)
+        for field in form:
+            if field.label.text not in quote_column_names:
+                continue
+            keys = []
+            keys = walk_form_fields(field, quote_column_names[field.label.text], keys=keys)
+            for key in list(itertools.combinations(keys, len(quote_column_names[field.label.text]))):
+                sorted_field_label = ''.join(sorted(field.label.text)).replace(' ', '')
+                print('f', [k[1] for k in key])
+                sorted_key_ = sorted(''.join([k[1] for k in key]))
+                p_key = sorted_field_label + ''.join(sorted_key_).replace(' ', '')
+                values = ', '.join(
+                    [f"<i>{k[1]}</i>" if "organism" in k[0] and k[1] != "None" else k[1] for k in key])
+                if p_key in quote_prices:
+                    prices = quote_prices[p_key]
+                    if p_key in quote_details:
+                        quote_details[p_key]["quantity"] += 1
+                    else:
+                        quote_details[p_key] = {"value": values, "price": prices, "quantity": 1}
+        quotation_no = ServiceNumberID.get_number('Quotation', db, lab=service_request.sub_lab.ref)
+        quotation = ServiceQuotation(quotation_no=quotation_no.number, request_id=request_id,
+                                     name=service_request.quotation_name,
+                                     address=service_request.quotation_issue_address,
+                                     taxpayer_identification_no=service_request.taxpayer_identification_no,
+                                     creator=current_user, created_at=arrow.now('Asia/Bangkok').datetime)
+        db.session.add(quotation)
+        quotation_no.count += 1
+        status_id = get_status(3)
+        service_request.status_id = status_id
+        db.session.add(service_request)
         db.session.commit()
-    if service_request.report_languages:
-        for rl in service_request.report_languages:
+        sequence_no = ServiceSequenceQuotationID.get_number('QT', db, quotation='quotation_' + str(quotation.id))
+        for _, (_, item) in enumerate(quote_details.items()):
             quotation_item = ServiceQuotationItem(sequence=sequence_no.number, quotation_id=quotation.id,
-                                                  item=rl.report_language.item,
-                                                  quantity=1,
-                                                  unit_price=rl.report_language.price,
-                                                  total_price=rl.report_language.price)
+                                                  item=item['value'],
+                                                  quantity=item['quantity'],
+                                                  unit_price=item['price'],
+                                                  total_price=int(item['quantity']) * item['price'])
             sequence_no.count += 1
             db.session.add(quotation_item)
             db.session.commit()
-    flash('ร่างใบเสนอราคาสำเร็จ กรุณาดำเนินการตรวจสอบข้อมูล', 'success')
-    return redirect(
-        url_for('service_admin.create_quotation_for_admin', quotation_id=quotation.id, tab='draft', menu=menu))
-# else:
-#     return render_template('service_admin/quotation_created_confirmation_page.html',
-#                            quotation_id=quotation.id, request_no=service_request.request_no, menu=menu)
+        if service_request.report_languages:
+            for rl in service_request.report_languages:
+                quotation_item = ServiceQuotationItem(sequence=sequence_no.number, quotation_id=quotation.id,
+                                                      item=rl.report_language.item,
+                                                      quantity=1,
+                                                      unit_price=rl.report_language.price,
+                                                      total_price=rl.report_language.price)
+                sequence_no.count += 1
+                db.session.add(quotation_item)
+                db.session.commit()
+        flash('ร่างใบเสนอราคาสำเร็จ กรุณาดำเนินการตรวจสอบข้อมูล', 'success')
+        return redirect(
+            url_for('service_admin.create_quotation_for_admin', quotation_id=quotation.id, tab='draft', menu=menu))
+    else:
+        return render_template('service_admin/quotation_created_confirmation_page.html',
+                               quotation_id=quotation.id, request_no=service_request.request_no, menu=menu)
 
 
 @service_admin.route('/quotation/virus/air_disinfection/generate', methods=['GET', 'POST'])
