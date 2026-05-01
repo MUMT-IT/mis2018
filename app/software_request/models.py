d1addabad87a0b9730c5a041621bcea4a0da1219
ASUS <somchai081643@gmail.com>
2026-05-01 11:14:21 +0700

    Deleted start column and end column in SoftwareIssues model of software_reqeust models

--- app/software_request/models.py
+++ app/software_request/models.py
@@ -194,8 +194,6 @@ class SoftwareIssues(db.Model):
         'choices': [(c,c) for c in ('Bug', 'Request', 'Enhancement')],
     })
     issue = db.Column('issue', db.Text(), nullable=False, info={'label': 'Issue/Request'})
-    start = db.Column('start', db.Date(), info={'label': 'วันที่เริ่มต้น'})
-    end = db.Column('end', db.Date(), info={'label': 'วันที่สิ้นสุด'})
     phase_id = db.Column('phase_id', db.ForeignKey('software_request_phases.id'))
     phase = db.relationship(SoftwareRequestPhase, backref=db.backref('software_request_issues'))
     created_by = db.Column('created_by', db.ForeignKey('staff_account.id'))
