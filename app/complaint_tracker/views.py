dbd126e426ec49d2b55566a7a752d251db9f0a6f
ASUS <somchai081643@gmail.com>
2026-05-12 16:11:11 +0700

    Added closed_at value in cancel_complaint view of complaint_tracker views

--- app/complaint_tracker/views.py
+++ app/complaint_tracker/views.py
@@ -781,6 +781,7 @@ def cancel_complaint(record_id):
     status = ComplaintStatus.query.filter_by(code='cancelled').first()
     record = ComplaintRecord.query.get(record_id)
     record.status = status
+    record.closed_at = arrow.now('Asia/Bangkok').datetime
     db.session.add(record)
     db.session.commit()
     flash('ยกเลิกรายการแจ้งปัญหาสำเร็จ', 'success')
