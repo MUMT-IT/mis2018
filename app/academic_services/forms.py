102880a0859a14fbcb6af09e167fd68a4fc7273d
ASUS <somchai081643@gmail.com>
2026-06-18 10:05:29 +0700

    Edited product_name column in ConditionField form in academic_services forms

--- app/academic_services/forms.py
+++ app/academic_services/forms.py
@@ -275,7 +275,7 @@ class BacteriaLiquidTestConditionForm(FlaskForm):
 
 
 class BacteriaLiquidConditionForm(FlaskForm):
-    product_type = HiddenField('ประเภทผลิตภัณฑ์',
+    liquid_product_type = HiddenField('ประเภทผลิตภัณฑ์',
                                default='ผลิตภัณฑ์ฆ่าเชื้อบนพื้นผิวไม่มีรูพรุนชนิดของเหลว หรือชนิดผง ที่ละลายน้้าได้',
                                render_kw={'class': 'input is-danger'})
     liquid_test_method = CheckboxField('วิธีทดสอบ', choices=[(c, c) for c in ['วิธีทดสอบ Use-Dilution 60 carriers',
@@ -307,7 +307,7 @@ class BacteriaSprayTestConditionForm(FlaskForm):
 
 
 class BacteriaSprayConditionForm(FlaskForm):
-    product_type = HiddenField('ประเภทผลิตภัณฑ์',
+    spray_product_type = HiddenField('ประเภทผลิตภัณฑ์',
                                default='ผลิตภัณฑ์ฆ่าเชื้อบนพื้นผิวไม่มีรูพรุนชนิดฉีดพ่นธรรมดา หรือ ฉีดพ่นอัดก๊าซ',
                                render_kw={'class': 'input is-danger'})
     spray_test_method = CheckboxField('วิธีทดสอบ',
@@ -335,7 +335,7 @@ class BacteriaSheetTestConditionForm(FlaskForm):
 
 
 class BacteriaSheetConditionForm(FlaskForm):
-    product_type = HiddenField('ประเภทผลิตภัณฑ์',
+    sheet_product_type = HiddenField('ประเภทผลิตภัณฑ์',
                                default='ผลิตภัณฑ์ฆ่าเชื้อชนิดแผ่น',
                                render_kw={'class': 'input is-danger'})
     sheet_test_method = CheckboxField('วิธีทดสอบ', choices=[(c, c) for c in ['วิธีทดสอบ Qualitative test 60 carriers',
@@ -360,7 +360,7 @@ class BacteriaAfterWashTestConditionForm(FlaskForm):
 
 
 class BacteriaAfterWashConditionForm(FlaskForm):
-    product_type = HiddenField('ประเภทผลิตภัณฑ์',
+    after_wash_product_type = HiddenField('ประเภทผลิตภัณฑ์',
                                default='ผลิตภัณฑ์ฆ่าเชื้อที่ใช้ในกระบวนการซักผ้า-ผลิตภัณฑ์ที่อ้างสรรพคุณฤทธิ์ตกค้างหลังซัก (After Wash Claim)',
                                render_kw={'class': 'input is-danger'})
     after_wash_qualitative_test = RadioField('วิธีทดสอบเชิงคุณภาพ',
@@ -389,7 +389,7 @@ class BacteriaInWashTestConditionForm(FlaskForm):
 
 
 class BacteriaInWashConditionForm(FlaskForm):
-    product_type = HiddenField('ประเภทผลิตภัณฑ์',
+    in_wash_product_type = HiddenField('ประเภทผลิตภัณฑ์',
                                default='ผลิตภัณฑ์ฆ่าเชื้อที่ใช้ในกระบวนการซักผ้า-ผลิตภัณฑ์ที่อ้างสรรพคุณฤทธิ์ฆ่าเชื้อขณะซัก (In Wash Claim)',
                                render_kw={'class': 'input is-danger'})
     in_wash_test_method = RadioField('วิธีทดสอบเชิงปริมาณ',
@@ -407,7 +407,7 @@ class BacteriaAlcoholBasedTestConditionForm(FlaskForm):
 
 
 class BacteriaAlcoholBasedConditionForm(FlaskForm):
-    product_type = HiddenField('ประเภทผลิตภัณฑ์',
+    alcohol_based_product_type = HiddenField('ประเภทผลิตภัณฑ์',
                                default='ผลิตภัณฑ์ที่มีแอลกอฮอล์เป็ยส่วนประกอบ เพื่อสุขภาพอนามัยสำหรับมือ (Alcohol-based Hand sanitizer)',
                                render_kw={'class': 'input is-danger'})
     alcohol_based_test = RadioField('วิธีทดสอบ',
@@ -426,7 +426,7 @@ class BacteriaAntibacterialTreatedTestConditionForm(FlaskForm):
 
 
 class BacteriaAntibacterialTreatedConditionForm(FlaskForm):
-    product_type = HiddenField('ประเภทผลิตภัณฑ์',
+    antibacterial_treated_product_type = HiddenField('ประเภทผลิตภัณฑ์',
                                default='Antibacterial-treated Surface (plastic and other non-porous surface)',
                                render_kw={'class': 'input is-danger'})
     antibacterial_treated_test = RadioField('วิธีทดสอบ',
@@ -458,7 +458,7 @@ class BacteriaDishWashTestConditionForm(FlaskForm):
 
 
 class BacteriaDishWashConditionForm(FlaskForm):
-    product_type = HiddenField('ประเภทผลิตภัณฑ์',
+    dish_wash_product_type = HiddenField('ประเภทผลิตภัณฑ์',
                                default='ผลิตภัณฑ์ล้างจาน (Dish Wash Detergent)',
                                render_kw={'class': 'input is-danger'})
     dish_wash_test = RadioField('วิธีทดสอบ',
@@ -718,15 +718,15 @@ class BacteriaAntimicrobialActivityTestConditionForm(FlaskForm):
 
 
 class BacteriaAntimicrobialActivityConditionForm(FlaskForm):
-    product_type = HiddenField('ประเภทการทดสอบ',
+    antimicrobial_test_product_type = HiddenField('ประเภทการทดสอบ',
                                default='การทดสอบประสิทธิภาพการยับยังเชื้อแบคทีเรีย (Antimicrobial Activity)',
                                render_kw={'class': 'input is-danger'})
     antimicrobial_test_method = RadioField('วิธีทดสอบ',
-                                  choices=[('ทดสอบปริะสิทธิภาพการยับยั่งเชื้อแบคทีเรีย วิธีทดสอบ Disc Diffusion method (Clear zone)',
-                                            'ทดสอบปริะสิทธิภาพการยับยั่งเชื้อแบคทีเรีย วิธีทดสอบ Disc Diffusion method (Clear zone)')
+                                  choices=[('ทดสอบประสิทธิภาพการยับยั่งเชื้อแบคทีเรีย วิธีทดสอบ Disc Diffusion method (Clear zone)',
+                                            'ทดสอบประสิทธิภาพการยับยั่งเชื้อแบคทีเรีย วิธีทดสอบ Disc Diffusion method (Clear zone)')
                                            ],
                                   validators=[Optional()])
-    antibacterial_treated_organism_fields = FieldList(FormField(BacteriaAntimicrobialActivityTestConditionForm), min_entries=1)
+    antimicrobial_organism_fields = FieldList(FormField(BacteriaAntimicrobialActivityTestConditionForm), min_entries=1)
 
 
 class BacteriaAntimicrobialActivityRequestForm(FlaskForm):
@@ -858,7 +858,7 @@ class VirusLiquidTestConditionForm(FlaskForm):
 
 
 class VirusLiquidConditionForm(FlaskForm):
-    product_type = HiddenField('ประเภทผลิตภัณฑ์',
+    liquid_product_type = HiddenField('ประเภทผลิตภัณฑ์',
                                default='ผลิตภัณฑ์ฆ่าเชื้อ ชนิดของเหลว ชนิดผง หรือชนิดเม็ดที่ละลายน้ำได้',
                                render_kw={'class': 'input is-danger'})
     liquid_test_method = RadioField('วิธีทดสอบ',
@@ -890,7 +890,7 @@ class VirusSprayTestConditionForm(FlaskForm):
 
 
 class VirusSprayConditionForm(FlaskForm):
-    product_type = HiddenField('ประเภทผลิตภัณฑ์',
+    spray_product_type = HiddenField('ประเภทผลิตภัณฑ์',
                                default='ผลิตภัณฑ์ฆ่าเชื้อ ชนิดฉีดพ่น',
                                render_kw={'class': 'input is-danger'})
     spray_inject_type = RadioField('ประเภทการฉีด',
@@ -922,7 +922,7 @@ class VirusCoatTestConditionForm(FlaskForm):
 
 
 class VirusCoatConditionForm(FlaskForm):
-    product_type = HiddenField('ประเภทผลิตภัณฑ์', default='ผลิตภัณฑ์ฆ่าเชื้อที่เคลือบบนพื้นผิวสำเร็จรูป',
+    coat_product_type = HiddenField('ประเภทผลิตภัณฑ์', default='ผลิตภัณฑ์ฆ่าเชื้อที่เคลือบบนพื้นผิวสำเร็จรูป',
                                render_kw={'class': 'input is-danger'})
     coat_test_method = RadioField('วิธีทดสอบ', choices=[(c, c) for c in ['วิธีทดสอบ Modified ASTM E1053-20']],
                                   validators=[Optional()])
@@ -1052,7 +1052,7 @@ class VirusSurfaceDisinfectionTestConditionForm(FlaskForm):
 
 
 class VirusSurfaceDisinfectionConditionForm(FlaskForm):
-    product_type = HiddenField('รายการทดสอบ',
+    surface_disinfection_cproduct_type = HiddenField('รายการทดสอบ',
                                default='การฆ่าเชื้อบนพื้นผิว',
                                render_kw={'class': 'input is-danger'})
     surface_disinfection_clean_type = RadioField(
@@ -1067,26 +1067,26 @@ class VirusSurfaceDisinfectionConditionForm(FlaskForm):
                                                      min_entries=1)
 
 
-class VirusAirborneDisinfectionTestConditionForm(FlaskForm):
-    airborne_disinfection_organism = CheckboxField('เชื้อ', validators=[Optional()])
-    airborne_disinfection_period_test = StringField('ระยะเวลาที่ต้องการทดสอบเพื่อทำลายเชื้อ (นาที/ชั่วโมง)',
-                                                    validators=[Optional()],
-                                                    render_kw={'class': 'input'})
-
-
-class VirusAirborneDisinfectionConditionForm(FlaskForm):
-    product_type = HiddenField('รายการทดสอบ',
-                               default='การลด/ทำลายเชื้อในอากาศ',
-                               render_kw={'class': 'input is-danger'})
-    airborne_disinfection_clean_type = RadioField(
-        'รูปแบบการทดสอบ',
-        choices=[
-            ('ทดสอบการลดปริมาณเชื้อในอากาศ (เครื่องฟอกอากาศ)', 'ทดสอบการลดปริมาณเชื้อในอากาศ (เครื่องฟอกอากาศ)'),
-            ('ทดสอบการทำลายเชื้อในอากาศ (เครื่องปล่อยสารหรืออนุภาคทำลายเชื้อ)',
-             'ทดสอบการทำลายเชื้อในอากาศ (เครื่องปล่อยสารหรืออนุภาคทำลายเชื้อ)')
-        ], validators=[Optional()])
-    airborne_disinfection_organism_fields = FieldList(FormField(VirusAirborneDisinfectionTestConditionForm),
-                                                      min_entries=1)
+# class VirusAirborneDisinfectionTestConditionForm(FlaskForm):
+#     airborne_disinfection_organism = CheckboxField('เชื้อ', validators=[Optional()])
+#     airborne_disinfection_period_test = StringField('ระยะเวลาที่ต้องการทดสอบเพื่อทำลายเชื้อ (นาที/ชั่วโมง)',
+#                                                     validators=[Optional()],
+#                                                     render_kw={'class': 'input'})
+#
+#
+# class VirusAirborneDisinfectionConditionForm(FlaskForm):
+#     product_type = HiddenField('รายการทดสอบ',
+#                                default='การลด/ทำลายเชื้อในอากาศ',
+#                                render_kw={'class': 'input is-danger'})
+#     airborne_disinfection_clean_type = RadioField(
+#         'รูปแบบการทดสอบ',
+#         choices=[
+#             ('ทดสอบการลดปริมาณเชื้อในอากาศ (เครื่องฟอกอากาศ)', 'ทดสอบการลดปริมาณเชื้อในอากาศ (เครื่องฟอกอากาศ)'),
+#             ('ทดสอบการทำลายเชื้อในอากาศ (เครื่องปล่อยสารหรืออนุภาคทำลายเชื้อ)',
+#              'ทดสอบการทำลายเชื้อในอากาศ (เครื่องปล่อยสารหรืออนุภาคทำลายเชื้อ)')
+#         ], validators=[Optional()])
+#     airborne_disinfection_organism_fields = FieldList(FormField(VirusAirborneDisinfectionTestConditionForm),
+#                                                       min_entries=1)
 
 
 class VirusAirDisinfectionRequestForm(FlaskForm):
