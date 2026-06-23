3d006a7a2d8bbc12b026bb7c585571f57f279f7b
ASUS <somchai081643@gmail.com>
2026-06-23 09:05:33 +0700

    Edited send_email in create_repair_approval view of complaint_tracker views

--- app/complaint_tracker/views.py
+++ app/complaint_tracker/views.py
@@ -2521,7 +2521,7 @@ def create_repair_approval(record_id, repair_approval_id=None):
     has_procurement = True if record.procurements else False
     requester = record.complainant if record.complainant else current_user
     owner_id = requester.id
-
+    secretary = None
     if repair_approval_id:
         rep_approval = ComplaintRepairApproval.query.get(repair_approval_id)
         form = ComplaintRepairApprovalForm(obj=rep_approval)
@@ -2542,6 +2542,7 @@ def create_repair_approval(record_id, repair_approval_id=None):
             if not form.item.data:
                 form.item.data = f'เลขครุภัณฑ์ {procurement.procurement_no} {procurement.name}'
             if cost_center_auth:
+                secretary = cost_center_auth.secretary
                 if ((org.parent and org.parent.parent and org.parent.parent.name == 'สำนักงานคณบดี') or
                         (org.parent and org.parent.name == 'สำนักงานคณบดี') or (org.name == 'สำนักงานคณบดี')):
                     owner_id = cost_center_auth.secretary_id
@@ -2597,37 +2598,33 @@ def create_repair_approval(record_id, repair_approval_id=None):
         rep_approval.reviewed_at = None
         db.session.add(rep_approval)
         db.session.commit()
-            # if rep_approval.get_other_org == True:
-            #     scheme = 'http' if current_app.debug else 'https'
-            #     link = url_for("comp_tracker.repair_approval_index", tab='new', _external=True, _scheme=scheme)
-            #     if repair_approval_id:
-            #         title = f'''แจ้งแก้ไขใบอนุมัติหลักการซ่อม {rep_approval.item}'''
-            #         message = f'''เจ้าหน้าที่ได้ดำเนินการแก้ไขข้อมูลใบอนุมัติหลักการซ่อมสำหรับรายการ {rep_approval.item} เรียบร้อยแล้ว กรุณาดำเนินการในขั้นตอนถัดไปตามกระบวนการที่เกี่ยวข้อง\n'''
-            #         message += f'''ท่านสามารถดำเนินการพิมพ์เอกสารได้ที่ลิงก์ด้านล่าง\n{link}\n\n'''
-            #         message += f'''ขอบคุณค่ะ\nระบบรับแจ้งปัญหาหรือข้อร้องเรียน\nคณะเทคนิคการแพทย์'''
-            #     else:
-            #         title = f'''แจ้งออกใบอนุมัติหลักการซ่อม {rep_approval.item}'''
-            #         message = f'''เจ้าหน้าที่ได้ดำเนินการออกใบอนุมัติหลักการซ่อมสำหรับรายการ {rep_approval.item} เรียบร้อยแล้ว กรุณาดำเนินการในขั้นตอนถัดไปตามกระบวนการที่เกี่ยวข้อง\n'''
-            #         message += f'''ท่านสามารถดำเนินการพิมพ์เอกสารได้ที่ลิงก์ด้านล่าง\n{link}\n\n'''
-            #         message += f'''ขอบคุณค่ะ\nระบบรับแจ้งปัญหาหรือข้อร้องเรียน\nคณะเทคนิคการแพทย์'''
-            #         if rep_approval.record.complainant:
-            #             msg = (f'เจ้าหน้าที่ได้ดำเนินการออกใบอนุมัติหลักการซ่อมสำหรับรายการ {rep_approval.item} เรียบร้อยแล้ว\n\n'
-            #                    f'ขอบคุณค่ะ\nระบบรับแจ้งปัญหาหรือข้อร้องเรียน\nคณะเทคนิคการแพทย์')
-            #             message_for_complainant = f'''เจ้าหน้าที่ได้ดำเนินการออกใบอนุมัติหลักการซ่อมสำหรับรายการ {rep_approval.item} เรียบร้อยแล้ว\n\n'''
-            #             message_for_complainant += f'''ขอบคุณค่ะ\nระบบรับแจ้งปัญหาหรือข้อร้องเรียน\nคณะเทคนิคการแพทย์'''
-            #
-            #             send_mail([rep_approval.record.complainant.email + '@mahidol.ac.th'], title,
-            #                       message_for_complainant)
-            #             if not current_app.debug:
-            #                 try:
-            #                     line_bot_api.push_message(to=rep_approval.record.complainant.line_id, messages=TextSendMessage(text=msg))
-            #                 except LineBotApiError:
-            #                     pass
-            #             else:
-            #                 print('msg :', msg, 'line :', rep_approval.record.complainant.line_id)
-            #     if rep_approval.owner.personal_info.org.secretary_staff:
-            #         send_mail([secretary.email + '@mahidol.ac.th' for secretary in rep_approval.owner.personal_info.org.secretary_staff],
-            #                   title, message)
+        scheme = 'http' if current_app.debug else 'https'
+        link = url_for("comp_tracker.repair_approval_index", tab='new', _external=True, _scheme=scheme)
+        if repair_approval_id:
+            title = f'''แจ้งแก้ไขใบอนุมัติหลักการซ่อม {rep_approval.item}'''
+            message = f'''เจ้าหน้าที่ได้ดำเนินการแก้ไขข้อมูลใบอนุมัติหลักการซ่อมสำหรับรายการ {rep_approval.item} เรียบร้อยแล้ว กรุณาดำเนินการในขั้นตอนถัดไปตามกระบวนการที่เกี่ยวข้อง\n'''
+            message += f'''ท่านสามารถดำเนินการพิมพ์เอกสารได้ที่ลิงก์ด้านล่าง\n{link}\n\n'''
+            message += f'''ขอบคุณค่ะ\nระบบรับแจ้งปัญหาหรือข้อร้องเรียน\nคณะเทคนิคการแพทย์'''
+        else:
+            title = f'''แจ้งออกใบอนุมัติหลักการซ่อม {rep_approval.item}'''
+            message = f'''เจ้าหน้าที่ได้ดำเนินการออกใบอนุมัติหลักการซ่อมสำหรับรายการ {rep_approval.item} เรียบร้อยแล้ว กรุณาดำเนินการในขั้นตอนถัดไปตามกระบวนการที่เกี่ยวข้อง\n'''
+            message += f'''ท่านสามารถดำเนินการพิมพ์เอกสารได้ที่ลิงก์ด้านล่าง\n{link}\n\n'''
+            message += f'''ขอบคุณค่ะ\nระบบรับแจ้งปัญหาหรือข้อร้องเรียน\nคณะเทคนิคการแพทย์'''
+        if rep_approval.record.complainant:
+            msg = (f'เจ้าหน้าที่ได้ดำเนินการออกใบอนุมัติหลักการซ่อมสำหรับรายการ {rep_approval.item} เรียบร้อยแล้ว\n\n'
+                        f'ขอบคุณค่ะ\nระบบรับแจ้งปัญหาหรือข้อร้องเรียน\nคณะเทคนิคการแพทย์')
+            message_for_complainant = f'''เจ้าหน้าที่ได้ดำเนินการออกใบอนุมัติหลักการซ่อมสำหรับรายการ {rep_approval.item} เรียบร้อยแล้ว\n\n'''
+            message_for_complainant += f'''ขอบคุณค่ะ\nระบบรับแจ้งปัญหาหรือข้อร้องเรียน\nคณะเทคนิคการแพทย์'''
+            send_mail([rep_approval.record.complainant.email + '@mahidol.ac.th'], title, message_for_complainant)
+            if not current_app.debug:
+                try:
+                    line_bot_api.push_message(to=rep_approval.record.complainant.line_id, messages=TextSendMessage(text=msg))
+                except LineBotApiError:
+                    pass
+            else:
+                print('msg :', msg, 'line :', rep_approval.record.complainant.line_id)
+            if secretary:
+                send_mail([secretary.email + '@mahidol.ac.th'], title, message)
         flash('บันทึกข้อมูลสำเร็จ', 'success')
         return redirect(url_for('comp_tracker.edit_record_admin', record_id=record_id))
     else:
