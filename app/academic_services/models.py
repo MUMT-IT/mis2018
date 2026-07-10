17e07f617b7613f7d0b6b19cb4ba55b839479f73
ASUS <somchai081643@gmail.com>
2026-07-10 16:04:48 +0700

    Edited status in customer_status def and admin_status def of ServiceResultItem model and ServiceResult model of academic_services models

--- app/academic_services/models.py
+++ app/academic_services/models.py
@@ -1347,7 +1347,7 @@ class ServiceResult(db.Model):
             status = 'รอยืนยันใบรายงานผล'
             color = 'is-warning'
         elif not uploaded_all or (uploaded_all and not self.sent_at):
-            status = 'กำลังรอออกผล'
+            status = 'กำลังออกรายงานผล'
             color = 'is-light'
         else:
             status = 'ยังไม่ดำเนินการทดสอบ'
@@ -1456,7 +1456,7 @@ class ServiceResultItem(db.Model):
             status = 'รอยืนยันใบรายงานผล'
             color = 'is-warning'
         elif not uploaded_all:
-            status = 'กำลังรอออกผล'
+            status = 'กำลังออกรายงานผล'
             color = 'is-light'
         else:
             status = 'ยังไม่ดำเนินการทดสอบ'
