71b4abd9eccb370ac4334918ea314dba6ac676b9
Likit Preeyanon <likit.pre@mahidol.ac.th>
2026-07-30 09:00:23 +0700

    added OCR support for scanned documents
    added Flask command for backfilling unprocessed documents

--- app/main.py
+++ app/main.py
@@ -1693,6 +1693,9 @@ app.register_blueprint(software_request_blueprint)
 from app.docs_query import docs_query as docs_query_blueprint
 
 app.register_blueprint(docs_query_blueprint)
+from app.docs_query.commands import register_commands as register_docs_query_commands
+
+register_docs_query_commands(app)
 
 from app.software_request.models import *
 
