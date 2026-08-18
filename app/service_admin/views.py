9a91a9dc335407e95f90e14d699807d5abcaede7
ASUS <somchai081643@gmail.com>
2026-08-18 15:05:33 +0700

    Edited remove_sds_page_condition_item view in academic_services and service_admin views

--- app/service_admin/views.py
+++ app/service_admin/views.py
@@ -4714,47 +4714,17 @@ def add_sds_page_condition_item():
 @service_admin.route('/api/request/sds_page/item/remove', methods=['DELETE'])
 @login_required
 def remove_sds_page_condition_item():
+    field_name = request.args.get('name')
     form = SDSPageRequestForm()
-    form.sds_page_condition_field.pop_entry()
-    resp = ''
-    for i, item_form in enumerate(form.sds_page_condition_field, start=1):
-        hr = '<hr style="background-color: #F3F3F3">' if i > 1 else ''
-        template = """
-            <div id="{}">
-                {}
-                <p><strong>รายการที่ {}</strong></p>
-                <table class="table is-fullwidth ">
-                    <thead>
-                        <th style="border: none">
-                            {}
-                            <span class="has-text-danger">*</span>
-                        </th>
-                        <th style="border: none">{}</th>
-                        <th style="border: none">
-                            {}
-                            <span class="has-text-danger">*</span>
-                        </th>
-                    </thead>
-                    <tbody>
-                        <td style="border: none" class="control">{}</td>
-                        <td style="border: none" class="control">{}</td>
-                        <td style="border: none" class="control">{}</td>
-                    </tbody>
-                </table>
-            </div>
-        """
-        resp += template.format(item_form.id,
-                                hr,
-                                i,
-                                item_form.sample_name.label,
-                                item_form.clean_up.label,
-                                item_form.staining.label,
-                                item_form.sample_name(class_='input'),
-                                item_form.clean_up(),
-                                item_form.staining()
-                                )
-    resp = make_response(resp)
-    return resp
+    temp_entries = []
+    for entry in form.sds_page_condition_field:
+        if entry.name != field_name:
+            temp_entries.append(entry)
+    while len(form.sds_page_condition_field) > 0:
+        form.sds_page_condition_field.pop_entry()
+    for entry in temp_entries:
+        form.sds_page_condition_field.append_entry(entry)
+    return ""
 
 
 @service_admin.route('/request/quantitative/add', methods=['GET', 'POST'])
