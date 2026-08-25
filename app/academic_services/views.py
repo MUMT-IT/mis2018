f1fef4e33840c2831dddaa4f12a8215085142b22
ASUS <somchai081643@gmail.com>
2026-08-25 10:31:55 +0700

    Edited template value remove_metabolomic_request_condition_item view in academic_services views

--- app/academic_services/views.py
+++ app/academic_services/views.py
@@ -3832,48 +3832,64 @@ def add_metabolomic_condition_item():
 @academic_services.route('/api/request/metabolomic/item/remove', methods=['DELETE'])
 @login_required
 def remove_metabolomic_condition_item():
+    field_name = request.args.get('name')
     form = MetabolomicRequestForm()
-    form.metabolomic_condition_field.pop_entry()
-    resp = ''
-    for i, item_form in enumerate(form.metabolomic_condition_field, start=1):
-        hr = '<hr style="background-color: #F3F3F3">' if i > 1 else ''
-        template = """
-            <div id="{}">
-                {}
-                <p><strong>รายการที่ {}</strong></p>
-                <table class="table is-fullwidth ">
-                    <thead>
-                        <th style="border: none">
-                            {}
-                            <span class="has-text-danger">*</span>
-                        </th>
-                        <th style="border: none">{}</th>
-                        <th style="border: none">{}</th>
-                        <th style="border: none">{}</th>
-                    </thead>
-                    <tbody>
-                        <td style="border: none" class="control">{}</td>
-                        <td style="border: none" class="control">{}</td>
-                        <td style="border: none" class="control">{}</td>
-                        <td style="border: none" class="control">{}</td>
-                    </tbody>
-                </table>
-            </div>
-        """
-        resp += template.format(item_form.id,
-                                hr,
-                                i,
-                                item_form.sample_name.label,
-                                item_form.clean_up.label,
-                                item_form.untargeted_metabolomic.label,
-                                item_form.quantitative_metabolomic.label,
-                                item_form.sample_name(class_='input'),
-                                item_form.clean_up(),
-                                item_form.untargeted_metabolomic(),
-                                item_form.quantitative_metabolomic()
-                                )
-    resp = make_response(resp)
-    return resp
+    temp_entries = []
+    for entry in form.metabolomic_condition_field:
+        if entry.name != field_name:
+            temp_entries.append(entry)
+    while len(form.metabolomic_condition_field) > 0:
+        form.metabolomic_condition_field.pop_entry()
+    for entry in temp_entries:
+        form.metabolomic_condition_field.append_entry(entry)
+    return ""
+
+
+# @academic_services.route('/api/request/metabolomic/item/remove', methods=['DELETE'])
+# @login_required
+# def remove_metabolomic_condition_item():
+#     form = MetabolomicRequestForm()
+#     form.metabolomic_condition_field.pop_entry()
+#     resp = ''
+#     for i, item_form in enumerate(form.metabolomic_condition_field, start=1):
+#         hr = '<hr style="background-color: #F3F3F3">' if i > 1 else ''
+#         template = """
+#             <div id="{}">
+#                 {}
+#                 <p><strong>รายการที่ {}</strong></p>
+#                 <table class="table is-fullwidth ">
+#                     <thead>
+#                         <th style="border: none">
+#                             {}
+#                             <span class="has-text-danger">*</span>
+#                         </th>
+#                         <th style="border: none">{}</th>
+#                         <th style="border: none">{}</th>
+#                         <th style="border: none">{}</th>
+#                     </thead>
+#                     <tbody>
+#                         <td style="border: none" class="control">{}</td>
+#                         <td style="border: none" class="control">{}</td>
+#                         <td style="border: none" class="control">{}</td>
+#                         <td style="border: none" class="control">{}</td>
+#                     </tbody>
+#                 </table>
+#             </div>
+#         """
+#         resp += template.format(item_form.id,
+#                                 hr,
+#                                 i,
+#                                 item_form.sample_name.label,
+#                                 item_form.clean_up.label,
+#                                 item_form.untargeted_metabolomic.label,
+#                                 item_form.quantitative_metabolomic.label,
+#                                 item_form.sample_name(class_='input'),
+#                                 item_form.clean_up(),
+#                                 item_form.untargeted_metabolomic(),
+#                                 item_form.quantitative_metabolomic()
+#                                 )
+#     resp = make_response(resp)
+#     return resp
 
 
 @academic_services.route("/request/sample_species_other")
