90ebf9ff9efef5eb3df0e41496a6f80ef5597aee
ASUS <somchai081643@gmail.com>
2026-08-25 09:20:11 +0700

    Edited condition in create_quantitative_request view of academic_services views

--- app/academic_services/views.py
+++ app/academic_services/views.py
@@ -3606,8 +3606,8 @@ def create_quantitative_request(request_id=None):
             url_for('academic_services.create_report_language', request_id=service_request.id, menu=menu,
                     code=code))
     else:
-        for er in form.errors:
-            flash(f'{er} {form.errors[er]}', 'danger')
+        for _ in form.errors:
+            flash(f'กรุณากรอกข้อมูลให้ครบถ้วน', 'danger')
     return render_template('academic_services/forms/quantitative_request_form.html', code=code, sub_lab=sub_lab,
                            form=form, menu=menu, request_id=request_id)
 
