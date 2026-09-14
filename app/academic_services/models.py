6a89d7cc2436e8550d025d657d11e3927e608b16
ASUS <somchai081643@gmail.com>
2026-09-14 10:51:39 +0700

    Added file column in ServiceCustomerAttachment model in academic_services models

--- app/academic_services/models.py
+++ app/academic_services/models.py
@@ -256,6 +256,7 @@ class ServiceCustomerAttachment(db.Model):
     id = db.Column('id', db.Integer(), primary_key=True, autoincrement=True)
     name = db.Column('name', db.String())
     note = db.Column('note', db.String())
+    file = db.Column('file', db.String())
     customer_id = db.Column('customer_id', db.ForeignKey('service_customer_infos.id'))
     customer = db.relationship(ServiceCustomerInfo, backref=db.backref('attachments', cascade='all, delete-orphan'))
 
