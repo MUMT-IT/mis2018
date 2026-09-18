8b6ca56e46b690ad1c2d451408443dfdd92d7927
ASUS <somchai081643@gmail.com>
2026-09-18 13:42:59 +0700

    Edited flash in create_request_id view of service_admin views

--- app/service_admin/views.py
+++ app/service_admin/views.py
@@ -2447,7 +2447,7 @@ def create_request_id(sub_lab_id):
         db.session.commit()
         resp = make_response()
         resp.headers['HX-Redirect'] = url_for('service_admin.external_lab_index', customer_id=customer_id)
-        flash('บันทึกข้อมูลลำเร็จ', 'success')
+        flash('สร้าง Request ID ลำเร็จ', 'success')
         return resp
     return redirect(url_for('service_admin.external_lab_index', customer_id=customer_id))
 
