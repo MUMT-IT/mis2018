ae2bf42348c906ec2c07448a161d2bc2027b0967
Likit Preeyanon <likit.pre@mahidol.edu>
2026-09-23 13:45:03 +0700

    Merged heads

---
+++ migrations/versions/1355548973a8_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 1355548973a8
+Revises: 696ee99c5bdc, d744d5b459b4
+Create Date: 2026-09-23 13:44:29.694025
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1355548973a8'
+down_revision = ('696ee99c5bdc', 'd744d5b459b4')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
